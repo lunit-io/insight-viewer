f5ba7be0356527be888575034bb12ab17a156749
SSen <i@ssen.name>
2020-03-09 12:29:09 +0900

    docs: handbook re-indexing

---
+++ src/_packages/@lunit/insight-viewer/__pages__/index.ts
@@ -0,0 +1,13 @@
+import { HandbookTreeNode } from '@handbook/components';
+import { page } from '@handbook/source';
+
+export const insightViewerPages: HandbookTreeNode = {
+  InsightViewer: {
+    Basic: page('./InsightViewer/Basic'),
+  },
+  Annotation: {
+    Contour: {
+      Viewer: page('./Annotation/Contour/Viewer'),
+    },
+  },
+};
