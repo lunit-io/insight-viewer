633426fe1a3e3a06c64582fa93e078ecc5767092
SSen <i@ssen.name>
2020-03-09 14:01:15 +0900

    docs: update insight-viewer docs...

--- src/_packages/@lunit/insight-viewer/__pages__/index.ts
+++ src/_packages/@lunit/insight-viewer/__pages__/index.ts
@@ -2,6 +2,7 @@ import { HandbookTreeNode } from '@handbook/components';
 import { page } from '@handbook/source';
 
 export const insightViewerPages: HandbookTreeNode = {
+  'Getting Started': page('./Basic/Getting-Started'),
   '<InsightViewer>': {
     Basic: page('./InsightViewer/Basic'),
   },
