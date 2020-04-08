120ea5bff39aa830faba9c096bfe428c1c7fcfea
SSen <i@ssen.name>
2020-04-08 19:08:06 +0900

    feat: touch device support - series scroll

--- src/_packages/@lunit/insight-viewer/hooks/useSeriesImageScroll.ts
+++ src/_packages/@lunit/insight-viewer/hooks/useSeriesImageScroll.ts
@@ -9,15 +9,23 @@ interface SeriesImageScrollParams {
 
   /** User Interaction 활성화 여부 */
   enabled?: boolean;
+
+  /** Touch Direction */
+  touchDirection?: 'horizontal' | 'vertical';
 }
 
-export function useSeriesImageScroll({ image, element, enabled = true }: SeriesImageScrollParams) {
+export function useSeriesImageScroll({
+  image,
+  element,
+  enabled = true,
+  touchDirection = 'horizontal',
+}: SeriesImageScrollParams) {
   useEffect(() => {
     if (!element || enabled !== true) {
       return () => {};
     }
 
-    function handler(event: WheelEvent) {
+    function wheel(event: WheelEvent) {
       event.preventDefault();
       event.stopPropagation();
       event.stopImmediatePropagation();
@@ -29,12 +37,70 @@ export function useSeriesImageScroll({ image, element, enabled = true }: SeriesI
       }
     }
 
-    element.addEventListener('wheel', handler);
+    let startPoint: { x: number; y: number } | null = null;
+
+    function touchStart(event: TouchEvent) {
+      if (event.targetTouches.length !== 2) return;
+
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+      event.preventDefault();
+
+      startPoint = {
+        x: event.targetTouches[0].pageX,
+        y: event.targetTouches[0].pageY,
+      };
+
+      if (!element) {
+        throw new Error(`undefined element!!!`);
+      }
+
+      element.removeEventListener('touchstart', touchStart);
+
+      element.addEventListener('touchmove', touchMove);
+      element.addEventListener('touchend', touchEnd);
+      element.addEventListener('touchcancel', touchEnd);
+    }
+
+    function touchMove(event: TouchEvent) {
+      if (event.targetTouches.length !== 2 || event.changedTouches.length !== 2 || !startPoint) return;
+
+      event.stopPropagation();
+      event.stopImmediatePropagation();
+      event.preventDefault();
+
+      const dx: number = event.targetTouches[0].pageX - startPoint.x;
+      const dy: number = event.targetTouches[0].pageY - startPoint.y;
+      const d: number = touchDirection === 'vertical' ? dy : dx;
+
+      image.goto(image.getIndex() + d / 100);
+    }
+
+    function touchEnd(event: TouchEvent) {
+      if (!element) {
+        throw new Error(`undefined element!!!`);
+      }
+
+      element.removeEventListener('touchmove', touchMove);
+      element.removeEventListener('touchend', touchEnd);
+      element.removeEventListener('touchcancel', touchEnd);
+
+      element.addEventListener('touchstart', touchStart);
+    }
+
+    element.addEventListener('wheel', wheel);
+    element.addEventListener('touchstart', touchStart);
 
     return () => {
-      element.removeEventListener('wheel', handler);
+      element.removeEventListener('wheel', wheel);
+
+      element.removeEventListener('touchstart', touchStart);
+
+      element.removeEventListener('touchmove', touchMove);
+      element.removeEventListener('touchend', touchEnd);
+      element.removeEventListener('touchcancel', touchEnd);
     };
-  }, [image, element, enabled]);
+  }, [image, element, enabled, touchDirection]);
 }
 
 /** @deprecated use useSeriesImageScroll */
