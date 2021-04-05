e5ed5c2c5b28de741b536c32fcc84021189c0367
skpark <skpark@lunit.io>
2021-04-05 15:50:45 +0900

    chore: Update lint-staged.config to run script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   '{apps,packages}/**/*.ts?(x)': [
-    () => 'tsc -p tsconfig.lint-staged.json',
-    'eslint --cache',
-    'prettier --write',
+    () => 'yarn run tsc --p tsconfig.lint-staged.json',
+    'yarn run eslint --cache',
+    'yarn run prettier --write',
   ],
   '{apps,packages}/**/*.js?(x)': ['eslint --cache', 'prettier --write'],
 }
