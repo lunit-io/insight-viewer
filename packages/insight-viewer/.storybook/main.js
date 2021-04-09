1e295e8466033a3e11fbfd62e789b7f5fb269718
skpark <skpark@lunit.io>
2021-04-09 09:49:09 +0900

    chore: Rename docs as doc

--- packages/insight-viewer/.storybook/main.js
+++ packages/insight-viewer/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    '../docs/**/*.stories.mdx',
-    '../docs/**/*.stories.@(js|jsx|ts|tsx)',
+    '../doc/**/*.stories.mdx',
+    '../doc/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
