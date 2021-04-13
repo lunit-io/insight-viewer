3fb2a417fc25fe05461892cd348de438b0bef0c3
skyparci <skyparci@gmail.com>
2021-04-13 17:27:38 +0900

    feat: Add _document.tsx and update app.tsx

---
+++ apps/insight-viewer-demo/src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
+
+export default class Document extends NextDocument {
+  static getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    return NextDocument.getInitialProps(ctx)
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
