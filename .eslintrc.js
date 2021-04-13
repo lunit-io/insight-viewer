716ac7c41672296b95513e44192b956d851fa109
skyparci <skyparci@gmail.com>
2021-04-13 16:16:14 +0900

    chore: Turn off rule that is not necessary in Next.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,6 +71,16 @@ module.exports = {
         'react/prop-types': 'off',
         'react/require-default-props': 'off',
       },
-    }
+    },
+    {
+      // Turn off rule that are not necessary in Next.js
+      files: [
+        './apps/insight-viewer-demo/src/**/*.ts', 
+        './apps/insight-viewer-demo/src/**/*.tsx',
+      ],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
   ]
 }
