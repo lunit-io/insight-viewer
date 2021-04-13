45ef0f13570bd69a979bbe159c82d9c91e64b049
skyparci <skyparci@gmail.com>
2021-04-13 19:55:05 +0900

    feat: Add NextChakraLink component

--- apps/insight-viewer-demo/src/pages/index.tsx
+++ apps/insight-viewer-demo/src/pages/index.tsx
@@ -1,9 +1,35 @@
 import { FC } from 'react'
+import { Box, VStack, Grid, ListItem, UnorderedList } from '@chakra-ui/react'
+import { Chakra } from '../components/Chakra'
+import { Layout } from '../components/Layout'
+import { NextChakraLink } from '../components/NextChakraLink'
 
-const IndexPage: FC = () => (
-  <div style={{ margin: 20 }}>
-    <h1>Insight Viewer Demo 👋</h1>
-  </div>
+interface IndexProps {
+  cookies?: string
+}
+
+const IndexPage: FC<IndexProps> = ({ cookies }) => (
+  <Chakra cookies={cookies}>
+    <Layout title="@lunit/insight-viewer demo">
+      <Box fontSize="lg">
+        <Grid minH="100vh" p={3}>
+          <VStack spacing={8}>
+            <UnorderedList>
+              <ListItem>
+                <NextChakraLink href="/" color="teal.500">
+                  Lorem ipsum dolor sit amet
+                </NextChakraLink>
+              </ListItem>
+              <ListItem>Consectetur adipiscing elit</ListItem>
+              <ListItem>Integer molestie lorem at massa</ListItem>
+              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+            </UnorderedList>
+          </VStack>
+        </Grid>
+      </Box>
+    </Layout>
+  </Chakra>
 )
 
 export default IndexPage
+export { getServerSideProps } from '../components/Chakra'
