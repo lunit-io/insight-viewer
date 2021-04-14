d358c1ffc94f587078e777bedb1f62709ea2dd89
skyparci <skyparci@gmail.com>
2021-04-14 21:19:21 +0900

    feat: Do not use ssr

--- apps/insight-viewer-demo/src/pages/_app.tsx
+++ apps/insight-viewer-demo/src/pages/_app.tsx
@@ -2,7 +2,10 @@ import { AppProps } from 'next/app'
 import { Layout } from '../components/Layout'
 import { Chakra } from '../components/Chakra'
 
+const isServer = typeof window === 'undefined'
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  if (isServer) return <></>
   return (
     <Chakra>
       <Layout title="@lunit/insight-viewer demo">
