3e65747e8eb332ca12ad5535003fee9014bb025c
skyparci <skyparci@gmail.com>
2021-04-14 17:41:08 +0900

    feat: Use @lunit/insight-viewer in insight-viewer-demo

--- apps/insight-viewer-demo/src/pages/basic.tsx
+++ apps/insight-viewer-demo/src/pages/basic.tsx
@@ -1,5 +1,13 @@
+// import InsightViewer from '@lunit/insight-viewer'
+// 로컬에서 verdaccio로 배포테스트.
+import InsightViewer from '../../../../packages/insight-viewer/dist/index.esm'
 import { WithoutProp } from '../types'
 
-const Basic: WithoutProp = () => <div>basic</div>
+// eslint-disable-next-line no-console
+const handleClick = () => console.log('click')
+
+const Basic: WithoutProp = () => (
+  <InsightViewer onClick={handleClick}>Yay!</InsightViewer>
+)
 
 export default Basic
