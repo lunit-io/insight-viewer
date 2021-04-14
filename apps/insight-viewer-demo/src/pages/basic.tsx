d4c6204d758c6aed119ae732d80f5515cccddcaf
skyparci <skyparci@gmail.com>
2021-04-14 18:05:11 +0900

    feat: Use consola

--- apps/insight-viewer-demo/src/pages/basic.tsx
+++ apps/insight-viewer-demo/src/pages/basic.tsx
@@ -1,10 +1,10 @@
 // import InsightViewer from '@lunit/insight-viewer'
 // 로컬에서 verdaccio로 배포테스트.
+import consola from 'consola'
 import InsightViewer from '../../../../packages/insight-viewer/dist/index.esm'
 import { WithoutProp } from '../types'
 
-// eslint-disable-next-line no-console
-const handleClick = () => console.log('click')
+const handleClick = () => consola.info('click')
 
 const Basic: WithoutProp = () => (
   <InsightViewer onClick={handleClick}>Yay!</InsightViewer>
