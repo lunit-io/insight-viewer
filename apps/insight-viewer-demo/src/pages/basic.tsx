86e7a6e3bc9493bad5acca34ad1c8e4ffb9ba7b8
skpark <skpark@lunit.io>
2021-04-15 09:35:55 +0900

    feat: Use local package

--- apps/insight-viewer-demo/src/pages/basic.tsx
+++ apps/insight-viewer-demo/src/pages/basic.tsx
@@ -1,7 +1,6 @@
-// import InsightViewer from '@lunit/insight-viewer'
-// 로컬에서 verdaccio로 배포테스트.
+/* eslint-disable import/no-unresolved */
+import InsightViewer from '@lunit/insight-viewer'
 import consola from 'consola'
-import InsightViewer from '../../../../packages/insight-viewer/dist/index.esm'
 import { WithoutProp } from '../types'
 
 const handleClick = () => consola.info('click')
