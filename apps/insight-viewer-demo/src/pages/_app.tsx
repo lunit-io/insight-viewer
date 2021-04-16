2d97feea35ed40cc62351813ae30097578c0dc0e
skpark <skpark@lunit.io>
2021-04-16 21:36:47 +0900

    feat: Update _app.tsx conditional rendering

--- apps/insight-viewer-demo/src/pages/_app.tsx
+++ apps/insight-viewer-demo/src/pages/_app.tsx
@@ -5,11 +5,10 @@ import { Chakra } from '../components/Chakra'
 const isServer = typeof window === 'undefined'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  if (isServer) return <></>
   return (
     <Chakra>
       <Layout title="@lunit/insight-viewer demo">
-        <Component {...pageProps} />
+        {!isServer && <Component {...pageProps} />}
       </Layout>
     </Chakra>
   )
