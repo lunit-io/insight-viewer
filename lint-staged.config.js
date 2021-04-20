8efb42efe9bda17310ed81c3548bb0575966b820
skpark-lunit <skpark@lunit.io>
2021-04-20 17:12:13 +0900

    chore: Fix lint-staged issue which does not ignore files from .eslintignore

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,24 @@
+const { ESLint } = require('eslint')
+const filterAsync = require('node-filter-async').default
+
+const eslintCli = new ESLint()
+
+const removeIgnoredFiles = async (files) => {
+  const filteredFiles = await filterAsync(files, async (file) => {
+    const isIgnored = await eslintCli.isPathIgnored(file)
+    return !isIgnored
+  })
+  return filteredFiles.join(' ')
+}
+
 module.exports = {
-  '{apps,packages}/**/*.ts?(x)': [
-    () => 'yarn run tsc --p tsconfig.lint-staged.json',
-    `yarn run eslint --max-warnings=0 --cache`,
-    `yarn run prettier --write`,
-  ],
-  '{apps,packages}/**/*.js?(x)': [
-    'yarn run eslint --cache',
-    'yarn run prettier --write',
-  ],
+  '{apps,packages}/**/*.ts?(x)': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [
+      'yarn run tsc --p tsconfig.lint-staged.json',
+      `yarn run eslint --max-warnings=0 --cache ${filesToLint}`,
+      `yarn run prettier --write`,
+    ]
+  },
+  '{apps,packages}/**/*.js?(x)': ['yarn run eslint --cache', 'yarn run prettier --write'],
 }
