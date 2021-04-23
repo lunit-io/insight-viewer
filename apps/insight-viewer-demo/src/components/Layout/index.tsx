a3167e079455f155994aac3898e59aa3e54361aa
skpark-lunit <skpark@lunit.io>
2021-04-23 16:15:40 +0900

    feat: Remove WithoutProp type

--- apps/insight-viewer-demo/src/components/Layout/index.tsx
+++ apps/insight-viewer-demo/src/components/Layout/index.tsx
@@ -1,44 +1,43 @@
-import { ReactNode, FC } from 'react'
 import { Flex, Heading, HStack, Box } from '@chakra-ui/react'
 import { Logo } from '../Logo'
 import Nav from '../Nav'
 import { NextChakraLink } from '../NextChakraLink'
 import config from '../../../config'
+import { WithChildren } from '../../types'
 
-type Props = {
-  children?: ReactNode
-  title?: string
-}
-
-export const Layout: FC<Props> = ({ children }) => (
-  <div>
-    <Flex
-      w="100%"
-      h="100%"
-      px="6"
-      justify="space-between"
-      flexDirection="column"
-    >
-      <header>
-        <Flex w="100%">
-          <Box p="4">
-            <HStack spacing={12}>
-              <NextChakraLink href={`${config.HOST}`}>
-                <Logo h="1.5rem" pointerEvents="none" />
-              </NextChakraLink>
-              <Heading as="h1" size="md">
-                @lunit/insight-viewer demo
-              </Heading>
-            </HStack>
+function Layout({ children }: WithChildren): JSX.Element {
+  return (
+    <div>
+      <Flex
+        w="100%"
+        h="100%"
+        px="6"
+        justify="space-between"
+        flexDirection="column"
+      >
+        <header>
+          <Flex w="100%">
+            <Box p="4">
+              <HStack spacing={12}>
+                <NextChakraLink href={`${config.HOST}`}>
+                  <Logo h="1.5rem" pointerEvents="none" />
+                </NextChakraLink>
+                <Heading as="h1" size="md">
+                  @lunit/insight-viewer demo
+                </Heading>
+              </HStack>
+            </Box>
+          </Flex>
+        </header>
+        <Flex w="100%" h="100%">
+          <Nav />
+          <Box flex="1" p="12" align="flex-start">
+            {children}
           </Box>
         </Flex>
-      </header>
-      <Flex w="100%" h="100%">
-        <Nav />
-        <Box flex="1" p="12" align="flex-start">
-          {children}
-        </Box>
       </Flex>
-    </Flex>
-  </div>
-)
+    </div>
+  )
+}
+
+export default Layout
--- apps/insight-viewer-demo/src/pages/_app.tsx
+++ apps/insight-viewer-demo/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import { Layout } from '../components/Layout'
+import Layout from '../components/Layout'
 import { Chakra } from '../components/Chakra'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
