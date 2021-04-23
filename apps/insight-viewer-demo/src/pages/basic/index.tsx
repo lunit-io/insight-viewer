3e8e63942cd96d3326425147391ab053f9acd98e
skpark-lunit <skpark@lunit.io>
2021-04-23 16:30:12 +0900

    feat: Separate pages and containers for handling dynamic import

---
+++ apps/insight-viewer-demo/src/pages/basic/index.tsx
@@ -0,0 +1,8 @@
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../../containers/Basic'),
+  { ssr: false }
+)
+
+export default DynamicComponentWithNoSSR
