ad0847369fff7c21f1b7de2dffcabbe553e4c81e
skpark-lunit <skpark@lunit.io>
2021-04-26 12:26:42 +0900

    feat: Add WebImageViewer demo and CodeBlock guide

--- apps/insight-viewer-demo/src/containers/Basic.tsx
+++ apps/insight-viewer-demo/src/containers/Basic.tsx
@@ -1,10 +1,52 @@
-import InsightViewer from '@lunit/insight-viewer'
+import { Box, Heading } from '@chakra-ui/react'
+import InsightViewer, { WebImageViewer } from '@lunit/insight-viewer'
+import CodeBlock from '../components/CodeBlock'
 
 const IMAGE_ID =
   'wadouri:https://raw.githubusercontent.com/cornerstonejs/cornerstoneWADOImageLoader/master/testImages/CT2_J2KR'
 
-function Basic(): JSX.Element {
+export const WEB_URL =
+  'https://rawgit.com/cornerstonejs/cornerstoneWebImageLoader/master/examples/Renal_Cell_Carcinoma.jpg'
+
+const code = `\
+import InsightViewer from '@lunit/insight-viewer'
+
+export default function() {
   return <InsightViewer imageId={IMAGE_ID} />
+}
+`
+
+const vebImageViewercode = `\
+import { WebImageViewer } from '@lunit/insight-viewer'
+
+export default function() {
+  return <InsightViewer imageId={WEB_URL} />
+}
+`
+
+function Basic(): JSX.Element {
+  return (
+    <>
+      <Box mb={6}>
+        <Heading as="h3">InsightViewer</Heading>
+      </Box>
+      <Box w={700}>
+        <Box w={500} h={500}>
+          <InsightViewer imageId={IMAGE_ID} />
+        </Box>
+        <CodeBlock code={code} />
+      </Box>
+      <Box mt={10} mb={6}>
+        <Heading as="h3">WebImageViewer</Heading>
+      </Box>
+      <Box w={700}>
+        <Box w={500} h={500}>
+          <WebImageViewer imageId={WEB_URL} />
+        </Box>
+        <CodeBlock code={vebImageViewercode} />
+      </Box>
+    </>
+  )
 }
 
 export default Basic
