d88ba1ca901c17b9d614b1e22b94572f561155fb
skpark-lunit <skpark@lunit.io>
2021-04-30 23:39:54 +0900

    feat: Add axios client demo page

--- apps/insight-viewer-demo/src/components/Nav/const.ts
+++ apps/insight-viewer-demo/src/components/Nav/const.ts
@@ -1,4 +1,4 @@
 export const LINKS = [
   { href: 'basic', name: 'Basic' },
-  { href: 'test', name: 'test' },
+  { href: 'axios-client', name: 'AxiosClient' },
 ]
---
+++ apps/insight-viewer-demo/src/pages/axios-client/index.tsx
@@ -0,0 +1,101 @@
+import { useState } from 'react'
+import { Box, Button, Grid, GridItem } from '@chakra-ui/react'
+import AxiosClient from '@lunit/axios-client'
+import config from '../../../config'
+import { Model } from '../../mocks/apis'
+import CodeBlock from '../../components/CodeBlock'
+
+const axiosClient = new AxiosClient({
+  baseURL: `${config.HOST}`,
+})
+
+const code = `\
+import AxiosClient from '@lunit/axios-client'
+
+const axiosClient = new AxiosClient({
+  baseURL: 'baseURL',
+})
+
+async function handleSuccess(): Promise<void> {
+  const { data, error } = await axiosClient.request<{
+    username: string
+  }>(
+    'GET', 
+    '/api/success', 
+    { params: { username: 'lunit' } }
+  )
+}
+`
+
+export default function Test(): JSX.Element {
+  const [{ username, errorMessage }, setState] = useState({
+    username: '',
+    errorMessage: '',
+  })
+
+  async function handleSuccess(): Promise<void> {
+    const { data } = await axiosClient.request<Model>('GET', '/api/success', {
+      params: {
+        username: 'lunit',
+      },
+    })
+
+    setState(prev => ({
+      ...prev,
+      username: data?.username ?? '',
+    }))
+  }
+
+  async function handleError(): Promise<void> {
+    const { error } = await axiosClient.request<Model>(
+      'POST',
+      '/api/error/503',
+      {
+        data: {
+          username: 'lunit',
+        },
+      }
+    )
+
+    setState(prev => ({
+      ...prev,
+      errorMessage: `${error?.message}`,
+    }))
+  }
+
+  return (
+    <div>
+      <Box w={900} mb={6}>
+        <CodeBlock code={code} />
+      </Box>
+      <Box mb={6}>
+        <Grid templateColumns="repeat(12, 1fr)">
+          <GridItem colSpan={1}>
+            <Button colorScheme="blue" onClick={handleSuccess}>
+              200 OK
+            </Button>
+          </GridItem>
+          <GridItem colSpan={11}>
+            <Box w={300} p={6} bg="blue.50" borderWidth="1px">
+              username: {username}
+            </Box>
+          </GridItem>
+        </Grid>
+      </Box>
+      <Box mb={6}>
+        <Grid templateColumns="repeat(12, 1fr)">
+          <GridItem colSpan={1}>
+            <Button colorScheme="blue" onClick={handleError}>
+              error
+            </Button>
+          </GridItem>
+          <GridItem colSpan={11}>
+            <Box w={300} p={6} bg="red.50" borderWidth="1px">
+              error: {errorMessage}
+            </Box>
+          </GridItem>
+        </Grid>
+      </Box>
+    </div>
+  )
+}
