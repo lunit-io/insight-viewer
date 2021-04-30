5c2c45c71f3134691c136305cfe802bbdb2dc514
skpark-lunit <skpark@lunit.io>
2021-04-30 17:50:37 +0900

    chore: Start msw mock service worker

--- apps/insight-viewer-demo/src/pages/_app.tsx
+++ apps/insight-viewer-demo/src/pages/_app.tsx
@@ -1,14 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable global-require */
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from '../components/Layout'
 import { Chakra } from '../components/Chakra'
 import '../styles/globals.css'
 
+if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
+  const { worker } = require('../mocks/browser')
+  worker.start()
+}
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <Chakra>
-      <Layout title="@lunit/insight-viewer demo">
-        <Component {...pageProps} />
-      </Layout>
-    </Chakra>
+    <>
+      <Head>
+        <title>@lunit/insight-viewer demo</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Chakra>
+        <Layout title="@lunit/insight-viewer demo">
+          <Component {...pageProps} />
+        </Layout>
+      </Chakra>
+    </>
   )
 }
