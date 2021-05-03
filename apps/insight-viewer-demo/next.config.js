9e7d1225f1134f2d278823b344c4fe847c82971f
skpark-lunit <skpark@lunit.io>
2021-05-03 09:39:50 +0900

    chore: Remove next.js exportPathMap config

--- apps/insight-viewer-demo/next.config.js
+++ apps/insight-viewer-demo/next.config.js
@@ -6,15 +6,6 @@ module.exports = {
   assetPrefix: process.env.HOST,
   trailingSlash: true,
   productionBrowserSourceMaps: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/basic': { page: '/basic' },
-    }
-  },
   env: {
     HOST: process.env.HOST,
   },
