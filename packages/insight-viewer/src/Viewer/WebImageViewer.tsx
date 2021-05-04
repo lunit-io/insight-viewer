29f5d906039830658495cf3a690e7001e6cbc3d3
skpark-lunit <skpark@lunit.io>
2021-05-04 20:00:53 +0900

    feat: Add viewer type prop to ImageViewer so loading progress displays only for dicom image viewer

--- packages/insight-viewer/src/Viewer/WebImageViewer.tsx
+++ packages/insight-viewer/src/Viewer/WebImageViewer.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import ViewerWrapper from '../components/ViewerWrapper'
 import { WithChildren } from '../types'
 import useWebImageLoader from '../hooks/useWebImageLoader'
+import { VIEWER_TYPE } from '../const'
 
 export function WebImageViewer({
   imageId,
@@ -9,5 +10,5 @@ export function WebImageViewer({
   const elRef = useRef<HTMLDivElement>(null)
   useWebImageLoader(imageId, elRef.current)
 
-  return <ViewerWrapper ref={elRef} />
+  return <ViewerWrapper ref={elRef} type={VIEWER_TYPE.WEB} />
 }
--- packages/insight-viewer/src/components/ViewerWrapper/index.tsx
+++ packages/insight-viewer/src/components/ViewerWrapper/index.tsx
@@ -2,8 +2,9 @@ import React, { forwardRef, useCallback } from 'react'
 import styled from 'styled-components'
 import { useResizeDetector } from 'react-resize-detector'
 import { resize } from '../../utils/cornerstoneHelper'
-import { WithChildren } from '../../types'
+import { WithChildren, ViewerType } from '../../types'
 import LoadingProgress from '../LoadingProgress'
+import { VIEWER_TYPE } from '../../const'
 
 const StyledWrapper = styled.div`
   position: relative;
@@ -13,29 +14,32 @@ const StyledWrapper = styled.div`
   user-select: none;
 `
 
-const ViewerWrapper = forwardRef<HTMLDivElement, WithChildren>(
-  ({ children }, ref) => {
-    const element = (ref as React.MutableRefObject<HTMLDivElement | null>)
-      ?.current
-    const handleResize = useCallback(() => {
-      if (!element) return undefined
-      return resize(element)
-    }, [element])
+const ViewerWrapper = forwardRef<
+  HTMLDivElement,
+  WithChildren<{
+    type: ViewerType
+  }>
+>(({ type, children }, ref) => {
+  const element = (ref as React.MutableRefObject<HTMLDivElement | null>)
+    ?.current
+  const handleResize = useCallback(() => {
+    if (!element) return undefined
+    return resize(element)
+  }, [element])
 
-    const { ref: resizeRef } = useResizeDetector({
-      targetRef: ref as React.MutableRefObject<HTMLDivElement>,
-      onResize: handleResize,
-      skipOnMount: false,
-    })
+  const { ref: resizeRef } = useResizeDetector({
+    targetRef: ref as React.MutableRefObject<HTMLDivElement>,
+    onResize: handleResize,
+    skipOnMount: false,
+  })
 
-    return (
-      <StyledWrapper ref={resizeRef}>
-        <LoadingProgress />
-        <canvas className="cornerstone-canvas" />
-        {children}
-      </StyledWrapper>
-    )
-  }
-)
+  return (
+    <StyledWrapper ref={resizeRef}>
+      {type === VIEWER_TYPE.DICOM && <LoadingProgress />}
+      <canvas className="cornerstone-canvas" />
+      {children}
+    </StyledWrapper>
+  )
+})
 
 export default ViewerWrapper
