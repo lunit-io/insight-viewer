33612c55e553dc047abd991a602e943d10b33c23
skpark-lunit <skpark@lunit.io>
2021-05-06 11:12:56 +0900

    feat: Add error demo page

---
+++ apps/insight-viewer-demo/src/pages/error/index.tsx
@@ -0,0 +1,8 @@
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../../containers/Error'),
+  { ssr: false }
+)
+
+export default DynamicComponentWithNoSSR
