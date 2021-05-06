2b9ff3a912600fb454a5e8dfeeac00ca8d57675f
skpark-lunit <skpark@lunit.io>
2021-05-06 16:28:58 +0900

    feat: Use custom or default error handler

--- packages/insight-viewer/src/hooks/useLoadImage.ts
+++ packages/insight-viewer/src/hooks/useLoadImage.ts
@@ -1,12 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import {
   displayImage,
   loadImage as cornerstoneLoadImage,
   getDefaultViewportForImage,
 } from '../utils/cornerstoneHelper'
-import { handleError } from '../utils/common'
 import httpClient from '../utils/httpClient'
-import { errorMessage } from '../utils/messageService'
+import { ViewContext } from '../Viewer'
 
 interface Prop {
   imageId: string
@@ -20,6 +19,7 @@ export default function useLoadImage({
   setLoader,
 }: Prop): boolean {
   const [hasLoader, setHasLoader] = useState(false)
+  const { onError } = useContext(ViewContext)
 
   // eslint-disable-next-line no-extra-semi
   ;(async function asyncLoad(): Promise<undefined> {
@@ -49,12 +49,12 @@ export default function useLoadImage({
          * https://github.com/sindresorhus/ky/blob/main/source/errors/HTTPError.ts
          * { error: { name: 'HTTPError', options, request, response, message, stack }
          */
-        errorMessage.sendMessage(e?.error?.message || 'An error has occured!')
-        handleError(e)
+        onError(e?.error?.message || 'An error has occured!')
       }
     }
 
     loadImage()
-  }, [imageId, element, hasLoader])
+    return undefined
+  }, [imageId, element, hasLoader, onError])
   return hasLoader
 }
