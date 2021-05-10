4ec20f2d087d80f816862681f311210fc71bc2d7
skpark-lunit <skpark@lunit.io>
2021-05-10 17:22:00 +0900

    feat: Add http-header demo page

--- apps/insight-viewer-demo/src/components/Nav/const.ts
+++ apps/insight-viewer-demo/src/components/Nav/const.ts
@@ -2,4 +2,5 @@ export const LINKS = [
   { href: 'basic', name: 'Basic' },
   { href: 'error', name: 'Error' },
   { href: 'progress', name: 'Progress' },
+  { href: 'http-header', name: 'Http header' },
 ]
---
+++ apps/insight-viewer-demo/src/containers/HttpHeader/WithCookie.tsx
@@ -0,0 +1,42 @@
+import { Box, Heading } from '@chakra-ui/react'
+import useInsightViewer from '@lunit/insight-viewer'
+import CodeBlock from '../../components/CodeBlock'
+
+const Code = `\
+import useInsightViewer from '@lunit/insight-viewer'
+
+const { DICOMImageViewer } = useInsightViewer({
+  setHeader: () => {
+    document.cookie =
+      'authToken=test; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/'
+  },
+})
+
+export default function() {
+  return <DICOMImageViewer imageId={IMAGE_ID} />
+}
+`
+const IMAGE_ID = 'wadouri:http://localhost:3000/api/with-cookie'
+
+export default function WithCookie(): JSX.Element {
+  const { DICOMImageViewer } = useInsightViewer({
+    setHeader: () => {
+      document.cookie =
+        'authToken=test; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/'
+    },
+  })
+
+  return (
+    <>
+      <Box mb={6}>
+        <Heading as="h4">cookie header</Heading>
+      </Box>
+      <Box w={700}>
+        <Box w={500} h={500}>
+          <DICOMImageViewer imageId={IMAGE_ID} />
+        </Box>
+        <CodeBlock code={Code} />
+      </Box>
+    </>
+  )
+}
