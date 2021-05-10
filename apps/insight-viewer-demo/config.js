96dfa6ff162c031006bb370105dd8dcff96bf317
skpark-lunit <skpark@lunit.io>
2021-05-10 18:23:10 +0900

    feat: Do not show http-header page which only works in development env

--- apps/insight-viewer-demo/config.js
+++ apps/insight-viewer-demo/config.js
@@ -1,5 +1,6 @@
 const config = {
   HOST: process.env.HOST || 'production',
+  IS_DEV: process.env.NODE_ENV === 'development',
 }
 
 export default config
--- apps/insight-viewer-demo/src/components/Nav/const.ts
+++ apps/insight-viewer-demo/src/components/Nav/const.ts
@@ -1,6 +1,12 @@
+import config from '../../../config'
+
+function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  return value !== null && value !== undefined
+}
+
 export const LINKS = [
   { href: 'basic', name: 'Basic' },
   { href: 'error', name: 'Error' },
   { href: 'progress', name: 'Progress' },
-  { href: 'http-header', name: 'Http header' },
-]
+  !config.IS_DEV ? undefined : { href: 'http-header', name: 'Http header' },
+].filter(notEmpty)
