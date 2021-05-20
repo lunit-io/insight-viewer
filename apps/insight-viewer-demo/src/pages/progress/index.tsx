c144c4c024a9a8a66ce359360304a3de24087543
skpark-lunit <skpark@lunit.io>
2021-05-20 09:50:27 +0900

    doc: Convert page directory to file

--- apps/insight-viewer-demo/src/pages/progress/index.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('../../containers/Progress'),
-  { ssr: false }
-)
-
-export default DynamicComponentWithNoSSR
