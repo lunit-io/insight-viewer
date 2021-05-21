4fcf278322063ee5acfa8f7035b91949d0660ce1
skpark-lunit <skpark@lunit.io>
2021-05-21 11:41:15 +0900

    feat: Add Viewport context

---
+++ packages/insight-viewer/src/Context/Viewport.tsx
@@ -0,0 +1,25 @@
+import React, { createContext } from 'react'
+import { WithChildren, Viewport } from '../types'
+
+export const ViewrportContextDefaultValue: Viewport = {
+  invert: false,
+  hflip: false,
+  vflip: false,
+}
+
+const ViewportContext = createContext<Viewport>(ViewrportContextDefaultValue)
+
+export function ViewportContextProvider({
+  invert = ViewrportContextDefaultValue.invert,
+  hflip = ViewrportContextDefaultValue.hflip,
+  vflip = ViewrportContextDefaultValue.vflip,
+  children,
+}: WithChildren<Viewport>): JSX.Element {
+  return (
+    <ViewportContext.Provider value={{ invert, hflip, vflip }}>
+      {children}
+    </ViewportContext.Provider>
+  )
+}
+
+export default ViewportContext
