3bcd3f7a3f909e65e1ddf77db4de8ec5f7e5e686
skpark-lunit <skpark@lunit.io>
2021-05-21 11:34:52 +0900

    feat: Add viewport type

--- packages/insight-viewer/src/Context/types.ts
+++ packages/insight-viewer/src/Context/types.ts
@@ -1,17 +1,7 @@
-import { Dispatch, SetStateAction } from 'react'
 import { OnError, Progress, SetHeader } from '../types'
-// import { CornerstoneEventData } from '../utils/cornerstoneHelper/types'
 
 export interface ContextProp {
   onError: OnError
   Progress: Progress
   setHeader: SetHeader
-}
-
-export interface ViewrportContexState {
-  invert: boolean
-}
-
-export type ViewrportContextProp = ViewrportContexState & {
-  setViewportContext: Dispatch<SetStateAction<ViewrportContexState>>
 }
