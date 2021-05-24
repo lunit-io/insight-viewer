9965d5b1f3242d83df3e4b72da78fd4938bd311d
skpark-lunit <skpark@lunit.io>
2021-05-24 19:49:40 +0900

    doc: Update viewport demo

--- apps/insight-viewer-demo/src/containers/Viewport/index.tsx
+++ apps/insight-viewer-demo/src/containers/Viewport/index.tsx
@@ -1,15 +1,33 @@
 import { Box, Heading, HStack, Switch } from '@chakra-ui/react'
 import useInsightViewer from '@lunit/insight-viewer'
+import CodeBlock from '../../components/CodeBlock'
 
 const IMAGE_ID =
   'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm'
 
+const Code = `\
+  import useInsightViewer from '@lunit/insight-viewer'
+  
+  export default function Viewer() {
+    const { DICOMImageViewer, setViewport } = useInsightViewer()
+
+    function handleViewport() {
+      setViewport('invert', true)
+      setViewport('hflip', false)
+      setViewport('vflip', true)
+      setViewport('x', 0)
+      setViewport('y', 10)
+      setViewport('scale', 1)
+      setViewport('windowWidth', 128)
+      setViewport('windowCenter', 256)
+    }
+  
+    return <DICOMImageViewer imageId={IMAGE_ID} />
+  }
+  `
+
 function Viewport(): JSX.Element {
-  const { DICOMImageViewer, useViewport } = useInsightViewer()
-  const {
-    viewport: { invert, hflip, vflip },
-    setViewport,
-  } = useViewport()
+  const { DICOMImageViewer, setViewport } = useInsightViewer()
 
   return (
     <>
@@ -21,21 +39,112 @@ function Viewport(): JSX.Element {
         <Box mb={6}>
           <HStack spacing="24px">
             <Box>
-              Invert <Switch onChange={() => setViewport('invert', !invert)} />
+              Invert{' '}
+              <Switch onChange={e => setViewport('invert', e.target.checked)} />
             </Box>
             <Box>
-              Hflip <Switch onChange={() => setViewport('hflip', !hflip)} />
+              Hflip{' '}
+              <Switch onChange={e => setViewport('hflip', e.target.checked)} />
             </Box>
             <Box>
-              vflip <Switch onChange={() => setViewport('vflip', !vflip)} />
+              vflip{' '}
+              <Switch onChange={e => setViewport('vflip', e.target.checked)} />
+            </Box>
+          </HStack>
+          <HStack spacing="24px" mt={3}>
+            <Box>
+              <Box>x transition</Box>
+              <Box>
+                <input
+                  type="range"
+                  id="x"
+                  name="x"
+                  min="0"
+                  max="100"
+                  step="10"
+                  defaultValue={0}
+                  onChange={e => {
+                    setViewport('x', Number(e.target.value))
+                  }}
+                />
+              </Box>
+            </Box>
+            <Box>
+              <Box>y transition</Box>
+              <Box>
+                <input
+                  type="range"
+                  id="y"
+                  name="y"
+                  min="0"
+                  max="100"
+                  step="10"
+                  defaultValue={0}
+                  onChange={e => {
+                    setViewport('y', Number(e.target.value))
+                  }}
+                />
+              </Box>
+            </Box>
+          </HStack>
+
+          <Box>zoom</Box>
+          <Box>
+            <input
+              type="range"
+              id="scale"
+              name="scale"
+              min="1"
+              max="2"
+              step="0.1"
+              defaultValue={1}
+              onChange={e => {
+                setViewport('scale', Number(e.target.value))
+              }}
+            />
+          </Box>
+          <HStack spacing="24px" mt={3}>
+            <Box>
+              <Box>windowWidth</Box>
+              <Box>
+                <input
+                  type="range"
+                  id="windowWidth"
+                  name="windowWidth"
+                  min="0"
+                  max="256"
+                  step="25.6"
+                  defaultValue={128}
+                  onChange={e => {
+                    setViewport('windowWidth', Number(e.target.value))
+                  }}
+                />
+              </Box>
+            </Box>
+            <Box>
+              <Box>windowCenter</Box>
+              <Box>
+                <input
+                  type="range"
+                  id="windowCenter"
+                  name="windowCenter"
+                  min="0"
+                  max="256"
+                  step="25.6"
+                  defaultValue={128}
+                  onChange={e => {
+                    setViewport('windowCenter', Number(e.target.value))
+                  }}
+                />
+              </Box>
             </Box>
           </HStack>
         </Box>
         <Box w={500} h={500}>
-          <DICOMImageViewer
-            imageId={IMAGE_ID}
-            viewport={{ invert, hflip, vflip }}
-          />
+          <DICOMImageViewer imageId={IMAGE_ID} />
+        </Box>
+        <Box w={700}>
+          <CodeBlock code={Code} />
         </Box>
       </Box>
     </>
