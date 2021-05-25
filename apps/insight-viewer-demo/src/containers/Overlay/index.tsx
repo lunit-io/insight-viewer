81ddeb27e531caf30afae99f917764f95979fcee
skpark-lunit <skpark@lunit.io>
2021-05-25 16:14:22 +0900

    doc: Add overlay demo

---
+++ apps/insight-viewer-demo/src/containers/Overlay/index.tsx
@@ -0,0 +1,100 @@
+import { Box, Heading, UnorderedList, ListItem } from '@chakra-ui/react'
+import useInsightViewer, { Viewport } from '@lunit/insight-viewer'
+import CodeBlock from '../../components/CodeBlock'
+import { WithChildren } from '../../types'
+
+const IMAGE_ID =
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm'
+
+const Code = `\
+  import useInsightViewer, { Viewport } from '@lunit/insight-viewer'
+
+  function Nested({ viewport }: {viewport: Viewport }) {
+    return (
+      <ul>
+        <li>scale: {viewport?.scale}</li>
+        <li>
+          hflip/vflip: {viewport?.hflip} / {viewport?.vflip}
+        </li>
+        <li>
+          translation: {viewport?.translation?.x} / {viewport?.translation?.y}
+        </li>
+        <li>invert: {viewport?.invert}</li>
+        <li>
+          voi: {viewport?.voi?.windowWidth} / {viewport?.voi?.windowCenter}
+        </li>
+      </ul>
+    )
+  }
+
+  export default function Viewer() {
+    const { DICOMImageViewer, ViewportConsumer } = useInsightViewer()
+
+    return (
+      <DICOMImageViewer imageId={IMAGE_ID}>
+        <ViewportConsumer>
+          {viewport => <Nested viewport={viewport} />}
+        </ViewportConsumer>
+      </DICOMImageViewer>
+    )
+  }
+  `
+
+function Nested({
+  viewport,
+}: WithChildren<{ viewport?: Viewport }>): JSX.Element {
+  return (
+    <Box
+      position="absolute"
+      top="0"
+      left="0"
+      w="100%"
+      h="100%"
+      p="4"
+      bg="rgba(255, 255, 255, .3)"
+      color="blue.200"
+      textShadow="1px 1px 1px black"
+    >
+      <UnorderedList>
+        <ListItem>scale: {viewport?.scale}</ListItem>
+        <ListItem>
+          hflip/vflip: {`${viewport?.hflip}`} / {`${viewport?.vflip}`}
+        </ListItem>
+        <ListItem>
+          translation: {viewport?.translation?.x} / {viewport?.translation?.y}
+        </ListItem>
+        <ListItem>invert: {`${viewport?.invert}`}</ListItem>
+        <ListItem>
+          voi: {viewport?.voi?.windowWidth} / {viewport?.voi?.windowCenter}
+        </ListItem>
+      </UnorderedList>
+    </Box>
+  )
+}
+
+function Overlay(): JSX.Element {
+  const { DICOMImageViewer, ViewportConsumer } = useInsightViewer()
+
+  return (
+    <>
+      <Box mb={6}>
+        <Heading as="h3">Overlay</Heading>
+      </Box>
+
+      <Box w={700}>
+        <Box w={500} h={500}>
+          <DICOMImageViewer imageId={IMAGE_ID}>
+            <ViewportConsumer>
+              {viewport => <Nested viewport={viewport} />}
+            </ViewportConsumer>
+          </DICOMImageViewer>
+        </Box>
+        <Box w={700}>
+          <CodeBlock code={Code} />
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default Overlay
---
+++ apps/insight-viewer-demo/src/pages/overlay.tsx
@@ -0,0 +1,8 @@
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../containers/Overlay'),
+  { ssr: false }
+)
+
+export default DynamicComponentWithNoSSR
