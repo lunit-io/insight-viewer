a761d1ab1e74f3205bcef8cc4bf4b4fa75e49ac0
skpark-lunit <skpark@lunit.io>
2021-05-25 20:41:46 +0900

    doc: Expose all demo pages

--- apps/insight-viewer-demo/src/containers/HttpHeader/index.tsx
+++ apps/insight-viewer-demo/src/containers/HttpHeader/index.tsx
@@ -1,7 +1,6 @@
 import { Box, Heading } from '@chakra-ui/react'
 import WithCookie from './WithCookie'
 import WithJwt from './WithJwt'
-import config from '../../../config'
 
 function Basic(): JSX.Element {
   return (
@@ -9,7 +8,7 @@ function Basic(): JSX.Element {
       <Box mb={6}>
         <Heading as="h3">Http header</Heading>
       </Box>
-      {config.IS_DEV && <WithCookie />}
+      <WithCookie />
       <WithJwt />
     </>
   )
--- apps/insight-viewer-demo/src/containers/MultiFrame/Tabs.tsx
+++ apps/insight-viewer-demo/src/containers/MultiFrame/Tabs.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Base from './Base'
 import NoContentLength from './NoContentLength'
-import config from '../../../config'
 
 export default function ProgressTabs(): JSX.Element {
   const [active, setActive] = useState(0)
@@ -15,14 +14,12 @@ export default function ProgressTabs(): JSX.Element {
     <Tabs isLazy onChange={handleChange}>
       <TabList>
         <Tab>Default Multiframe</Tab>
-        {config.IS_DEV && <Tab>Multiframe with no content-length</Tab>}
+        <Tab>Multiframe with no content-length</Tab>
       </TabList>
 
       <TabPanels>
         <TabPanel>{active === 0 && <Base />}</TabPanel>
-        {config.IS_DEV && (
-          <TabPanel>{active === 1 && <NoContentLength />}</TabPanel>
-        )}
+        <TabPanel>{active === 1 && <NoContentLength />}</TabPanel>
       </TabPanels>
     </Tabs>
   )
