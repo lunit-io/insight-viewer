f6871f553f5d45a4a178f16188cb3bd00e76b67f
skpark-lunit <skpark@lunit.io>
2021-06-02 12:08:36 +0900

    doc: Delete cookie set header demo

--- apps/insight-viewer-demo/src/mocks/handlers.ts
+++ apps/insight-viewer-demo/src/mocks/handlers.ts
@@ -1,24 +1,6 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.get('/api/with-cookie', async (req, res, ctx) => {
-    const { authToken } = req.cookies
-    const imageBuffer = await fetch(
-      'https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm'
-    ).then(r => r.arrayBuffer())
-
-    if (!authToken) {
-      return res(
-        ctx.status(403),
-        ctx.json({ message: 'Failed to authenticate!' })
-      )
-    }
-    return res(
-      ctx.set('Content-Length', imageBuffer.byteLength.toString()),
-      // Respond with the "ArrayBuffer".
-      ctx.body(imageBuffer)
-    )
-  }),
   rest.get('/api/with-jwt', async (req, res, ctx) => {
     const authHeader = req.headers.get('Authorization')
     const imageBuffer = await fetch(
