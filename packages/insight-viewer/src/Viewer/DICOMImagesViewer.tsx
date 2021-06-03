02b3c1287d2d69add1a94ec2764b0b757dfc5ac3
skpark-lunit <skpark@lunit.io>
2021-06-03 14:11:16 +0900

    feat: Update DICOMImagesViewer to receive imageIds props instead of imageId

--- packages/insight-viewer/src/Viewer/DICOMImagesViewer.tsx
+++ packages/insight-viewer/src/Viewer/DICOMImagesViewer.tsx
@@ -2,16 +2,20 @@ import React, { useRef } from 'react'
 import ViewerWrapper from '../components/ViewerWrapper'
 import { WithChildren } from '../types'
 import useDICOMImageLoader from '../hooks/useDICOMImageLoader'
+import usePrefetch from '../hooks/usePrefetch'
+import useFrame from '../hooks/useFrame'
 import { VIEWER_TYPE } from '../const'
 
 export function DICOMImagesViewer({
-  imageId,
+  imageIds,
+  initial,
   children,
-}: WithChildren<{ imageId: string }>): JSX.Element {
+}: WithChildren<{ imageIds: string[]; initial: number }>): JSX.Element {
   const elRef = useRef<HTMLDivElement>(null)
-
+  usePrefetch(imageIds)
+  useFrame(elRef.current, imageIds)
   useDICOMImageLoader({
-    imageId,
+    imageId: imageIds[initial],
     element: elRef.current,
     isSingleImage: false,
   })
