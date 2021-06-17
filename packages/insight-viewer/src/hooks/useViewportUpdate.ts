3aaf73b3b79772ee9018c478ab4c7d4016c8f22f
skpark-lunit <skpark@lunit.io>
2021-06-17 10:02:31 +0900

    refactor: Rename useViewportUpdate hook which update cornerstone viewport

---
+++ packages/insight-viewer/src/hooks/useViewportUpdate.ts
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { Subscription } from 'rxjs'
+import {
+  getViewport,
+  setViewport,
+  CornerstoneViewport,
+} from '../utils/cornerstoneHelper'
+import { viewportMessage } from '../utils/messageService'
+import { formatCornerstoneViewport } from '../utils/common/formatViewport'
+import { Element } from '../types'
+import { Viewport } from '../Context/Viewport/types'
+
+let subscription: Subscription
+
+export default function useViewportUpdate(element: Element): void {
+  useEffect(() => {
+    if (!element) return undefined
+
+    subscription = viewportMessage
+      .getMessage()
+
+      .subscribe((message: Partial<Viewport>) => {
+        const viewport = getViewport(
+          <HTMLDivElement>element
+        ) as CornerstoneViewport
+
+        if (viewport)
+          setViewport(
+            <HTMLDivElement>element,
+            formatCornerstoneViewport(viewport, message)
+          )
+      })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [element])
+}
