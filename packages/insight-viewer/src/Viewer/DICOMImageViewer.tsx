510ed8ad201ee8d28c87f494096b4dee55499883
skpark-lunit <skpark@lunit.io>
2021-06-17 10:09:57 +0900

    feat: Update Dicom Viewer to pass setViewport prop to useImageLoader

--- packages/insight-viewer/src/Viewer/DICOMImageViewer.tsx
+++ packages/insight-viewer/src/Viewer/DICOMImageViewer.tsx
@@ -1,9 +1,16 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import ViewerWrapper from '../components/ViewerWrapper'
-import { WithChildren, ViewerProp, ProgressComponent } from '../types'
+import {
+  WithChildren,
+  ViewerProp,
+  ProgressComponent,
+  SetViewport,
+} from '../types'
+import { Viewport } from '../Context/Viewport/types'
 import useCornerstone from '../hooks/useCornerstone'
 import useImageLoader from '../hooks/useImageLoader'
-import { setWadoImageLoader } from '../utils/cornerstoneHelper'
+import setWadoImageLoader from '../utils/cornerstoneHelper/setWadoImageLoader'
+import { viewportMessage } from '../utils/messageService/viewport'
 import { DefaultProp } from './const'
 
 export function DICOMImageViewer({
@@ -11,22 +18,31 @@ export function DICOMImageViewer({
   onError = DefaultProp.onError,
   Progress = DefaultProp.Progress,
   requestInterceptor = DefaultProp.requestInterceptor,
+  viewport,
+  setViewport,
   children,
 }: WithChildren<
   ViewerProp & {
     Progress?: ProgressComponent
+    viewport?: Viewport
+    setViewport?: SetViewport
   }
 >): JSX.Element {
   const elRef = useRef<HTMLDivElement>(null)
   useCornerstone(elRef.current)
-  // TODO: 불필요한 매개변수 제거.
+
   useImageLoader({
     imageId,
     element: elRef.current,
     onError,
     requestInterceptor,
     setLoader: () => setWadoImageLoader(onError),
+    setViewport,
   })
+
+  useEffect(() => {
+    if (viewport) viewportMessage.sendMessage(viewport)
+  }, [viewport])
 
   return (
     <ViewerWrapper ref={elRef} Progress={Progress}>
