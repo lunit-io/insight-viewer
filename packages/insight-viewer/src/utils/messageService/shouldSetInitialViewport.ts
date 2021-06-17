8132f7542a443d95ddb23365742dcf90dbd9cbcf
skpark-lunit <skpark@lunit.io>
2021-06-17 21:46:04 +0900

    feat: Delete shouldSetInitialViewport

--- packages/insight-viewer/src/utils/messageService/shouldSetInitialViewport.ts
+++
@@ -1,8 +0,0 @@
-import { Observable, Subject } from 'rxjs'
-
-const subject = new Subject<boolean>()
-
-export const shouldSetInitialViewportMessage = {
-  sendMessage: (message: boolean): void => subject.next(message),
-  getMessage: (): Observable<boolean> => subject.asObservable(),
-}
