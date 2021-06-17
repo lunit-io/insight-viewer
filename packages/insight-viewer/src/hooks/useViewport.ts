fe7e2e66e3ff8c6e433bf691dc5ced58aa25f06a
skpark-lunit <skpark@lunit.io>
2021-06-17 10:27:17 +0900

    feat: Update useViewport initial value

--- packages/insight-viewer/src/hooks/useViewport.ts
+++ packages/insight-viewer/src/hooks/useViewport.ts
@@ -3,13 +3,15 @@ import { Viewport } from '../Context/Viewport/types'
 import { ViewportContextDefaultValue } from '../Context/Viewport/const'
 
 export default function useViewport(
-  initial?: Viewport
+  initial?: Partial<Viewport>
 ): {
   viewport: Viewport
   setViewport: React.Dispatch<React.SetStateAction<Viewport>>
 } {
   const [viewport, setViewport] = useState(
-    initial ?? ViewportContextDefaultValue
+    initial
+      ? { ...ViewportContextDefaultValue, ...initial }
+      : ViewportContextDefaultValue
   )
 
   return {
