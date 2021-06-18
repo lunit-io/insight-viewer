3c191e3fae17b8e0727e1fced22a6694303ae01d
skpark-lunit <skpark@lunit.io>
2021-06-18 10:25:00 +0900

    chore: Delete build process from pre-commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,24 +1,14 @@
 const { ESLint } = require('eslint')
-const filterAsync = require('node-filter-async').default
 
 const eslintCli = new ESLint()
 
-const removeIgnoredFiles = async (files) => {
-  const filteredFiles = await filterAsync(files, async (file) => {
-    const isIgnored = await eslintCli.isPathIgnored(file)
-    return !isIgnored
-  })
-  return filteredFiles.join(' ')
-}
-
 module.exports = {
-  '{apps,packages}/**/*.ts?(x)': async (files) => {
-    const filesToLint = await removeIgnoredFiles(files)
-    return [
-      'yarn run tsc --p tsconfig.lint-staged.json',
-      `yarn run eslint --max-warnings=0 --cache ${filesToLint}`,
-      `yarn run prettier --write "**/*.ts?(x)"`,
-    ]
-  },
-  '{apps,packages}/**/*.js?(x)': ['yarn run eslint --cache', 'yarn run prettier --write'],
+  '{apps,packages}/**/*.ts?(x)': [
+    'yarn run eslint --max-warnings=0 --cache --debug',
+    'yarn run prettier --write "**/*.ts?(x)"',
+  ],
+  '{apps,packages}/**/*.js?(x)': [
+    'yarn run eslint --cache',
+    'yarn run prettier --write',
+  ],
 }
