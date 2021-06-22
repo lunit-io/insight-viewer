524c195934bcd661214c687b9a3d3036196ef279
skpark-lunit <skpark@lunit.io>
2021-06-22 10:39:21 +0900

    feat: Export useInteraction hook and types

--- packages/insight-viewer/src/index.ts
+++ packages/insight-viewer/src/index.ts
@@ -1,5 +1,12 @@
 export { default } from './Viewer'
 export { default as useMultiframe } from './hooks/useMultiframe'
 export { default as useViewport } from './hooks/useViewport'
+export { default as useInteraction } from './hooks/useInteraction'
 export type { Viewport } from './Context/Viewport/types'
+export type {
+  Interaction,
+  SetInteraction,
+  MouseDownMove,
+  MouseWheel,
+} from './hooks/useInteraction/types'
 export type { ViewerError } from './types'
