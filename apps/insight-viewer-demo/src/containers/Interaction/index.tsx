b8e2155d9e7d5f8011ea6e500a70744d51cb764e
skpark-lunit <skpark@lunit.io>
2021-06-22 10:45:54 +0900

    doc: Update interaction demo to implement useInteraction hook

--- apps/insight-viewer-demo/src/containers/Interaction/index.tsx
+++ apps/insight-viewer-demo/src/containers/Interaction/index.tsx
@@ -1,34 +1,48 @@
 import { Box } from '@chakra-ui/react'
-import Viewer, { useViewport } from '@lunit/insight-viewer'
+import Viewer, { useInteraction } from '@lunit/insight-viewer'
 import CodeBlock from '../../components/CodeBlock'
-import OverlayLayer from '../Overlay/OverlayLayer'
+import Control from './Control'
 
 const IMAGE_ID =
   'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm'
 
 const Code = `\
-  import Viewer, { useViewport } from '@lunit/insight-viewer'
+  import Viewer, { useInteraction, Interaction } from '@lunit/insight-viewer'
 
   export default function Viewer() {
-    const { viewport } = useViewport()
+    const { interaction, setInteraction } = useInteraction()
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+      const v = e.target.value
+
+      setInteraction((prev: Interaction) => ({
+        ...prev,
+        mouseDownMove: v === 'none' ? undefined : v,
+      }))
+    }
 
     return (
       <Viewer.Dicom imageId={IMAGE_ID}>
-        <OverlayLayer viewport={viewport} />
+        <OverlayLayer 
+          viewport={viewport}
+          interaction={interaction}
+        />
+        <input type="radio" value="none" onChange={handleChange} />
+        <input type="radio" value="pan" onChange={handleChange}  />
+        <input type="radio" value="adjust" onChange={handleChange}  />
       </Viewer.Dicom>
     )
   }
   `
 
 function Interaction(): JSX.Element {
-  const { viewport } = useViewport()
+  const { interaction, setInteraction } = useInteraction()
 
   return (
     <Box w={700}>
+      <Control setInteraction={setInteraction} />
       <Box w={500} h={500}>
-        <Viewer.Dicom imageId={IMAGE_ID}>
-          <OverlayLayer viewport={viewport} />
-        </Viewer.Dicom>
+        <Viewer.Dicom imageId={IMAGE_ID} interaction={interaction} />
       </Box>
       <Box w={900}>
         <CodeBlock code={Code} />
