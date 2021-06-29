f35a0debb1a5c66ab71b503ff90cf5e1c2e749db
skpark-lunit <skpark@lunit.io>
2021-06-29 15:59:50 +0900

    feat: Add updateViewport to Drag type

--- packages/insight-viewer/src/hooks/useInteraction/types.ts
+++ packages/insight-viewer/src/hooks/useInteraction/types.ts
@@ -1,11 +1,22 @@
 import { DRAG, MOUSE_WHEEL, EVENT } from './const'
 import { CornerstoneViewport } from '../../utils/cornerstoneHelper'
 
+export type DragEvent = keyof typeof DRAG
+export type WheelEvent = keyof typeof MOUSE_WHEEL
+
 export interface Coord {
   x: number
   y: number
 }
-export type Drag = (viewport: CornerstoneViewport, delta: Coord) => void
+export type Drag = ({
+  viewport,
+  delta,
+  updateViewport,
+}: {
+  viewport: CornerstoneViewport
+  delta: Coord
+  updateViewport: (e?: DragEvent) => void
+}) => void
 export type Pan = (viewport: CornerstoneViewport, delta: Coord) => Coord
 export type Adjust = (
   viewport: CornerstoneViewport,
@@ -14,12 +25,9 @@ export type Adjust = (
   windowWidth: number
   windowCenter: number
 }
-export type DragEvent = keyof typeof DRAG
-export type WheelEvent = keyof typeof MOUSE_WHEEL
-
 export interface DragInteraction {
-  [EVENT.primaryDrag]: DragEvent | Pan | undefined
-  [EVENT.secondaryDrag]: DragEvent | Adjust | undefined
+  [EVENT.primaryDrag]: DragEvent | Drag | undefined
+  [EVENT.secondaryDrag]: DragEvent | Drag | undefined
 }
 
 export interface WheelInteraction {
