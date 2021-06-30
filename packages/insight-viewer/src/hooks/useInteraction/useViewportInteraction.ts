17394dda18793c31ac39ac32db7b1036d93973ed
skpark-lunit <skpark@lunit.io>
2021-06-30 19:44:09 +0900

    feat: Use useHandleWheel hook

--- packages/insight-viewer/src/hooks/useInteraction/useViewportInteraction.ts
+++ packages/insight-viewer/src/hooks/useInteraction/useViewportInteraction.ts
@@ -1,13 +1,21 @@
 import { ViewportInteraction } from './types'
 import useHandleDrag from './useHandleDrag'
 import useHandleClick from './useHandleClick'
+import useHandleWheel from './useHandleWheel'
 
 export default function useViewportInteraction({
   element,
   interaction,
   viewport,
   onViewportChange,
+  onFrameChange,
 }: ViewportInteraction): void {
+  useHandleWheel({
+    element,
+    interaction,
+    onFrameChange,
+  })
+
   useHandleDrag({
     element,
     interaction,
