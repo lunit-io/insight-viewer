48345cf8e4049f4fd705f75e133fdd1cf116c294
skpark-lunit <skpark@lunit.io>
2021-07-05 14:05:11 +0900

    chore: Update doc export path

--- apps/insight-viewer-demo/next.config.js
+++ apps/insight-viewer-demo/next.config.js
@@ -1,7 +1,5 @@
 const { join } = require('path')
 
-const workspace = join(__dirname, '../..')
-
 module.exports = {
   assetPrefix: process.env.HOST,
   trailingSlash: true,
@@ -13,7 +11,7 @@ module.exports = {
   // You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file.
   // 참고: https://webman.pro/blog/how-to-setup-typescript-path-aliases-in-lerna-monorepo/
   // https://medium.com/@NiGhTTraX/making-typescript-monorepos-play-nice-with-other-tools-a8d197fdc680
-  webpack: (config, options) => {
+  webpack: config => {
     // Let Babel compile outside of src/
     const tsRule = config.module.rules.find(
       rule => rule.test && rule.test.toString().includes('tsx|ts')
