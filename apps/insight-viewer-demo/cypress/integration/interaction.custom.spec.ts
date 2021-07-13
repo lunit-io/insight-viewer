833a1a96a66eb9d0c7f7a9e35cd9854d4750512b
skpark-lunit <skpark@lunit.io>
2021-07-13 12:22:54 +0900

    test: Add custom interaction spec

---
+++ apps/insight-viewer-demo/cypress/integration/interaction.custom.spec.ts
@@ -0,0 +1,202 @@
+import '@percy/cypress'
+import { setup } from '../support/utils'
+import { DEFAULT_SCALE } from '../../src/containers/Interaction/const'
+
+describe('Interaction', () => {
+  before(() => {
+    setup()
+  })
+
+  beforeEach(() => {
+    cy.visit('/interaction')
+    cy.get('.custom-tab').click()
+  })
+
+  describe('Custom Interaction', () => {
+    describe('Primary Drag', () => {
+      it('pan', () => {
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.primary-drag-pan').click()
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: 200,
+              y: -50,
+              button: 0,
+            })
+          })
+
+        cy.percySnapshot()
+        cy.wait(1000)
+      })
+
+      it('adjust', () => {
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.primary-drag-adjust').click()
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: -100,
+              y: 250,
+              button: 0,
+            })
+          })
+
+        cy.percySnapshot()
+        cy.wait(1000)
+      })
+
+      it('none', () => {
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.primary-drag-none').click()
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: 250,
+              y: 350,
+              button: 0,
+            })
+          })
+
+        cy.percySnapshot()
+        cy.wait(1000)
+      })
+    })
+
+    describe('Secondary Drag', () => {
+      it('pan', () => {
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.secondary-drag-pan').click()
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: 50,
+              y: 180,
+              button: 2,
+            })
+          })
+
+        cy.percySnapshot()
+        cy.wait(1000)
+      })
+
+      it('adjust', () => {
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.secondary-drag-adjust').click()
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: 250,
+              y: 400,
+              button: 2,
+            })
+          })
+
+        cy.percySnapshot()
+        cy.wait(1000)
+      })
+
+      it('none', () => {
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.secondary-drag-none').click()
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: -50,
+              y: -30,
+              button: 0,
+            })
+          })
+
+        cy.percySnapshot()
+        cy.wait(1000)
+      })
+    })
+
+    describe('Primary Click', () => {
+      const CLIENT_X = 400
+      const CLIENT_Y = 610
+
+      it('normal click', { scrollBehavior: false }, () => {
+        cy.get('.primary-click').click()
+
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.cornerstone-canvas-wrapper').mouseclick({
+              x: CLIENT_X,
+              y: CLIENT_Y,
+              button: 0,
+            })
+            cy.percySnapshot()
+            cy.wait(1000)
+          })
+      })
+
+      it('click after padding', { scrollBehavior: false }, () => {
+        cy.get('.primary-drag-pan').click() // for dragging
+        cy.get('.primary-click').click()
+
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: 140,
+              y: 30,
+              button: 0,
+            })
+            cy.get('.cornerstone-canvas-wrapper').mouseclick({
+              x: CLIENT_X,
+              y: CLIENT_Y,
+              button: 0,
+            })
+            cy.percySnapshot()
+            cy.wait(1000)
+          })
+      })
+    })
+
+    describe('Secondary Click', () => {
+      const CLIENT_X = 849
+      const CLIENT_Y = 838
+
+      it('normal click', { scrollBehavior: false }, () => {
+        cy.get('.primary-click').click()
+
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.cornerstone-canvas-wrapper').mouseclick({
+              x: CLIENT_X,
+              y: CLIENT_Y,
+              button: 0,
+            })
+            cy.percySnapshot()
+            cy.wait(1000)
+          })
+      })
+
+      it('click after padding', { scrollBehavior: false }, () => {
+        cy.get('.primary-drag-pan').click() // for dragging
+        cy.get('.primary-click').click()
+
+        cy.get('.scale')
+          .contains(DEFAULT_SCALE)
+          .then(() => {
+            cy.get('.cornerstone-canvas-wrapper').dragCanvas({
+              x: -140,
+              y: -60,
+              button: 0,
+            })
+            cy.get('.cornerstone-canvas-wrapper').mouseclick({
+              x: CLIENT_X,
+              y: CLIENT_Y,
+              button: 0,
+            })
+            cy.percySnapshot()
+            cy.wait(1000)
+          })
+      })
+    })
+  })
+})
