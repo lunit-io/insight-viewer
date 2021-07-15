bcef64381fd0779e70d8926164d18e8ec6cde092
skpark-lunit <skpark@lunit.io>
2021-07-15 17:09:21 +0900

    test: Use controlledInputChange custom Cypress command

--- apps/insight-viewer-demo/cypress/integration/multiframe.spec.ts
+++ apps/insight-viewer-demo/cypress/integration/multiframe.spec.ts
@@ -1,27 +1,29 @@
 import '@percy/cypress'
 import { setup } from '../support/utils'
 
-describe('Multiframe', () => {
+describe('Multiframe', { scrollBehavior: false }, () => {
   before(() => {
     setup()
     cy.visit('/multi-frame')
   })
 
+  beforeEach(() => {
+    cy.wait(1000)
+  })
+
   it('shows initial viewer', () => {
-    cy.percySnapshot()
+    cy.get('.is-mount').then(() => {
+      cy.percySnapshot()
+    })
   })
 
   it('change frame to 5', () => {
-    cy.get('.is-mount').then(() => {
-      cy.get('.frame').invoke('val', 5).trigger('change')
-      cy.percySnapshot()
-    })
+    cy.get('input[type="range"]').controlledInputChange(5)
+    cy.percySnapshot()
   })
 
   it('change frame to 10', () => {
-    cy.get('.is-mount').then(() => {
-      cy.get('.frame').invoke('val', 10).trigger('change')
-      cy.percySnapshot()
-    })
+    cy.get('input[type="range"]').controlledInputChange(10)
+    cy.percySnapshot()
   })
 })
