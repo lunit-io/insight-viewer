74b46cfaaa92e4fe59062d8c01b1238021c7cc9d
skpark-lunit <skpark@lunit.io>
2021-07-16 15:31:51 +0900

    test: Screen capture after resizng

--- apps/insight-viewer-demo/cypress/integration/basic.spec.ts
+++ apps/insight-viewer-demo/cypress/integration/basic.spec.ts
@@ -1,29 +1,37 @@
 import '@percy/cypress'
 import { setup } from '../support/utils'
+import { VIEWPORT_WIDTH, VIEWPORT_HEIGHT } from '../support/const'
 
-describe('Basic Viewer', () => {
-  before(() => {
-    cy.visit('/')
-    setup()
-  })
+describe(
+  'Basic Viewer',
+  {
+    viewportWidth: VIEWPORT_WIDTH,
+    viewportHeight: VIEWPORT_HEIGHT,
+    scrollBehavior: false,
+  },
+  () => {
+    before(() => {
+      cy.visit('/')
+      setup()
+    })
 
-  beforeEach(() => {
-    cy.wait(1000)
-  })
+    it('shows initial image', () => {
+      cy.get('.is-mount').then(() => {
+        cy.get('.image').should('have.text', 'image1')
+        cy.percySnapshot()
+      })
+    })
 
-  it('shows initial image', () => {
-    cy.get('.is-mount').then(() => {
+    it('shows second image', () => {
+      cy.get('.button2').click()
+      cy.get('.image').should('have.text', 'image2')
       cy.percySnapshot()
     })
-  })
 
-  it('shows second image', () => {
-    cy.get('.button2').click()
-    cy.percySnapshot()
-  })
-
-  it('shows third image', () => {
-    cy.get('.button3').click()
-    cy.percySnapshot()
-  })
-})
+    it('shows third image', () => {
+      cy.get('.button3').click()
+      cy.get('.image').should('have.text', 'image3')
+      cy.percySnapshot()
+    })
+  }
+)
--- apps/insight-viewer-demo/cypress/integration/overlay.spec.ts
+++ apps/insight-viewer-demo/cypress/integration/overlay.spec.ts
@@ -1,15 +1,24 @@
 import '@percy/cypress'
 import { setup } from '../support/utils'
+import { VIEWPORT_WIDTH, VIEWPORT_HEIGHT } from '../support/const'
 
-describe('Overlay', () => {
-  before(() => {
-    setup()
-    cy.visit('/overlay')
-  })
-
-  it('shows overlay', () => {
-    cy.get('.is-mount').then(() => {
-      cy.percySnapshot()
+describe(
+  'Overlay',
+  {
+    viewportWidth: VIEWPORT_WIDTH,
+    viewportHeight: VIEWPORT_HEIGHT,
+    scrollBehavior: false,
+  },
+  () => {
+    before(() => {
+      setup()
+      cy.visit('/overlay')
     })
-  })
-})
+
+    it('shows overlay', () => {
+      cy.get('.is-mount').then(() => {
+        cy.percySnapshot()
+      })
+    })
+  }
+)
