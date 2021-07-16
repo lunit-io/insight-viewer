5165441b030106a4e048a61f03ff710b0974ce33
skpark-lunit <skpark@lunit.io>
2021-07-16 15:33:12 +0900

    test: Remove cy.wait from spec

--- apps/insight-viewer-demo/cypress/integration/multiframe.spec.ts
+++ apps/insight-viewer-demo/cypress/integration/multiframe.spec.ts
@@ -1,29 +1,37 @@
 import '@percy/cypress'
 import { setup } from '../support/utils'
+import { VIEWPORT_WIDTH, VIEWPORT_HEIGHT } from '../support/const'
 
-describe('Multiframe', { scrollBehavior: false }, () => {
-  before(() => {
-    setup()
-    cy.visit('/multi-frame')
-  })
+describe(
+  'Multiframe',
+  {
+    viewportWidth: VIEWPORT_WIDTH,
+    viewportHeight: VIEWPORT_HEIGHT,
+    scrollBehavior: false,
+  },
+  () => {
+    before(() => {
+      setup()
+      cy.visit('/multi-frame')
+    })
 
-  beforeEach(() => {
-    cy.wait(1000)
-  })
+    it('shows initial viewer', () => {
+      cy.get('.is-mount').then(() => {
+        cy.get('.frame-number').should('have.text', '0')
+        cy.percySnapshot()
+      })
+    })
 
-  it('shows initial viewer', () => {
-    cy.get('.is-mount').then(() => {
+    it('change frame to 5', () => {
+      cy.get('.frame-control').controlledInputChange(5)
+      cy.get('.frame-number').should('have.text', '5')
       cy.percySnapshot()
     })
-  })
 
-  it('change frame to 5', () => {
-    cy.get('input[type="range"]').controlledInputChange(5)
-    cy.percySnapshot()
-  })
-
-  it('change frame to 10', () => {
-    cy.get('input[type="range"]').controlledInputChange(10)
-    cy.percySnapshot()
-  })
-})
+    it('change frame to 10', () => {
+      cy.get('.frame-control').controlledInputChange(10)
+      cy.get('.frame-number').should('have.text', '10')
+      cy.percySnapshot()
+    })
+  }
+)
