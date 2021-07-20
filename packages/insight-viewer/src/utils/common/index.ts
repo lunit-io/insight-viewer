66a07288dc024e74df1ab6a8b0ffec2acc38610a
skpark-lunit <skpark@lunit.io>
2021-07-20 16:04:46 +0900

    feat: Add hasViewport utility which check whether valid viewport exists

--- packages/insight-viewer/src/utils/common/index.ts
+++ packages/insight-viewer/src/utils/common/index.ts
@@ -1,5 +1,19 @@
 import consola from 'consola'
+import { Viewport, BasicViewport } from '../../types'
 
 export function handleError(e: Error): void {
   consola.error(e)
+}
+
+export function hasViewport(viewport: Viewport): viewport is BasicViewport {
+  return (
+    viewport.scale !== undefined &&
+    viewport.invert !== undefined &&
+    viewport.hflip !== undefined &&
+    viewport.vflip !== undefined &&
+    viewport.x !== undefined &&
+    viewport.y !== undefined &&
+    viewport.windowWidth !== undefined &&
+    viewport.windowCenter !== undefined
+  )
 }
