d316c1e46874728a8431e6fd1be87685a2f11623
skpark-lunit <skpark@lunit.io>
2021-07-21 16:46:53 +0900

    feat: Load image loader in useImageLoader hook

--- packages/insight-viewer/src/hooks/useImageLoader.ts
+++ packages/insight-viewer/src/hooks/useImageLoader.ts
@@ -1,92 +1,11 @@
-import { useEffect, useState, useRef } from 'react'
-import {
-  displayImage,
-  loadImage as cornerstoneLoadImage,
-} from '../utils/cornerstoneHelper'
-import getHttpClient from '../utils/httpClient'
-import {
-  loadingProgressMessage,
-  initialViewportMessage,
-} from '../utils/messageService'
-import { formatViewport } from '../utils/common/formatViewport'
-import {
-  Element,
-  Viewport,
-  ViewerError,
-  ViewerProp,
-  OnViewportChange,
-} from '../types'
+import { useState } from 'react'
+import { OnError } from '../types'
 
-export default function useImageLoader({
-  imageId,
-  element,
-  setLoader,
-  onError,
-  requestInterceptor,
-  onViewportChange,
-  viewportRef,
-}: Required<ViewerProp> & {
-  element: Element
-  setLoader: () => Promise<boolean>
-  viewportRef?: React.MutableRefObject<Viewport | undefined>
-  onViewportChange?: OnViewportChange
-}): void {
+export default function useImageLoader(
+  loader: (onerror: OnError) => Promise<boolean>,
+  onError: OnError
+): boolean {
   const [hasLoader, setHasLoader] = useState(false)
-  const loadCountRef = useRef(0)
-
-  // eslint-disable-next-line no-extra-semi
-  ;(async function asyncLoad(): Promise<void> {
-    if (!hasLoader) setHasLoader(await setLoader())
-  })()
-
-  useEffect(() => {
-    if (!hasLoader) return undefined
-    if (!element) return undefined
-
-    // determine whether it is first load or subsequent load(multiframe viewer)
-    loadCountRef.current += 1
-
-    async function loadAndDisplayImage(): Promise<void> {
-      try {
-        const image = await cornerstoneLoadImage(imageId, {
-          loader: getHttpClient(requestInterceptor),
-        })
-
-        const { viewport, defaultViewport } = displayImage(
-          <HTMLDivElement>element,
-          image,
-          loadCountRef.current === 1
-            ? viewportRef?.current?._default
-            : viewportRef?.current
-        )
-        // This is for no content-length gzip image. Normally, meaningless.
-        loadingProgressMessage.sendMessage(100)
-        initialViewportMessage.sendMessage(defaultViewport)
-
-        if (onViewportChange) {
-          onViewportChange(formatViewport(viewport))
-        }
-      } catch (e) {
-        /**
-         * ky HTTPError
-         * https://github.com/sindresorhus/ky/blob/main/source/errors/HTTPError.ts
-         * { error: { name: 'HTTPError', options, request, response, message, stack }
-         */
-        const err: ViewerError = new Error(e?.error?.message ?? e.message)
-        err.status = e?.error?.response?.status
-        onError(err)
-      }
-    }
-
-    loadAndDisplayImage()
-    return undefined
-  }, [
-    imageId,
-    element,
-    hasLoader,
-    onError,
-    onViewportChange,
-    requestInterceptor,
-    viewportRef,
-  ])
+  loader(onError).then(res => setHasLoader(res))
+  return hasLoader
 }
