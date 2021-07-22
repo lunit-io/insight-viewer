141de5f6b50c5d48b2723cfec26f74367cbebff7
skpark-lunit <skpark@lunit.io>
2021-07-22 17:37:23 +0900

    feat: Split useImageLoadAndDisplay hook

--- packages/insight-viewer/src/hooks/useImageLoadAndDisplay.ts
+++ packages/insight-viewer/src/hooks/useImageLoadAndDisplay.ts
@@ -1,86 +1,150 @@
-import { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import {
   displayImage,
   loadImage as cornerstoneLoadImage,
 } from '../utils/cornerstoneHelper'
+import { CornerstoneViewport } from '../utils/cornerstoneHelper/types'
 import getHttpClient from '../utils/httpClient'
 import {
   loadingProgressMessage,
   initialViewportMessage,
 } from '../utils/messageService'
 import { formatViewport } from '../utils/common/formatViewport'
+import { formatError } from '../utils/common'
 import {
   Element,
   Viewport,
-  ViewerError,
   ViewerProp,
   OnViewportChange,
+  RequestInterceptor,
+  BasicViewport,
 } from '../types'
 
-export default function useImageLoadAndDisplay({
-  imageId,
-  element,
-  hasLoader,
-  onError,
-  requestInterceptor,
-  onViewportChange,
-  viewportRef,
-}: Required<ViewerProp> & {
+type Prop = ViewerProp & {
   element: Element
   hasLoader: boolean
   viewportRef?: React.MutableRefObject<Viewport | undefined>
   onViewportChange?: OnViewportChange
-}): void {
+}
+
+type ImageViewport = Promise<{
+  viewport: CornerstoneViewport
+  defaultViewport: CornerstoneViewport
+}>
+
+type DefaultImageViewport = (arg: {
+  imageId: string
+  element: Element
+  requestInterceptor: RequestInterceptor
+  option: Partial<BasicViewport> | undefined
+}) => ImageViewport
+
+export type GetImageViewport = DefaultImageViewport | (() => ImageViewport)
+
+/**
+ * Load and display image with corenrstone.js.
+ * If successful, return viewport data.
+ * If not successful, return error.
+ */
+const _getImageViewport: GetImageViewport = async ({
+  imageId,
+  element,
+  requestInterceptor,
+  option,
+}) => {
+  try {
+    const image = await cornerstoneLoadImage(imageId, {
+      loader: getHttpClient(requestInterceptor),
+    })
+    const { viewport, defaultViewport } = displayImage(
+      <HTMLDivElement>element,
+      image,
+      option
+    )
+    return { viewport, defaultViewport }
+  } catch (e) {
+    throw formatError(e)
+  }
+}
+
+/**
+ * Initialize viewport and handle loading state after loading and displaying image.
+ * If successful, return true. It works well.
+ * If not successful, return false. It calls onError.
+ * getImageViewport is pluggable for unit test.
+ */
+export async function loadAndDisplayImage({
+  imageId,
+  element,
+  requestInterceptor,
+  onViewportChange,
+  loadCountRef,
+  viewportRef,
+  onError,
+  hasLoader,
+  getImageViewport = _getImageViewport,
+}: Prop & {
+  loadCountRef: React.MutableRefObject<number>
+  getImageViewport?: GetImageViewport
+}): Promise<boolean> {
+  try {
+    if (!element || !hasLoader) return false
+    const imageViewport = await getImageViewport({
+      imageId,
+      element,
+      requestInterceptor,
+      option:
+        loadCountRef.current === 1
+          ? viewportRef?.current?._default
+          : viewportRef?.current,
+    })
+    const { viewport, defaultViewport } = imageViewport
+    loadingProgressMessage.sendMessage(100)
+    initialViewportMessage.sendMessage(defaultViewport)
+
+    if (onViewportChange) {
+      onViewportChange(formatViewport(viewport))
+    }
+    return true
+  } catch (e) {
+    onError(e)
+    return false
+  }
+}
+
+export default function useImageLoadAndDisplay(prop: Prop): void {
   const loadCountRef = useRef(0)
+  const {
+    element,
+    imageId,
+    hasLoader,
+    viewportRef,
+    onError,
+    requestInterceptor,
+    onViewportChange,
+  } = prop
 
   useEffect(() => {
-    if (!hasLoader) return undefined
-    if (!element) return undefined
-
-    // determine whether it is first load or subsequent load(multiframe viewer)
+    // Determine whether it is first load or subsequent load(multiframe viewer)
     loadCountRef.current += 1
 
-    async function loadAndDisplayImage(): Promise<void> {
-      try {
-        const image = await cornerstoneLoadImage(imageId, {
-          loader: getHttpClient(requestInterceptor),
-        })
-
-        const { viewport, defaultViewport } = displayImage(
-          <HTMLDivElement>element,
-          image,
-          loadCountRef.current === 1
-            ? viewportRef?.current?._default
-            : viewportRef?.current
-        )
-        // This is for no content-length gzip image. Normally, meaningless.
-        loadingProgressMessage.sendMessage(100)
-        initialViewportMessage.sendMessage(defaultViewport)
-
-        if (onViewportChange) {
-          onViewportChange(formatViewport(viewport))
-        }
-      } catch (e) {
-        /**
-         * ky HTTPError
-         * https://github.com/sindresorhus/ky/blob/main/source/errors/HTTPError.ts
-         * { error: { name: 'HTTPError', options, request, response, message, stack }
-         */
-        const err: ViewerError = new Error(e?.error?.message ?? e.message)
-        err.status = e?.error?.response?.status
-        onError(err)
-      }
-    }
-
-    loadAndDisplayImage()
-    return undefined
+    loadAndDisplayImage({
+      element,
+      imageId,
+      hasLoader,
+      viewportRef,
+      onError,
+      requestInterceptor,
+      onViewportChange,
+      loadCountRef,
+    })
   }, [
-    imageId,
     element,
+    imageId,
     hasLoader,
-    onError,
-    onViewportChange,
-    requestInterceptor,
     viewportRef,
+    onError,
+    requestInterceptor,
+    onViewportChange,
   ])
 }
