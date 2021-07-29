4bbf0e8c22b978b2be59185b18096d7f2a383e6c
skpark-lunit <skpark@lunit.io>
2021-07-29 15:48:22 +0900

    doc: Delete LoadingState demo

--- apps/insight-viewer-demo/src/components/Nav/const.ts
+++ apps/insight-viewer-demo/src/components/Nav/const.ts
@@ -1,7 +1,6 @@
 export const LINKS = [
   { href: '', name: 'Basic Viewer' },
   { href: 'multi-frame', name: 'MultiFrame Viewer' },
-  { href: 'loading-state', name: 'Loading State' },
   { href: 'error', name: 'Error' },
   { href: 'progress', name: 'Progress' },
   { href: 'http-header', name: 'Http header' },
