757dfe5d73e933f68cf4c8825d5b4dee5c0d6229
skpark-lunit <skpark@lunit.io>
2021-07-29 16:00:50 +0900

    doc: Delete LoadingState demo page

--- apps/insight-viewer-demo/src/pages/loading-state.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('../containers/LoadingState'),
-  { ssr: false }
-)
-
-export default DynamicComponentWithNoSSR
