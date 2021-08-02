4f32570eaa483469ee0ee02e95a59006a093b32c
skpark-lunit <skpark@lunit.io>
2021-08-02 16:35:03 +0900

    test: Update prefetch spec

--- packages/insight-viewer/src/hooks/useMultiframe/usePrefetch.spec.ts
+++ packages/insight-viewer/src/hooks/useMultiframe/usePrefetch.spec.ts
@@ -1,7 +1,8 @@
-import { prefetch } from './usePrefetch'
+import { prefetch, Prefetched } from './usePrefetch'
 import { CONFIG } from '../../const'
 import { CornerstoneImage } from '../../utils/cornerstoneHelper'
 import { CORNERSTONE_IMAGE_MOCK } from '../../mocks/const'
+import { ViewerError } from '../../types'
 
 const { requestInterceptor } = CONFIG
 const IMAGES = [
@@ -9,32 +10,106 @@ const IMAGES = [
   'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000001.dcm',
   'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm',
 ]
-
 const cornerstoneImage = CORNERSTONE_IMAGE_MOCK as unknown as CornerstoneImage
+const getLoadImageMock = jest.fn()
 
 describe('usePrefetch', () => {
-  it('getLoadImage succeeds', async () => {
-    const getLoadImage = async () => cornerstoneImage
-    const res = await prefetch({
-      images: IMAGES,
-      requestInterceptor,
-      getLoadImage,
-    })
+  let count: number
 
-    expect(res?.length).toBe(IMAGES.length)
-    expect(res?.[0]).toMatchObject(CORNERSTONE_IMAGE_MOCK)
+  beforeEach(() => {
+    count = -1
   })
 
-  it('getLoadImage fails', async () => {
-    const getLoadImage = () => Promise.reject(new Error('request fails'))
+  it('fetches all images successfully', () => {
+    const loadedPercentage = {
+      '0': 33,
+      '1': 67,
+      '2': 100,
+    }
+    prefetch({
+      images: IMAGES,
+      requestInterceptor,
+      getLoadImage: getLoadImageMock.mockImplementation(() =>
+        Promise.resolve(cornerstoneImage)
+      ),
+    }).subscribe({
+      next: async (res: Prefetched) => {
+        count += 1
 
-    const action = async () =>
-      prefetch({
-        images: IMAGES,
-        requestInterceptor,
-        getLoadImage,
-      })
+        expect(res).toMatchObject({
+          image: cornerstoneImage,
+          loadedPercentage:
+            loadedPercentage[String(count) as keyof typeof loadedPercentage],
+        })
+      },
+    })
+  })
 
-    await expect(action()).rejects.toThrow('request fails')
+  it('fails on first request', () => {
+    prefetch({
+      images: IMAGES,
+      requestInterceptor,
+      getLoadImage: getLoadImageMock.mockImplementation(() => {
+        count += 1
+        if (count === 0)
+          return Promise.reject(new Error('first image fetch fails'))
+        return Promise.resolve(cornerstoneImage)
+      }),
+    }).subscribe({
+      error: async (err: ViewerError) => {
+        expect(err.message).toBe('first image fetch fails')
+        expect(count).toBe(0)
+      },
+    })
+  })
+
+  it('fails on second request', () => {
+    prefetch({
+      images: IMAGES,
+      requestInterceptor,
+      getLoadImage: getLoadImageMock.mockImplementation(() => {
+        count += 1
+        if (count === 1)
+          return Promise.reject(new Error('second image fetch fails'))
+        return Promise.resolve(cornerstoneImage)
+      }),
+    }).subscribe({
+      error: async (err: ViewerError) => {
+        expect(err.message).toBe('second image fetch fails')
+        expect(count).toBe(1)
+      },
+    })
+  })
+
+  it('fails on the last request', () => {
+    prefetch({
+      images: IMAGES,
+      requestInterceptor,
+      getLoadImage: getLoadImageMock.mockImplementation(() => {
+        count += 1
+        if (count === IMAGES.length - 1)
+          return Promise.reject(new Error('last image fetch fails'))
+        return Promise.resolve(cornerstoneImage)
+      }),
+    }).subscribe({
+      error: async (err: ViewerError) => {
+        expect(err.message).toBe('last image fetch fails')
+        expect(count).toBe(IMAGES.length - 1)
+      },
+    })
+  })
+
+  it('fails for all images', () => {
+    prefetch({
+      images: IMAGES,
+      requestInterceptor,
+      getLoadImage: getLoadImageMock.mockImplementation(() =>
+        Promise.reject(new Error('all images fetch fails'))
+      ),
+    }).subscribe({
+      error: async (err: ViewerError) => {
+        expect(err.message).toBe('all images fetch fails')
+      },
+    })
   })
 })
