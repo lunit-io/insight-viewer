f7f9197928854a6bd40ac98e07adb9ef829452dc
skpark-lunit <skpark@lunit.io>
2021-08-02 22:10:27 +0900

    doc: Add no content-length mock http

--- apps/insight-viewer-demo/src/mocks/handlers.ts
+++ apps/insight-viewer-demo/src/mocks/handlers.ts
@@ -1,11 +1,12 @@
 import { rest } from 'msw'
+import { IMAGES } from '../const'
+
+const PREFIX = 'wadouri:'
 
 export const handlers = [
   rest.get('/msw/with-jwt', async (req, res, ctx) => {
     const authHeader = req.headers.get('Authorization')
-    const imageBuffer = await fetch(
-      'https://static.lunit.io/fixtures/dcm-files/series/CT000009.dcm'
-    ).then(r => r.arrayBuffer())
+    const imageBuffer = await fetch(IMAGES[9]).then(r => r.arrayBuffer())
 
     if (!authHeader) {
       return res(
@@ -17,5 +18,30 @@ export const handlers = [
       ctx.set('Content-Length', imageBuffer.byteLength.toString()),
       ctx.body(imageBuffer)
     )
+  }),
+
+  rest.get('/msw/no-content-length/1', async (_, res, ctx) => {
+    const imageBuffer = await fetch(IMAGES[1].replace(PREFIX, '')).then(r =>
+      r.arrayBuffer()
+    )
+    return res(ctx.body(imageBuffer))
+  }),
+  rest.get('/msw/no-content-length/2', async (_, res, ctx) => {
+    const imageBuffer = await fetch(IMAGES[6].replace(PREFIX, '')).then(r =>
+      r.arrayBuffer()
+    )
+    return res(ctx.body(imageBuffer))
+  }),
+  rest.get('/msw/no-content-length/3', async (_, res, ctx) => {
+    const imageBuffer = await fetch(IMAGES[7].replace(PREFIX, '')).then(r =>
+      r.arrayBuffer()
+    )
+    return res(ctx.body(imageBuffer))
+  }),
+  rest.get('/msw/no-content-length/4', async (_, res, ctx) => {
+    const imageBuffer = await fetch(IMAGES[8].replace(PREFIX, '')).then(r =>
+      r.arrayBuffer()
+    )
+    return res(ctx.body(imageBuffer))
   }),
 ]
