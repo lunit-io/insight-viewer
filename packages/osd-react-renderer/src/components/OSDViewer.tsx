ed711ac4bc21b087ebdd40c9e8df931fb061945c
Hyunjin Kwon <hyunjin.kwon@lunit.io>
2021-08-02 14:42:18 +0900

    feat: Set OSDViewer component

---
+++ packages/osd-react-renderer/src/components/OSDViewer.tsx
@@ -0,0 +1,26 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import ReactOSDDOM from '../ReactOSDDOM'
+import { OSDViewerProps } from '../types'
+
+const OSDViewer: React.FC<OSDViewerProps> = ({ children, options = {} }) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (!(containerRef.current instanceof HTMLElement)) {
+      return
+    }
+    ReactOSDDOM.render(<>{children}</>, containerRef.current, options)
+  })
+
+  return (
+    <div
+      style={{ width: '100%', height: '100%' }}
+      className="openseadragon"
+      ref={containerRef}
+    />
+  )
+}
+
+OSDViewer.displayName = 'OSDViwer'
+
+export default OSDViewer
