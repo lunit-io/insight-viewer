79e9198a8620644e5339e2043f738804c497069d
skpark-lunit <skpark@lunit.io>
2021-08-02 19:37:29 +0900

    feat: Add useImageLoader

---
+++ packages/insight-viewer/src/hooks/useImageLoader.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { LOADER_TYPE } from '../const'
+import { LoaderType, OnError } from '../types'
+import setWadoImageLoader from '../utils/cornerstoneHelper/setWadoImageLoader'
+import setWebImageLoader from '../utils/cornerstoneHelper/setWebImageLoader'
+
+export function useImageLoader(
+  type: LoaderType = LOADER_TYPE.Dicom,
+  onError: OnError
+): boolean {
+  const [hasLoader, setHasLoader] = useState(false)
+
+  const loader =
+    type === LOADER_TYPE.Dicom ? setWadoImageLoader : setWebImageLoader
+
+  // eslint-disable-next-line no-extra-semi
+  ;(async function asyncLoad(): Promise<void> {
+    if (!hasLoader) setHasLoader(await loader(onError))
+  })()
+
+  return hasLoader
+}
