f5b2b20acdda1fee7c93df6a52076eb2d0dd3a77
Taiwon Chung <tchung@lunit.io>
2021-08-05 15:55:30 +0900

    craco config updated to run babel on osd-react-renderer src

--- apps/osd-viewer-demo/craco.config.js
+++ apps/osd-viewer-demo/craco.config.js
@@ -1,13 +1,31 @@
 const CracoAlias = require('craco-alias')
+const { getLoader, loaderByName } = require('@craco/craco')
 const path = require('path')
+
+const absolutePath = path.join(
+  __dirname,
+  '../../packages/osd-react-renderer/src'
+)
 
 module.exports = {
   webpack: {
     alias: {
-      '@lunit/osd-react-renderer': path.resolve(
-        __dirname,
-        '../../packages/osd-react-renderer/src'
-      ),
+      '@lunit/osd-react-renderer': absolutePath,
+    },
+    configure: (webpackConfig, { env, paths }) => {
+      const { isFound, match } = getLoader(
+        webpackConfig,
+        loaderByName('babel-loader')
+      )
+
+      if (isFound) {
+        const include = Array.isArray(match.loader.include)
+          ? match.loader.include
+          : [match.loader.include]
+        match.loader.include = include.concat[absolutePath]
+      }
+
+      return webpackConfig
     },
   },
   plugins: [
