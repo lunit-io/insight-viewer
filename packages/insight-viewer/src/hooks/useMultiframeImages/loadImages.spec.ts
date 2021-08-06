d280cb961acbd55564ba8fcb7a3c285258dd999f
skpark-lunit <skpark@lunit.io>
2021-08-06 18:47:21 +0900

    test: Add handleMock util method

--- packages/insight-viewer/src/hooks/useMultiframeImages/loadImages.spec.ts
+++ packages/insight-viewer/src/hooks/useMultiframeImages/loadImages.spec.ts
@@ -14,108 +14,123 @@ const IMAGES = [
 const cornerstoneImage = CORNERSTONE_IMAGE_MOCK as unknown as CornerstoneImage
 const getLoadImageMock = jest.fn()
 
+const errorText = {
+  0: 'first',
+  1: 'second',
+  2: 'last',
+  3: 'all',
+}
+
+type ErrorTextKey = keyof typeof errorText
+
+/** It mocks getLoadImage() which returns Promise.
+ * When loop counter equals to errorIndex, the Promise is rejected.
+ * Otherwise, the Promise is resolved.
+ * @param errorIndex The index to reject. If it is -1, no error.
+ * @returns Promise<CornerstoneImage>
+ */
+function handleMock({ errorIndex = -1 }) {
+  let count = -1
+  return getLoadImageMock.mockImplementation(() => {
+    count += 1
+    if (errorIndex === count) {
+      return Promise.reject(
+        new Error(`${errorText[errorIndex as ErrorTextKey]} image fetch fails`)
+      )
+    }
+    return Promise.resolve(cornerstoneImage)
+  })
+}
+
 describe('loadImages()', () => {
-  let count: number
+  describe('return images and loaded count', () => {
+    let count: number
 
-  beforeEach(() => {
-    count = -1
-  })
-
-  it('fetches all images successfully', () => {
-    loadImages({
-      images: IMAGES,
-      requestInterceptor,
-      getLoadImage: getLoadImageMock.mockImplementation(() => {
-        count += 1
-        Promise.resolve(cornerstoneImage)
-      }),
-    }).subscribe({
-      next: async (res: Loaded) => {
-        expect(res).toMatchObject({
-          image: cornerstoneImage,
-          loaded: count + 1,
-        })
-      },
+    beforeEach(() => {
+      count = 0
     })
-  })
 
-  it('fails on first request', () => {
-    loadImages({
-      images: IMAGES,
-      requestInterceptor,
-      getLoadImage: getLoadImageMock.mockImplementation(() => {
-        count += 1
-        if (count === 0)
-          return Promise.reject(new Error('first image fetch fails'))
-        return Promise.resolve(cornerstoneImage)
-      }),
-    }).subscribe({
-      error: async (err: ViewerError) => {
-        expect(err.message).toBe('first image fetch fails')
-        expect(count).toBe(count)
-      },
+    it('fetches all images successfully', () => {
+      // TODO: Needs to implement observable mock.
+      loadImages({
+        images: IMAGES,
+        requestInterceptor,
+        getLoadImage: handleMock({ errorIndex: -1 }),
+      }).subscribe({
+        next: async (res: Loaded) => {
+          count += 1
+          expect(res).toMatchObject({
+            image: cornerstoneImage,
+            loaded: count,
+          })
+        },
+      })
     })
-  })
 
-  it('fails on second request', () => {
-    loadImages({
-      images: IMAGES,
-      requestInterceptor,
-      getLoadImage: getLoadImageMock.mockImplementation(() => {
-        count += 1
-        if (count === 1)
-          return Promise.reject(new Error('second image fetch fails'))
-        return Promise.resolve(cornerstoneImage)
-      }),
-    }).subscribe({
-      next: async (res: Loaded) => {
-        expect(res).toMatchObject({
-          image: cornerstoneImage,
-          loaded: count + 1,
-        })
-      },
-      error: async (err: ViewerError) => {
-        expect(err.message).toBe('second image fetch fails')
-        expect(count).toBe(count)
-      },
+    it('fails on first request', () => {
+      loadImages({
+        images: IMAGES,
+        requestInterceptor,
+        getLoadImage: handleMock({ errorIndex: 0 }),
+      }).subscribe({
+        error: async (err: ViewerError) => {
+          expect(err.message).toBe('first image fetch fails')
+          expect(count).toBe(0)
+        },
+      })
     })
-  })
 
-  it('fails on the last request', () => {
-    loadImages({
-      images: IMAGES,
-      requestInterceptor,
-      getLoadImage: getLoadImageMock.mockImplementation(() => {
-        count += 1
-        if (count === IMAGES.length - 1)
-          return Promise.reject(new Error('last image fetch fails'))
-        return Promise.resolve(cornerstoneImage)
-      }),
-    }).subscribe({
-      next: async (res: Loaded) => {
-        expect(res).toMatchObject({
-          image: cornerstoneImage,
-          loaded: count + 1,
-        })
-      },
-      error: async (err: ViewerError) => {
-        expect(err.message).toBe('last image fetch fails')
-        expect(count).toBe(count)
-      },
+    it('fails on second request', () => {
+      loadImages({
+        images: IMAGES,
+        requestInterceptor,
+        getLoadImage: handleMock({ errorIndex: 1 }),
+      }).subscribe({
+        next: async (res: Loaded) => {
+          count += 1
+          expect(res).toMatchObject({
+            image: cornerstoneImage,
+            loaded: count,
+          })
+        },
+        error: async (err: ViewerError) => {
+          expect(err.message).toBe('second image fetch fails')
+          expect(count).toBe(1)
+        },
+      })
     })
-  })
 
-  it('fails for all images', () => {
-    loadImages({
-      images: IMAGES,
-      requestInterceptor,
-      getLoadImage: getLoadImageMock.mockImplementation(() =>
-        Promise.reject(new Error('all images fetch fails'))
-      ),
-    }).subscribe({
-      error: async (err: ViewerError) => {
-        expect(err.message).toBe('all images fetch fails')
-      },
+    it('fails on the last request', () => {
+      loadImages({
+        images: IMAGES,
+        requestInterceptor,
+        getLoadImage: handleMock({ errorIndex: 2 }),
+      }).subscribe({
+        next: async (res: Loaded) => {
+          count += 1
+          expect(res).toMatchObject({
+            image: cornerstoneImage,
+            loaded: count,
+          })
+        },
+        error: async (err: ViewerError) => {
+          expect(err.message).toBe('last image fetch fails')
+          expect(count).toBe(2)
+        },
+      })
+    })
+
+    it('fails for all images', () => {
+      loadImages({
+        images: IMAGES,
+        requestInterceptor,
+        getLoadImage: handleMock({ errorIndex: 3 }),
+      }).subscribe({
+        error: async (err: ViewerError) => {
+          expect(err.message).toBe('all images fetch fails')
+          expect(count).toBe(0)
+        },
+      })
     })
   })
 })
