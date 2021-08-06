620dd7fd568bed49272e6a98f94bdbbb64afd145
skpark-lunit <skpark@lunit.io>
2021-08-06 20:14:56 +0900

    refactor: Rename useMultipleImages

--- packages/insight-viewer/src/hooks/useMultipleImages/index.ts
+++ packages/insight-viewer/src/hooks/useMultipleImages/index.ts
@@ -1,6 +1,5 @@
 /**
- * @fileoverview Loads images(Dicom/Web) and
- * return the current image and loading state, current frame and frame updater.
+ * @fileoverview Loads images(Dicom/Web) and return the current image and loading state.
  */
 
 import { useLoadImages } from './useLoadImages'
@@ -11,31 +10,23 @@ import { ImagesLoadState } from './types'
 
 type Prop = {
   imageIds: string[]
-  initialFrame?: number
   type?: LoaderType
 } & Partial<HTTP>
 
 interface UseMultiframeImages {
-  ({
-    imageIds,
-    initialFrame,
-    onError,
-    requestInterceptor,
-    type,
-  }: Prop): ImagesLoadState
+  ({ imageIds, onError, requestInterceptor, type }: Prop): ImagesLoadState
 }
 
 /**
  * @param imageIds The images urls to load.
  * @param type The image type to load. 'Dicom'(default) | 'Web'.
- * @param initialFrame
  * @param requestInterceptor The callback is called before a request is sent.
  *  It uses ky.js beforeRequest hook.
  * @param onError The error handler.
- * @returns <{ image, loadingStates, frame, setFrame }> image is a CornerstoneImage.
+ * @returns <{ image, loadingStates }> image is a CornerstoneImage.
  *  loadingStates are each image's <'initial'|'loading'|'success'|'fail'>
  */
-export const useMultiframeImages: UseMultiframeImages = ({
+export const useMultipleImages: UseMultiframeImages = ({
   imageIds,
   type = LOADER_TYPE.Dicom,
   requestInterceptor = CONFIG.requestInterceptor,
--- packages/insight-viewer/src/index.ts
+++ packages/insight-viewer/src/index.ts
@@ -1,5 +1,5 @@
 export { ImageViewer as default } from './Viewer'
-export { useMultiframeImages } from './hooks/useMultiframeImages'
+export { useMultipleImages } from './hooks/useMultipleImages'
 export { useViewport } from './hooks/useViewport'
 export { useInteraction } from './hooks/useInteraction'
 export { useImage } from './hooks/useImage'
