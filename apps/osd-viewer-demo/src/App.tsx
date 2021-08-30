1ee694a5197bd0c907ada0292ca81a2143faf44f
Taiwon Chung <tchung@lunit.io>
2021-08-30 11:18:00 +0900

    feat(osd-viewer-demo): changed some confusing constants to named variables

--- apps/osd-viewer-demo/src/App.tsx
+++ apps/osd-viewer-demo/src/App.tsx
@@ -9,13 +9,16 @@ function App() {
     console.log(num)
   }, [num])
 
+  const demoMpp = 0.263175
+  const micronsPerMeter = 1e6
+
   return (
     <div style={{ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0 }}>
       <OSDViewer options={{}}>
         <viewport zoom={10} rotation={90} onZoom={onZoom} />
         <tiledImage url="https://image-pdl1.api.opt.scope.lunit.io/slides/images/dzi/41f49f4c-8dcd-4e85-9e7d-c3715f391d6f/3/122145f9-7f68-4f85-82f7-5b30364c2323/D_202103_Lunit_NSCLC_011_IHC_22C3.svs" />
         <scalebar
-          pixelsPerMeter={1e6 / 0.263175}
+          pixelsPerMeter={micronsPerMeter / demoMpp}
           xOffset={10}
           yOffset={30}
           barThickness={3}
