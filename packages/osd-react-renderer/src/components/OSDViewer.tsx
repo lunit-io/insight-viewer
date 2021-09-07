dc79f939b5fd991f9d8bdd4a47e74ea892f34a3e
Hyunjin Kwon <hyunjin.kwon@lunit.io>
2021-09-07 23:11:12 +0900

    chore(osd-react-renderer): create osdViewerRef to access viewer

--- packages/osd-react-renderer/src/components/OSDViewer.tsx
+++ packages/osd-react-renderer/src/components/OSDViewer.tsx
@@ -1,25 +1,39 @@
 import React, { useLayoutEffect, useRef } from 'react'
 import ReactOSDDOM from '../ReactOSDDOM'
-import { OSDViewerProps } from '../types'
+import { OSDViewerProps, OSDViewerRef } from '../types'
 
-const OSDViewer: React.FC<OSDViewerProps> = ({ children, options = {} }) => {
-  const containerRef = useRef<HTMLDivElement>(null)
+const OSDViewer = React.forwardRef<OSDViewerRef, OSDViewerProps>(
+  ({ children, options = {} }, ref) => {
+    const containerRef = useRef<HTMLDivElement>(null)
 
-  useLayoutEffect(() => {
-    if (!(containerRef.current instanceof HTMLElement)) {
-      return
-    }
-    ReactOSDDOM.render(<>{children}</>, containerRef.current, options)
-  }, [children, options])
+    useLayoutEffect(() => {
+      if (!(containerRef.current instanceof HTMLElement)) {
+        return
+      }
+      const viewer = ReactOSDDOM.render(
+        <>{children}</>,
+        containerRef.current,
+        options
+      )
+      const refValue = { container: containerRef.current, viewer }
+      if (ref != null) {
+        if (typeof ref === 'function') {
+          ref(refValue)
+        } else if (typeof ref === 'object') {
+          ref.current = refValue
+        }
+      }
+    }, [children, options])
 
-  return (
-    <div
-      style={{ width: '100%', height: '100%' }}
-      className="openseadragon"
-      ref={containerRef}
-    />
-  )
-}
+    return (
+      <div
+        style={{ width: '100%', height: '100%' }}
+        className="openseadragon"
+        ref={containerRef}
+      />
+    )
+  }
+)
 
 OSDViewer.displayName = 'OSDViewer'
 
