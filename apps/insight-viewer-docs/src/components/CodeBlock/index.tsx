824620244cab57dedeff0424af62e72db674a415
skpark-lunit <skpark@lunit.io>
2021-09-08 13:55:02 +0900

    docs(viewer-docs): update prismjs style and add buttons to CodeBlock

--- apps/insight-viewer-docs/src/components/CodeBlock/index.tsx
+++ apps/insight-viewer-docs/src/components/CodeBlock/index.tsx
@@ -1,24 +1,40 @@
 import { useEffect } from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Button, HStack, Link } from '@chakra-ui/react'
 import Prism from 'prismjs'
-import 'prismjs/themes/prism-tomorrow.css'
 import { WithChildren } from '../../types'
 import config from '../../../config'
 
 export default function CodeBlock({
   code,
+  codeSandbox,
 }: WithChildren<{
   code: string
+  codeSandbox?: string
 }>): JSX.Element {
   useEffect(() => {
     Prism.highlightAll()
   }, [])
 
+  const copyCodeToClipboard = () => {
+    navigator.clipboard.writeText(code)
+  }
   return (
-    <Box className="Code" display={config.IS_CYPRESS ? 'none' : 'block'}>
+    <Box position="relative" display={config.IS_CYPRESS ? 'none' : 'block'}>
       <pre>
         <code className="language-javascript">{code}</code>
       </pre>
+      <HStack position="absolute" top="3" right="3">
+        {navigator.clipboard && (
+          <Button colorScheme="blue" size="xs" onClick={copyCodeToClipboard}>
+            COPY
+          </Button>
+        )}
+        {codeSandbox && (
+          <Link href={codeSandbox} isExternal>
+            <Button size="xs">CODESANDBOX</Button>
+          </Link>
+        )}
+      </HStack>
     </Box>
   )
 }
