a4d9c3985deb3bf5ddf1f979aaf656af91980d60
skpark-lunit <skpark@lunit.io>
2021-10-13 17:19:53 +0900

    refactor(viewer): replace isError type guard with inline code

--- packages/insight-viewer/src/utils/common/index.ts
+++ packages/insight-viewer/src/utils/common/index.ts
@@ -10,10 +10,6 @@ interface HTTPError {
   error: { response: { status: number }; message: string }
 }
 
-function isError(e: unknown): e is Error {
-  return e instanceof Error
-}
-
 function isHTTPError(e: unknown): e is HTTPError {
   return (
     (e as HTTPError)?.error !== undefined &&
@@ -22,7 +18,7 @@ function isHTTPError(e: unknown): e is HTTPError {
 }
 
 export function normalizeError(e: Error | HTTPError | unknown): ViewerError {
-  if (isError(e)) return e
+  if (e instanceof Error) return e
   if (isHTTPError(e)) {
     const err: ViewerError = new Error(e.error.message)
     err.status = e?.error?.response?.status
