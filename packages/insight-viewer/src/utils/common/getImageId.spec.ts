a984bf21a372bfc6ad82302c3b6a6f48d1c9d7cf
skpark-lunit <skpark@lunit.io>
2021-10-13 20:43:15 +0900

    test(viewer): add getImageIds test and update getLoaderType test

--- packages/insight-viewer/src/utils/common/getImageId.spec.ts
+++ packages/insight-viewer/src/utils/common/getImageId.spec.ts
@@ -1,6 +1,6 @@
 import { IMAGE_TYPE } from '../../const'
-import { ImageType } from '../../types'
-import { getImageId } from './getImageId'
+import { ImageType, ImageTypes } from '../../types'
+import { getImageId, getImageIds } from './getImageId'
 
 // TODO: 파일 string 값 유효성 체크해야 하는가?
 // TODO: wadouri를 자동으로 붙여주기?
@@ -26,5 +26,28 @@ describe('getImageId()', () => {
       [IMAGE_TYPE.WEB]: file,
     }
     expect(getImageId(imageType)).toEqual(file)
+  })
+})
+
+describe('getImageIds()', () => {
+  it('return dicom when image types are wadouri', () => {
+    const imageTypes: ImageTypes = {
+      [IMAGE_TYPE.WADOURI]: [file],
+    }
+    expect(getImageIds(imageTypes)).toEqual([file])
+  })
+
+  it('return dicom when image types are dicomfile', () => {
+    const imageTypes: ImageTypes = {
+      [IMAGE_TYPE.DICOMFILE]: [file],
+    }
+    expect(getImageIds(imageTypes)).toEqual([file])
+  })
+
+  it('return web when image types are web', () => {
+    const imageTypes: ImageTypes = {
+      [IMAGE_TYPE.WEB]: [file],
+    }
+    expect(getImageIds(imageTypes)).toEqual([file])
   })
 })
--- packages/insight-viewer/src/utils/common/getLoaderType.spec.ts
+++ packages/insight-viewer/src/utils/common/getLoaderType.spec.ts
@@ -1,5 +1,5 @@
 import { IMAGE_TYPE, LOADER_TYPE } from '../../const'
-import { ImageType } from '../../types'
+import { ImageType, ImageTypes } from '../../types'
 import { getLoaderType } from './getLoaderType'
 
 const file = 'wadouri:https://example/CT000000.dcm'
@@ -12,6 +12,13 @@ describe('getLoaderType()', () => {
     expect(getLoaderType(imageType)).toEqual(LOADER_TYPE.DICOM)
   })
 
+  it('return dicom when image types are wadouri', () => {
+    const imageTypes: ImageTypes = {
+      [IMAGE_TYPE.WADOURI]: [file],
+    }
+    expect(getLoaderType(imageTypes)).toEqual(LOADER_TYPE.DICOM)
+  })
+
   it('return dicom when image type is dicomfile', () => {
     const imageType: ImageType = {
       [IMAGE_TYPE.DICOMFILE]: file,
@@ -19,10 +26,24 @@ describe('getLoaderType()', () => {
     expect(getLoaderType(imageType)).toEqual(LOADER_TYPE.DICOM)
   })
 
+  it('return dicom when image types are dicomfile', () => {
+    const imageTypes: ImageTypes = {
+      [IMAGE_TYPE.DICOMFILE]: [file],
+    }
+    expect(getLoaderType(imageTypes)).toEqual(LOADER_TYPE.DICOM)
+  })
+
   it('return web when image type is web', () => {
     const imageType: ImageType = {
       [IMAGE_TYPE.WEB]: file,
     }
     expect(getLoaderType(imageType)).toEqual(LOADER_TYPE.WEB)
+  })
+
+  it('return web when image types are web', () => {
+    const imageTypes: ImageTypes = {
+      [IMAGE_TYPE.WEB]: [file],
+    }
+    expect(getLoaderType(imageTypes)).toEqual(LOADER_TYPE.WEB)
   })
 })
