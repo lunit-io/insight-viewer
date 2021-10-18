5bb24ef0faf30269512a50ed9983e629dc5a437b
skpark-lunit <skpark@lunit.io>
2021-10-18 21:16:18 +0900

    refactor(viewer): use renamed IMAGE_LOADER_SCHEME

--- packages/insight-viewer/src/hooks/useImageLoader/getLoaderType.ts
+++ packages/insight-viewer/src/hooks/useImageLoader/getLoaderType.ts
@@ -1,7 +1,7 @@
-import { IMAGE_TYPE, LOADER_TYPE } from '../../const'
+import { IMAGE_LOADER_SCHEME, LOADER_TYPE } from '../../const'
 import { ImageId, LoaderType } from '../../types'
 
 export function getLoaderType(obj: ImageId): LoaderType {
-  if (obj[IMAGE_TYPE.WEB]) return LOADER_TYPE.WEB
+  if (obj[IMAGE_LOADER_SCHEME.WEB]) return LOADER_TYPE.WEB
   return LOADER_TYPE.DICOM
 }
