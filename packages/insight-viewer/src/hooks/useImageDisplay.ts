49432c49b22c5ff073c11a36f9904085a5b52003
skpark-lunit <skpark@lunit.io>
2021-12-17 17:07:47 +0900

    feat(viewer): reset viewport when image is changed

--- packages/insight-viewer/src/hooks/useImageDisplay.ts
+++ packages/insight-viewer/src/hooks/useImageDisplay.ts
@@ -2,7 +2,11 @@
  * @fileoverview Display the cornerstone image in a given HTML element and set viewport.
  */
 import React, { useEffect, useRef } from 'react'
-import { displayImage, CornerstoneImage } from '../utils/cornerstoneHelper'
+import {
+  displayImage,
+  CornerstoneImage,
+  CornerstoneViewport,
+} from '../utils/cornerstoneHelper'
 import { initialViewportMessage } from '../utils/messageService'
 import { formatViewport } from '../utils/common/formatViewport'
 import { Element, Viewport, OnViewportChange } from '../types'
@@ -28,21 +32,28 @@ export default function useImageDisplay({
 }: Prop): void {
   // Determine whether it is the first load or subsequent load(in case of multiframe viewer)
   const loadCountRef = useRef(0)
+  const initialViewportRef = useRef<CornerstoneViewport>() // use as initial viewport on reset.
+
+  useEffect(() => {
+    if (!image) return
+    loadCountRef.current += 1
+  }, [image])
 
   useEffect(() => {
     if (!image || !element) return
 
-    loadCountRef.current += 1 // This is 1 unless the image is not changed.
     // viewport and defaultViewport from dispalyImage() are same when there is no user-defined default value.
     const { viewport, defaultViewport } = displayImage(
       element,
       image,
       loadCountRef.current === 1 // This is the first time to display the image.
         ? viewportRef?.current?._initialViewport // Use the user-defined default value.
-        : viewportRef?.current // Use the current viewport prop of Viewer(for multiframe images).
+        : initialViewportRef?.current // reset viewport
+      // : viewportRef?.current // Use the current viewport prop of Viewer(for multiframe images).
     )
     // Sets the default viewport for later use on resetting.
     initialViewportMessage.sendMessage(defaultViewport)
+    if (loadCountRef.current === 1) initialViewportRef.current = viewport
     // Updates viewport prop of Viewer after setting the image object.
     if (onViewportChange) {
       onViewportChange(formatViewport(viewport))
