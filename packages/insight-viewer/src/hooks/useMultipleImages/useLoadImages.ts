55d76a49f35b0587201555df9156e27a2db39cea
skpark-lunit <skpark@lunit.io>
2021-12-27 18:36:11 +0900

    feat(viewer): add _persistentViewport property to loaded image value for multiframe image reset

--- packages/insight-viewer/src/hooks/useMultipleImages/useLoadImages.ts
+++ packages/insight-viewer/src/hooks/useMultipleImages/useLoadImages.ts
@@ -1,19 +1,23 @@
-import { useEffect, useState } from 'react'
-import { CornerstoneImage } from '../../utils/cornerstoneHelper'
+import { useEffect, useState, useRef } from 'react'
+import { noop } from '../../utils/common'
 import { LOADING_STATE } from '../../const'
 import { useImageLoader } from '../useImageLoader'
 import { loadImages } from './loadImages'
 import { HTTP, LoadingState, ImageId } from '../../types'
-import { Loaded, ImagesLoadState } from './types'
+import { Image } from '../../Viewer/types'
+import { Loaded, ImagesLoadState, OnImagesLoaded } from './types'
 import { getLoadingStateMap, updateLoadedStates } from './loadingStates'
 import { getImageIdsAndScheme } from './getImageIdsAndScheme'
 
 interface UseLoadImages {
-  ({ onError, requestInterceptor, ...rest }: HTTP & ImageId): ImagesLoadState
+  ({
+    onError,
+    requestInterceptor,
+    ...rest
+  }: HTTP & ImageId & { onImagesLoaded?: OnImagesLoaded }): ImagesLoadState
 }
 
 interface State {
-  images: CornerstoneImage[]
   loadingStates: Map<number, LoadingState>
   _currentIndex: number
 }
@@ -31,24 +35,27 @@ interface State {
 export const useLoadImages: UseLoadImages = ({
   onError,
   requestInterceptor,
+  onImagesLoaded = noop,
   ...rest
 }) => {
   const { ids: imageIds, scheme: imageScheme } = getImageIdsAndScheme(rest)
-  const [{ images, loadingStates }, setState] = useState<State>({
-    images: [],
+  const [{ loadingStates }, setState] = useState<State>({
     loadingStates: getLoadingStateMap(
       Array.isArray(imageIds) ? imageIds.length : 0
     ),
     _currentIndex: -1,
   })
-
+  // The "_persistentViewport" flag is used for persisting viewport when the image frame is changed.
+  // When all images are loaded, the "_persistentViewport" is changed to true. It makes the viewport is persistent.
+  // Before all images are loaded, the "_persistentViewport" is false. The viewport is reset when the Multiframe image case is changed.
+  const imagesRef = useRef<Image[]>([])
   const hasLoader = useImageLoader(rest, onError)
 
   useEffect(() => {
     if (!imageIds || imageIds.length === 0 || !imageScheme) return // No images to load
     if (!hasLoader) return // No image loader
-
     // Initialize first image loading state.
+    imagesRef.current = []
     setState((prev: State) => ({
       ...prev,
       loadingStates: prev.loadingStates.set(0, LOADING_STATE.LOADING),
@@ -57,8 +64,11 @@ export const useLoadImages: UseLoadImages = ({
     loadImages({ images: imageIds, imageScheme, requestInterceptor }).subscribe(
       {
         next: ({ image, loaded }: Loaded) => {
+          imagesRef.current = [
+            ...imagesRef.current,
+            { ...image, _persistentViewport: false },
+          ]
           setState((prev: State) => ({
-            images: [...prev.images, image],
             loadingStates: updateLoadedStates({
               size: imageIds.length,
               stateMap: prev.loadingStates,
@@ -77,12 +87,31 @@ export const useLoadImages: UseLoadImages = ({
             ),
           }))
         },
+        complete: () => {
+          setTimeout(() => {
+            imagesRef.current = [...imagesRef.current].map(
+              image =>
+                <Image>{
+                  ...image,
+                  _persistentViewport: true,
+                }
+            )
+          }, 0)
+          onImagesLoaded()
+        },
       }
     )
-  }, [imageIds, imageScheme, onError, requestInterceptor, hasLoader])
+  }, [
+    imageIds,
+    imageScheme,
+    onError,
+    onImagesLoaded,
+    requestInterceptor,
+    hasLoader,
+  ])
 
   return {
-    images,
+    images: imagesRef.current,
     loadingStates: Array.from(loadingStates.values()),
   }
 }
