fefb11838b2388f46e1c881c662cf42325282cbf
Takhyun Kim <kimkih1218@lunit.io>
2022-01-04 17:37:42 +0900

    feat(viewer): add SvgContourDrawer component

---
+++ packages/insight-viewer/src/Viewer/SvgContourDrawer/index.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import React, { useRef } from 'react'
+import { useOverlayContext } from '../../contexts'
+import { Contour, Point } from '../../types'
+import { svgStyle, polyline } from './SvgContourDrawer.styles'
+import { SvgContourDrawerProps } from './SvgContourDrawer.types'
+import useSvgContourDrawing from '../../hooks/useSvgContourDrawing'
+
+export function SvgContourDrawer<T extends Contour>({
+  draw,
+  style,
+  width,
+  height,
+  device,
+  contours,
+  className,
+  animateStroke,
+  onAdd,
+  onFocus,
+  onRemove,
+}: SvgContourDrawerProps<T>): JSX.Element {
+  const svgRef = useRef<SVGSVGElement>(null)
+  const [polygon] = useSvgContourDrawing({
+    svgElement: svgRef,
+    draw,
+    device,
+    contours,
+    animateStroke,
+    onAdd,
+    onFocus,
+    onRemove,
+  })
+  const { pixelToCanvas } = useOverlayContext()
+
+  const transformedPoints: Point[] = polygon.map(point => pixelToCanvas(point))
+  const polygonPoints = transformedPoints.map(([x, y]) => `${x},${y}`).join(' ')
+
+  return (
+    <svg
+      ref={svgRef}
+      width={width}
+      height={height}
+      style={{ ...svgStyle.default, ...style }}
+      className={className}
+    >
+      {polygon && polygon.length > 0 && (
+        <>
+          <polyline style={polyline.default} points={polygonPoints} />
+          <polyline style={polyline.highlight} points={polygonPoints} />
+        </>
+      )}
+    </svg>
+  )
+}
