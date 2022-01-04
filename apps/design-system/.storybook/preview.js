61c6c563c98a461e2cbbffeedad44509a05d7f75
SangYeob Yu <syyu@lunit.io>
2022-01-04 10:42:31 +0900

    feat(design-system): add product theme button on toolbar

--- apps/design-system/.storybook/preview.js
+++ apps/design-system/.storybook/preview.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from "@mui/material/CssBaseline";
-import {mmg} from "@lunit/mui-theme"
+import {cxr, mmg, scope} from "@lunit/mui-theme"
 
-const theme = createTheme(mmg);
+export const globalTypes = {
+  theme: {
+    name: 'Product Theme',
+    description: 'Global theme for components',
+    defaultValue: 'CXR',
+    toolbar: {
+      icon: 'paintbrush',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['CXR', 'MMG', 'SCOPE'],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
+
+const getTheme = (themeName) => {
+  if (themeName === 'CXR') {
+    return cxr;
+  } else if(themeName === 'MMG') {
+    return mmg;
+  } else if(themeName === 'SCOPE') {
+    return scope;
+  }
+  return cxr
+}
 export const decorators = [
-  Story => (
+  (Story, context) => {
+    const theme = createTheme(getTheme(context.globals.theme));
+    return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Story />
     </ThemeProvider>
-  ),
+  )},
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+  actions: { argTypesRegex: "^on[A-Z].*" }
 }
