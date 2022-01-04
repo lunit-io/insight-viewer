a3f39411fd199c9b409fe3f3df4fae74fc1e5f9d
Takhyun Kim <kimkih1218@lunit.io>
2022-01-04 16:45:54 +0900

    feat(viewer): add SvgContourDrawer props interface

---
+++ packages/insight-viewer/src/Viewer/SvgContourDrawer/SvgContourDrawer.types.ts
@@ -0,0 +1,42 @@
+import { CSSProperties } from 'react'
+import { Contour, Point } from '../../types'
+
+export interface SvgContourDrawerProps<T extends Contour> {
+  width?: number
+  height?: number
+
+  contours?: T[]
+
+  /**
+   * Whether the drawing function is enabled
+   *
+   * When inputting as HTMLElement, MouseEvent is handled using the HTMLElement
+   */
+  draw?: boolean | HTMLElement | null
+
+  /**
+   * When Mouse Over on a Contour
+   * Required to determine focusedContour
+   */
+  onFocus: (contour: T | null) => void
+
+  /** When drawing is complete and a new contour occurs */
+  onAdd: (polygon: Point[]) => void
+
+  /** Needed to delete a specific contour by clicking on it */
+  onRemove: (contour: T) => void
+
+  className?: string
+  style?: CSSProperties
+
+  /** In the process of drawing, you can deactivate the animation expressed on the line */
+  animateStroke?: boolean
+
+  /** Access Device Settings */
+  device?:
+    | 'all'
+    | 'mouse-only'
+    | 'touch-only'
+    | 'stylus-only'
+    | 'mouse-and-stylus'
+}
