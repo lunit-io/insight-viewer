3e58e864bf8a0a77bb7e548c9bb285212f777f8b
Takhyun Kim <kimkih1218@lunit.io>
2022-01-17 13:47:15 +0900

    feat(viewer): delete canvasRef constant in HeatmapViewer component

--- packages/insight-viewer/src/Viewer/HeatmapViewer/index.tsx
+++ packages/insight-viewer/src/Viewer/HeatmapViewer/index.tsx
@@ -1,17 +1,14 @@
-import React, { useRef, ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 
 import useHeatmapDrawing from '../../hooks/useHeatmapDrawing'
 
 import { style } from './HeatmapViewer.styles'
 import { HeatmapViewerProps } from './HeatmapViewer.types'
 
-export function HeatmapViewer({
-  posMap,
-  threshold,
-}: HeatmapViewerProps): ReactElement<HTMLCanvasElement> {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-
-  useHeatmapDrawing({ posMap, threshold, baseCanvas: canvasRef?.current })
+export function HeatmapViewer(
+  props: HeatmapViewerProps
+): ReactElement<HTMLCanvasElement> {
+  const [canvasRef] = useHeatmapDrawing(props)
 
   return <canvas data-cy-name="heatmap-canvas" ref={canvasRef} style={style} />
 }
