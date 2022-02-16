3b9167fc4321df3c482024c1ce22326b97368b14
SangYeob Yu <syyu@lunit.io>
2022-02-16 14:37:40 +0900

    feat(viewer): add loader options to useImage hook

--- packages/insight-viewer/src/hooks/useImageLoader/index.ts
+++ packages/insight-viewer/src/hooks/useImageLoader/index.ts
@@ -4,21 +4,27 @@
 import { useState } from 'react'
 import { LOADER_TYPE } from '../../const'
 import { OnError, ImageId } from '../../types'
-import setWadoImageLoader from '../../utils/cornerstoneHelper/setWadoImageLoader'
+import setWadoImageLoader, {
+  WadoImageLoaderOptions,
+} from '../../utils/cornerstoneHelper/setWadoImageLoader'
 import setWebImageLoader from '../../utils/cornerstoneHelper/setWebImageLoader'
 import { getLoaderType } from './getLoaderType'
 
-export function useImageLoader(imageId: ImageId, onError: OnError): boolean {
+export function useImageLoader(
+  imageId: ImageId,
+  onError: OnError,
+  options?: WadoImageLoaderOptions
+): boolean {
   const [hasLoader, setHasLoader] = useState(false)
   const loaderType = getLoaderType(imageId)
-
-  const loader =
-    loaderType === LOADER_TYPE.DICOM ? setWadoImageLoader : setWebImageLoader
 
   // eslint-disable-next-line no-extra-semi
   ;(async function asyncLoad(): Promise<void> {
     if (!hasLoader) {
-      const imageLoader = await loader(onError)
+      const imageLoader =
+        loaderType === LOADER_TYPE.DICOM
+          ? await setWadoImageLoader(onError, options)
+          : await setWebImageLoader(onError)
       setHasLoader(!!imageLoader)
     }
   })()
