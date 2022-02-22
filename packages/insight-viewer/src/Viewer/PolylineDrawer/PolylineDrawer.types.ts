70db2b57555799e101bf9a9afe53d5ab93a71cf9
Takhyun Kim <kimkih1218@lunit.io>
2022-02-22 12:39:21 +0900

    feat(viewer): add PolylineDrawer component

---
+++ packages/insight-viewer/src/Viewer/PolylineDrawer/PolylineDrawer.types.ts
@@ -0,0 +1,5 @@
+import { Point } from '../../types'
+
+export interface PolylineDrawerProps {
+  polygon: Point[]
+}
---
+++ packages/insight-viewer/src/Viewer/PolylineDrawer/index.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react'
+
+import { useOverlayContext } from '../../contexts'
+import { PolylineDrawerProps } from './PolylineDrawer.types'
+import { polyline } from '../AnnotationDrawer/AnnotationDrawer.styles'
+
+export function PolylineDrawer({ polygon }: PolylineDrawerProps): ReactElement {
+  const { pixelToCanvas } = useOverlayContext()
+
+  const polygonPoints = polygon
+    .map(point => {
+      const [x, y] = pixelToCanvas(point)
+      return `${x},${y}`
+    })
+    .join(' ')
+
+  return (
+    <>
+      {polygon && polygon.length > 0 && (
+        <>
+          <polyline style={polyline.default} points={polygonPoints} />
+          <polyline style={polyline.highlight} points={polygonPoints} />
+        </>
+      )}
+    </>
+  )
+}
