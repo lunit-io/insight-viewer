f807c841763d434457de8f809644764dada4ce08
Takhyun Kim <kimkih1218@lunit.io>
2022-02-22 14:35:47 +0900

    docs(viewer-docs): add polygon, line viewer drawer tab

--- apps/insight-viewer-docs/src/containers/UseOverlayContext/Tabs.tsx
+++ apps/insight-viewer-docs/src/containers/UseOverlayContext/Tabs.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Heatmap from './Heatmap'
 import Contour from './Contour'
-import SvgContourContainer from './SvgContourViewer'
-import SvgContourDrawerContainer from './SvgContourDrawer'
+import LineContainer from './Line'
+import LineDrawerContainer from './LineDrawer'
+import PolygonContainer from './PolygonViewer'
+import PolygonDrawerContainer from './PolygonDrawer'
 
 export default function ProgressTabs(): JSX.Element {
   const [active, setActive] = useState(0)
@@ -17,8 +19,10 @@ export default function ProgressTabs(): JSX.Element {
       <TabList>
         <Tab data-cy-tab="heatmap">Heatmap</Tab>
         <Tab data-cy-tab="contour">Contour</Tab>
-        <Tab data-cy-tab="viewer">SvgContourViewer</Tab>
-        <Tab data-cy-tab="drawer">SvgContourDrawer</Tab>
+        <Tab data-cy-tab="viewer">Polygon</Tab>
+        <Tab data-cy-tab="drawer">PolygonDrawer</Tab>
+        <Tab data-cy-tab="Line">Line</Tab>
+        <Tab data-cy-tab="LineDrawer">LineDrawer</Tab>
       </TabList>
 
       <TabPanels>
@@ -29,10 +33,16 @@ export default function ProgressTabs(): JSX.Element {
           {active === 1 && <Contour />}
         </TabPanel>
         <TabPanel p={0} pt={6}>
-          {active === 2 && <SvgContourContainer />}
+          {active === 2 && <PolygonContainer />}
         </TabPanel>
         <TabPanel p={0} pt={6}>
-          {active === 3 && <SvgContourDrawerContainer />}
+          {active === 3 && <PolygonDrawerContainer />}
+        </TabPanel>
+        <TabPanel p={0} pt={6}>
+          {active === 4 && <LineContainer />}
+        </TabPanel>
+        <TabPanel p={0} pt={6}>
+          {active === 5 && <LineDrawerContainer />}
         </TabPanel>
       </TabPanels>
     </Tabs>
