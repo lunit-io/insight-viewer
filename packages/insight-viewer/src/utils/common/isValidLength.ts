46e778c97f284cb871428bb8bf9c90a922b056cc
오승현 <oshyun6623@gmail.com>
2022-03-04 17:13:09 +0900

    fix(viewer): modify valid line checker

--- packages/insight-viewer/src/utils/common/isValidLength.ts
+++ packages/insight-viewer/src/utils/common/isValidLength.ts
@@ -2,15 +2,19 @@ import { Point } from '../../types'
 
 const MINIMUM_LENGTH = 100
 
-export function isValidLength(line: Point[], minimumLength = MINIMUM_LENGTH): boolean {
+export function isValidLength(
+  line: Point[],
+  minimumLength = MINIMUM_LENGTH
+): boolean {
   // Calculate the length of a line and verify whether it is shorter than minimum.
 
   if (line.length === 1) return false
 
-  const [firstPoint, secondPoint] = line
+  const startPoint = line[0]
+  const endPoint = line[line.length - 1]
 
-  const x = firstPoint[0] - secondPoint[0]
-  const y = firstPoint[1] - secondPoint[1]
+  const x = startPoint[0] - endPoint[0]
+  const y = startPoint[1] - endPoint[1]
 
   const length = Math.sqrt(x ** 2 + y ** 2)
 
