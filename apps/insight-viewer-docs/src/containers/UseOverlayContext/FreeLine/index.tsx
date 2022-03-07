49c699ae65e290115940c5c086242398fb4bbe20
Takhyun Kim <kimkih1218@lunit.io>
2022-03-07 17:52:00 +0900

    docs(viewer-docs): add onFocus, onRemove to Viewer docs

--- apps/insight-viewer-docs/src/containers/UseOverlayContext/FreeLine/index.tsx
+++ apps/insight-viewer-docs/src/containers/UseOverlayContext/FreeLine/index.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-unresolved */
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React, { useState, ChangeEvent } from 'react'
+import { Box, Switch } from '@chakra-ui/react'
 import { Resizable } from 're-resizable'
-import InsightViewer, { useImage, useViewport, AnnotationOverlay } from '@lunit/insight-viewer'
+import InsightViewer, {
+  useAnnotation,
+  useImage,
+  useViewport,
+  AnnotationOverlay,
+  Annotation,
+} from '@lunit/insight-viewer'
 import { IMAGES } from '../../../const'
 import { initialAnnotation } from '../../../../mocks/freeLines'
 
@@ -16,22 +22,36 @@ const style = {
 const DEFAULT_SIZE = { width: 700, height: 700 }
 
 function FreeLineContainer(): JSX.Element {
+  const [isEdit, setIsEdit] = useState(false)
   const { loadingState, image } = useImage({
     wadouri: IMAGES[12],
   })
   const { viewport, setViewport } = useViewport()
+  const { annotations, focusedAnnotation, removeAnnotation, focusAnnotation } = useAnnotation<Annotation>({
+    mode: 'freeLine',
+    initalAnnotation: initialAnnotation,
+  })
+
+  const handleEditSwitchClick = (event: ChangeEvent<HTMLInputElement>) => {
+    setIsEdit(event.target.checked)
+  }
 
   return (
     <Box data-cy-loaded={loadingState}>
+      <Box>
+        isEdit <Switch onChange={handleEditSwitchClick} className="hflip-control" isChecked={isEdit} />
+      </Box>
       <Resizable style={style} defaultSize={DEFAULT_SIZE}>
         <InsightViewer image={image} viewport={viewport} onViewportChange={setViewport}>
           {loadingState === 'success' && (
             <AnnotationOverlay
               width={700}
               height={700}
-              annotations={initialAnnotation}
-              focusedAnnotation={null}
+              annotations={annotations}
+              focusedAnnotation={focusedAnnotation}
               mode="freeLine"
+              onFocus={isEdit ? focusAnnotation : undefined}
+              onRemove={isEdit ? removeAnnotation : undefined}
             />
           )}
         </InsightViewer>
--- apps/insight-viewer-docs/src/containers/UseOverlayContext/PolygonViewer/index.tsx
+++ apps/insight-viewer-docs/src/containers/UseOverlayContext/PolygonViewer/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React, { useState, ChangeEvent } from 'react'
+import { Box, Switch } from '@chakra-ui/react'
 import { Resizable } from 're-resizable'
 import InsightViewer, {
   AnnotationOverlay,
@@ -25,18 +25,26 @@ const style = {
 const DEFAULT_SIZE = { width: 500, height: 500 }
 
 function PolygonContainer(): JSX.Element {
+  const [isEdit, setIsEdit] = useState(false)
   const { loadingState, image } = useImage({
     wadouri: IMAGES[12],
   })
   const { viewport, setViewport } = useViewport()
-  const { annotations, focusedAnnotation } = useAnnotation<Annotation>({
+  const { annotations, focusedAnnotation, removeAnnotation, focusAnnotation } = useAnnotation<Annotation>({
     mode: 'polygon',
     initalAnnotation: ANNOTATIONS,
   })
 
+  const handleEditSwitchClick = (event: ChangeEvent<HTMLInputElement>) => {
+    setIsEdit(event.target.checked)
+  }
+
   return (
     <>
       <Box data-cy-loaded={loadingState}>
+        <Box>
+          isEdit <Switch onChange={handleEditSwitchClick} className="hflip-control" isChecked={isEdit} />
+        </Box>
         <Resizable style={style} defaultSize={DEFAULT_SIZE}>
           <InsightViewer image={image} viewport={viewport} onViewportChange={setViewport}>
             {loadingState === 'success' && (
@@ -48,6 +56,8 @@ function PolygonContainer(): JSX.Element {
                 annotationAttrs={getPolygonStyles}
                 showAnnotationLabel
                 mode="polygon" // If no mode is defined, the default value is polygon.
+                onFocus={isEdit ? focusAnnotation : undefined}
+                onRemove={isEdit ? removeAnnotation : undefined}
               />
             )}
           </InsightViewer>
