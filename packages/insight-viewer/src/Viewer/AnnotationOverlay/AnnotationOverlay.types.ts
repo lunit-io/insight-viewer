20a761afffdca690ba508b5fc80336907116dbe5
Takhyun Kim <kimkih1218@lunit.io>
2022-03-10 20:38:41 +0900

    feat(viewer): apply AnnotationLayer to Drawer, Overlay

--- packages/insight-viewer/src/Viewer/AnnotationOverlay/AnnotationOverlay.types.ts
+++ packages/insight-viewer/src/Viewer/AnnotationOverlay/AnnotationOverlay.types.ts
@@ -1,11 +1,11 @@
-import { Annotation, HeadType, Point } from '../../types'
+import { Annotation, AnnotationLayer, HeadType } from '../../types'
 import { AnnotationViewerProps } from '../AnnotationViewer/AnnotationViewer.types'
 
 export interface AnnotationOverlayProps<T extends Annotation> extends AnnotationViewerProps<T> {
   isDrawing?: boolean
   head?: HeadType
   onFocus?: (annotation: T | null) => void
-  onAdd?: (polygon: Point[]) => void
+  onAdd?: (layer: AnnotationLayer) => void
   onRemove?: (annotation: T) => void
   device?: 'all' | 'mouse-only' | 'touch-only' | 'stylus-only' | 'mouse-and-stylus'
 }
