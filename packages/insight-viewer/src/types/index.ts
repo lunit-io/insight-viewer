f01f0cac8d268fc3e273afa4f6e163d0d1f726e0
오승현 <sh5h@lunit.io>
2022-03-11 13:54:30 +0900

    feat(viewer): remove arrowLine from annotationMode and add HeadType

--- packages/insight-viewer/src/types/index.ts
+++ packages/insight-viewer/src/types/index.ts
@@ -85,4 +85,5 @@ export type AnnotationStyle = {
   [styleType in AnnotationStyleType]?: CSSProperties
 }
 
-export type AnnotationMode = 'line' | 'freeLine' | 'arrowLine' | 'polygon' | 'circle'
+export type AnnotationMode = 'line' | 'freeLine' | 'polygon' | 'circle'
+export type HeadType = 'normal' | 'arrow'
