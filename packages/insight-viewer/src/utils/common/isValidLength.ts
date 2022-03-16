0b8f75acc8154b2c5cfbdf25dbeb6c5804b20cec
Takhyun Kim <kimkih1218@lunit.io>
2022-03-16 23:35:59 +0900

    feat(viewer): change line parameter type, unused logic to isValidLength

--- packages/insight-viewer/src/utils/common/isValidLength.ts
+++ packages/insight-viewer/src/utils/common/isValidLength.ts
@@ -2,13 +2,10 @@ import { Point } from '../../types'
 
 const MINIMUM_LENGTH = 100
 
-export function isValidLength(line: Point[], minimumLength = MINIMUM_LENGTH): boolean {
+export function isValidLength(line: [Point, Point], minimumLength = MINIMUM_LENGTH): boolean {
   // Calculate the length of a line and verify whether it is shorter than minimum.
 
-  if (line.length === 1) return false
-
-  const startPoint = line[0]
-  const endPoint = line[line.length - 1]
+  const [startPoint, endPoint] = line
 
   const x = startPoint[0] - endPoint[0]
   const y = startPoint[1] - endPoint[1]
