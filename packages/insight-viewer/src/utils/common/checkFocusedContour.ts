cfdcc774c748ef359461413c50399e1b4188445a
Takhyun Kim <kimkih1218@lunit.io>
2022-03-17 14:52:30 +0900

    test(viewer): rename polygon to contour

--- packages/insight-viewer/src/utils/common/checkFocusedContour.ts
+++
@@ -1,8 +0,0 @@
-import pointInPolygon from 'point-in-polygon'
-import { Annotation, Point } from '../../types'
-
-export function checkFocusedContour<T extends Annotation>(contours: T[], cursor: Point): T | null {
-  const result: T | undefined = contours.find(contour => pointInPolygon(cursor, contour.layer.points))
-
-  return result || null
-}
---
+++ packages/insight-viewer/src/utils/common/checkFocusedPolygon.ts
@@ -0,0 +1,8 @@
+import pointInPolygon from 'point-in-polygon'
+import { PolygonAnnotation, Point } from '../../types'
+
+export function checkFocusedPolygon(contours: PolygonAnnotation[], cursor: Point): PolygonAnnotation | null {
+  const result: PolygonAnnotation | undefined = contours.find(contour => pointInPolygon(cursor, contour.points))
+
+  return result || null
+}
