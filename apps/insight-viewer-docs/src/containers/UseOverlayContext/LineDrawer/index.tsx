3fe2d93da4e2b1aceb84e1395e600665ac4757bc
Takhyun Kim <kimkih1218@lunit.io>
2022-03-17 11:39:00 +0900

    docs(viewer): apply head mode radio button to LineDrawer docs

--- apps/insight-viewer-docs/src/containers/UseOverlayContext/LineDrawer/index.tsx
+++ apps/insight-viewer-docs/src/containers/UseOverlayContext/LineDrawer/index.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-unresolved */
-import React from 'react'
-import { Box, Button } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Button, Radio, RadioGroup, Stack } from '@chakra-ui/react'
 import { Resizable } from 're-resizable'
-import InsightViewer, { AnnotationOverlay, useImage, useAnnotation, useViewport } from '@lunit/insight-viewer'
+import InsightViewer, {
+  AnnotationOverlay,
+  useImage,
+  useAnnotation,
+  useViewport,
+  LineHeadMode,
+} from '@lunit/insight-viewer'
 import { IMAGES } from '../../../const'
 import { getPolygonStyles } from '../../../utils/common/getPolygonStyles'
 
@@ -17,6 +23,7 @@ const DEFAULT_SIZE = { width: 700, height: 700 }
 const MODE = 'line'
 
 function LineDrawerContainer(): JSX.Element {
+  const [headMode, setHeadMode] = useState<LineHeadMode>('normal')
   const { loadingState, image } = useImage({
     wadouri: IMAGES[12],
   })
@@ -24,17 +31,29 @@ function LineDrawerContainer(): JSX.Element {
   const { annotations, selectedAnnotation, addAnnotation, removeAnnotation, selectAnnotation, removeAllAnnotation } =
     useAnnotation({ mode: MODE })
 
+  const handleClickHeadModeRadio = (mode: LineHeadMode) => {
+    setHeadMode(mode)
+  }
+
   return (
     <>
       <Button data-cy-name="remove-button" marginBottom="10px" colorScheme="blue" onClick={removeAllAnnotation}>
         remove all
       </Button>
+      <RadioGroup onChange={handleClickHeadModeRadio} value={headMode}>
+        <Stack direction="row">
+          <p style={{ marginRight: '10px' }}>Select Head mode</p>
+          <Radio value="normal">normal</Radio>
+          <Radio value="arrow">arrow</Radio>
+        </Stack>
+      </RadioGroup>
       <Box data-cy-loaded={loadingState}>
         <Resizable style={style} defaultSize={DEFAULT_SIZE}>
           <InsightViewer image={image} viewport={viewport} onViewportChange={setViewport}>
             {loadingState === 'success' && (
               <AnnotationOverlay
                 isDrawing
+                lineHead={headMode}
                 mode={MODE}
                 showAnnotationLabel={false}
                 width={DEFAULT_SIZE.width}
