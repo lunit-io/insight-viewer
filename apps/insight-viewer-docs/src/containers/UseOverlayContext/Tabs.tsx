d33cbb063e784983dbfdafb91b33e5bb226ef8a7
Takhyun Kim <kimkih1218@lunit.io>
2022-03-18 11:02:24 +0900

    docs(viewer-docs): delete unused annotation mode docs in overlayContext

--- apps/insight-viewer-docs/src/containers/UseOverlayContext/Tabs.tsx
+++ apps/insight-viewer-docs/src/containers/UseOverlayContext/Tabs.tsx
@@ -2,12 +2,6 @@ import { useState } from 'react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Heatmap from './Heatmap'
 import Contour from './Contour'
-import PolygonContainer from './PolygonViewer'
-import PolygonDrawerContainer from './PolygonDrawer'
-import FreeLineContainer from './FreeLine'
-import FreeLineDrawerContainer from './FreeLineDrawer'
-import LineContainer from './LineViewer'
-import LineDrawerContainer from './LineDrawer'
 
 export default function ProgressTabs(): JSX.Element {
   const [active, setActive] = useState(0)
@@ -21,12 +15,6 @@ export default function ProgressTabs(): JSX.Element {
       <TabList>
         <Tab data-cy-tab="heatmap">Heatmap</Tab>
         <Tab data-cy-tab="contour">Contour</Tab>
-        <Tab data-cy-tab="viewer">Polygon</Tab>
-        <Tab data-cy-tab="drawer">PolygonDrawer</Tab>
-        <Tab data-cy-tab="FreeLine">FreeLine</Tab>
-        <Tab data-cy-tab="FreeLineDrawer">FreeLineDrawer</Tab>
-        <Tab data-cy-tab="LineViewer">LineViewer</Tab>
-        <Tab data-cy-tab="LineDrawer">LineDrawer</Tab>
       </TabList>
 
       <TabPanels>
@@ -35,24 +23,6 @@ export default function ProgressTabs(): JSX.Element {
         </TabPanel>
         <TabPanel p={0} pt={6}>
           {active === 1 && <Contour />}
-        </TabPanel>
-        <TabPanel p={0} pt={6}>
-          {active === 2 && <PolygonContainer />}
-        </TabPanel>
-        <TabPanel p={0} pt={6}>
-          {active === 3 && <PolygonDrawerContainer />}
-        </TabPanel>
-        <TabPanel p={0} pt={6}>
-          {active === 4 && <FreeLineContainer />}
-        </TabPanel>
-        <TabPanel p={0} pt={6}>
-          {active === 5 && <FreeLineDrawerContainer />}
-        </TabPanel>
-        <TabPanel p={0} pt={7}>
-          {active === 6 && <LineContainer />}
-        </TabPanel>
-        <TabPanel p={0} pt={8}>
-          {active === 7 && <LineDrawerContainer />}
         </TabPanel>
       </TabPanels>
     </Tabs>
