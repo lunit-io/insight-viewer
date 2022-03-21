4641bb89b0cb7d254b78324ee70b95de2e4a5d46
Takhyun Kim <kimkih1218@lunit.io>
2022-03-21 21:28:58 +0900

    feat(viewer): add MeasurementViewer component, type, style

---
+++ packages/insight-viewer/src/Viewer/MeasurementViewer/MeasurementViewer.styles.ts
@@ -0,0 +1,50 @@
+import { ViewerStyle } from '../../types'
+
+export const svgStyle: ViewerStyle = {
+  default: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+}
+
+export const polylineStyle: ViewerStyle = {
+  default: {
+    strokeWidth: '3px',
+    stroke: '#ffffff',
+    transition: 'stroke 120ms ease-out, stroke-width 120ms ease-out',
+    fill: 'none',
+  },
+  outline: {
+    fill: 'none',
+    strokeWidth: '3px',
+    stroke: '#000000',
+  },
+  select: {
+    stroke: 'rgb(255, 194, 17)',
+    fill: 'none',
+    strokeWidth: '3px',
+  },
+}
+
+export const textStyle: ViewerStyle = {
+  default: {
+    fill: '#ffffff',
+    fontWeight: 600,
+    textAnchor: 'middle',
+  },
+  outline: {
+    fill: 'transparent',
+    fontWeight: 600,
+    textAnchor: 'middle',
+    stroke: '#000000',
+    strokeWidth: '2.5px',
+  },
+  select: {
+    fill: 'rgb(255, 194, 17)',
+    fontWeight: 600,
+    textAnchor: 'middle',
+  },
+}
---
+++ packages/insight-viewer/src/Viewer/MeasurementViewer/MeasurementViewer.types.ts
@@ -0,0 +1,49 @@
+import { CSSProperties, SVGProps } from 'react'
+import { Point, MeasurementMode, Measurement } from '../../types'
+
+export interface MeasurementViewerProps {
+  width?: number
+  height?: number
+
+  /** Measurement focused by user interaction such as mouse over */
+  measurements: Measurement[]
+
+  selectedMeasurement: Measurement | null
+
+  /** <svg className={}> */
+  className?: string
+
+  /** <svg style={}> */
+  style?: CSSProperties
+
+  /**
+   * You can set the attributes of individual measurement objects
+   * If you set properties such as strokeWidth, the Style set in Styled Components is ignored
+   * It is best not to use it as much as possible except in special cases
+   */
+  measurementAttrs?: (measurement: Measurement, showOutline: boolean) => SVGProps<SVGPolygonElement>
+
+  onFocus?: (measurement: Measurement | null) => void
+  onRemove?: (measurement: Measurement) => void
+
+  /**
+   * Draw an outline on the line
+   * Since the outline is expressed by drawing two lines, it can be deactivated in a performance-sensitive situation
+   */
+  showOutline?: boolean
+
+  /**
+   * measurement label notation flag variable
+   * Default value is false
+   */
+  showMeasurementLabel?: boolean
+
+  mode?: MeasurementMode
+}
+
+export interface MeasurementsDrawProps extends Omit<MeasurementViewerProps, 'width' | 'height'> {
+  mode: MeasurementMode
+  showOutline: boolean
+  showMeasurementLabel: boolean
+  pixelToCanvas: (point: Point) => Point
+}
