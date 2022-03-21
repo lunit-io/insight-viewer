408be48971354bca34ae35d6c6f646631eef1c2e
Takhyun Kim <kimkih1218@lunit.io>
2022-03-21 21:37:28 +0900

    docs(viewer-docs): add Measurement container, Tab

---
+++ apps/insight-viewer-docs/src/containers/Measurement/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import Tabs from './Tab'
+
+export default function Measurement(): JSX.Element {
+  return (
+    <Box mb={6}>
+      <Tabs />
+    </Box>
+  )
+}
