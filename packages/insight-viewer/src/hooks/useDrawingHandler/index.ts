a5533234d6677f887f43acc22082bf34b152582c
Takhyun Kim <kimkih1218@lunit.io>
2022-03-28 17:25:17 +0900

    feat(viewer): useDrawingHandler hook for handle mouse event

---
+++ packages/insight-viewer/src/hooks/useDrawingHandler/index.ts
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import { useEffect } from 'react'
+import { UseDrawingHandlerProps } from './types'
+import { useOverlayContext } from '../../contexts'
+
+const setPreProcessEvent = (event: MouseEvent | KeyboardEvent) => {
+  event.preventDefault()
+  event.stopPropagation()
+  event.stopImmediatePropagation()
+}
+
+function useDrawingHandler({
+  mode,
+  svgElement,
+  addStartPoint,
+  addDrawingPoint,
+  cancelDrawing,
+  addDrewMeasurement,
+}: UseDrawingHandlerProps): void {
+  const { pageToPixel, enabledElement } = useOverlayContext()
+
+  useEffect(() => {
+    if (!svgElement || !enabledElement) return
+
+    const handleMouseMove = (event: MouseEvent) => {
+      setPreProcessEvent(event)
+
+      const pixelPoint = pageToPixel([event.pageX, event.pageY])
+
+      addDrawingPoint(pixelPoint)
+    }
+
+    const handleMouseUp = (event: MouseEvent) => {
+      setPreProcessEvent(event)
+      deactivateMouseDrawEvents()
+
+      addDrewMeasurement()
+    }
+
+    const handleMouseLeave = (event: MouseEvent) => {
+      setPreProcessEvent(event)
+      deactivateMouseDrawEvents()
+
+      cancelDrawing()
+    }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === 'Escape') {
+        setPreProcessEvent(event)
+        deactivateMouseDrawEvents()
+
+        cancelDrawing()
+      }
+    }
+
+    const handleMouseDown = (event: MouseEvent) => {
+      activeMouseDrawEvents()
+
+      const pixelPoint = pageToPixel([event.pageX, event.pageY])
+
+      addStartPoint(pixelPoint)
+    }
+
+    const activeMouseDrawEvents = () => {
+      if (!enabledElement || !enabledElement.element) return
+
+      enabledElement.element.addEventListener('mousemove', handleMouseMove)
+      enabledElement.element.addEventListener('mouseup', handleMouseUp)
+      enabledElement.element.addEventListener('mouseleave', handleMouseLeave)
+      enabledElement.element.addEventListener('mousedown', handleMouseDown)
+      window.addEventListener('keydown', handleKeyDown)
+    }
+
+    const deactivateMouseDrawEvents = () => {
+      if (!enabledElement || !enabledElement.element) return
+
+      enabledElement.element.removeEventListener('mousemove', handleMouseMove)
+      enabledElement.element.removeEventListener('mouseup', handleMouseUp)
+      enabledElement.element.removeEventListener('mouseleave', handleMouseLeave)
+      enabledElement.element.removeEventListener('mousedown', handleMouseDown)
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+
+    activeMouseDrawEvents()
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      deactivateMouseDrawEvents()
+    }
+  }, [mode, svgElement, enabledElement, pageToPixel, addDrawingPoint, addDrewMeasurement, cancelDrawing, addStartPoint])
+}
+
+export default useDrawingHandler
