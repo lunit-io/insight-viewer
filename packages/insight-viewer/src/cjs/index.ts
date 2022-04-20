ae0c70a5fa25990690bd8f929591aed8a474f195
SangYeob Yu <syyu@lunit.io>
2022-04-20 21:39:07 +0900

    fix(viewer): missed exports on cjs build

--- packages/insight-viewer/src/cjs/index.ts
+++ packages/insight-viewer/src/cjs/index.ts
@@ -2,29 +2,57 @@ import { InsightViewer } from '../Viewer'
 import { AnnotationOverlay } from '../Viewer/AnnotationOverlay'
 import { AnnotationViewer } from '../Viewer/AnnotationViewer'
 import { AnnotationDrawer } from '../Viewer/AnnotationDrawer'
+import { MeasurementOverlay } from '../Viewer/MeasurementOverlay'
+import { MeasurementViewer } from '../Viewer/MeasurementViewer'
+import { MeasurementDrawer } from '../Viewer/MeasurementDrawer'
+import { HeatmapViewer } from '../Viewer/HeatmapViewer'
 import { useMultipleImages } from '../hooks/useMultipleImages'
 import { useViewport } from '../hooks/useViewport'
 import { useInteraction } from '../hooks/useInteraction'
 import { useAnnotation } from '../hooks/useAnnotation'
+import { useMeasurement } from '../hooks/useMeasurement'
 import { useImage } from '../hooks/useImage'
 import { useFrame } from '../hooks/useFrame'
 import { useDicomFile } from '../hooks/useDicomFile'
 import { useOverlayContext } from '../contexts'
 
 export type { Interaction, SetInteraction } from '../hooks/useInteraction/types'
-export type { Viewport, ViewerError, Contours, Annotation, Point } from '../types'
-export type { DragEvent, Click, Drag, Wheel } from '../hooks/useInteraction/types'
-
+export type {
+  Viewport,
+  ViewerError,
+  Contours,
+  Annotation,
+  Point,
+  LineHeadMode,
+  AnnotationBase,
+  PolygonAnnotation,
+  LineAnnotation,
+  FreeLineAnnotation,
+  CircleAnnotation,
+  AnnotationMode,
+  AnnotationViewerProps,
+  Measurement,
+  MeasurementBase,
+  MeasurementMode,
+  RulerMeasurement,
+  CircleMeasurement,
+} from '../types'
+export type { DragAction, DragEvent, Click, Drag, Wheel } from '../hooks/useInteraction/types'
 export type { OverlayContext } from '../contexts'
 
 Object.assign(InsightViewer, {
   AnnotationOverlay,
   AnnotationViewer,
   AnnotationDrawer,
+  MeasurementOverlay,
+  MeasurementViewer,
+  MeasurementDrawer,
+  HeatmapViewer,
   useMultipleImages,
   useViewport,
   useInteraction,
   useAnnotation,
+  useMeasurement,
   useImage,
   useFrame,
   useDicomFile,
