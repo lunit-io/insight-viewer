4efc3ac6df94b0da808a9fd4df1cac2439ca857b
Takhyun Kim <kimkih1218@lunit.io>
2022-06-03 14:41:05 +0900

    feat(viewer): apply textPoint value to CircleViewer component

--- packages/insight-viewer/src/Viewer/CircleViewer/index.tsx
+++ packages/insight-viewer/src/Viewer/CircleViewer/index.tsx
@@ -3,23 +3,22 @@ import React, { ReactElement } from 'react'
 
 import { CircleViewerProps } from './CircleViewer.types'
 import { circleStyle, textStyle } from './CircleViewer.styles'
-import { CIRCLE_TEXT_POSITION_SPACING } from '../../const'
+
 import { calculateDistance } from '../../utils/common/calculateDistance'
-import { getCircleTextPosition } from '../../utils/common/getCircleTextPosition'
 import { useOverlayContext } from '../../contexts'
 import { Point } from '../../types'
 
 export function CircleViewer({ measurement, hoveredMeasurement }: CircleViewerProps): ReactElement {
   const { pixelToCanvas, image } = useOverlayContext()
 
-  const { id, center, radius } = measurement
+  const { id, center, radius, textPoint } = measurement
   const calculatedDistance = calculateDistance(radius, image)
   const endPoint: Point = [center[0] + (calculatedDistance ?? 0), center[1]]
   const points: [Point, Point] = [center, endPoint]
 
   const [pixelStartPoint, pixelEndPoint] = points.map(pixelToCanvas)
+  const [textPointX, textPointY] = pixelToCanvas(textPoint)
   const drawingRadius = Math.abs(pixelStartPoint[0] - pixelEndPoint[0])
-  const textPosition = getCircleTextPosition(pixelToCanvas(center), drawingRadius)
 
   const isHoveredMeasurement = measurement === hoveredMeasurement
   const [cx, cy] = pixelStartPoint
@@ -36,11 +35,7 @@ export function CircleViewer({ measurement, hoveredMeasurement }: CircleViewerPr
         cy={cy}
         r={drawingRadius}
       />
-      <text
-        style={{ ...textStyle[isHoveredMeasurement ? 'hover' : 'default'] }}
-        x={textPosition[0] + CIRCLE_TEXT_POSITION_SPACING.x}
-        y={textPosition[1] + CIRCLE_TEXT_POSITION_SPACING.y}
-      >
+      <text style={{ ...textStyle[isHoveredMeasurement ? 'hover' : 'default'] }} x={textPointX} y={textPointY}>
         {`radius: ${radius.toFixed(2)}mm`}
       </text>
     </>
