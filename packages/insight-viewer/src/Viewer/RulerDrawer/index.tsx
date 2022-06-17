ab18f3f1b7a2a12cabe73a536a340290bd53a81e
Takhyun Kim <kimkih1218@lunit.io>
2022-06-17 15:02:36 +0900

    feat(viewer): apply DrawingMeasurement type to RulerDrawer component

--- packages/insight-viewer/src/Viewer/RulerDrawer/index.tsx
+++ packages/insight-viewer/src/Viewer/RulerDrawer/index.tsx
@@ -2,33 +2,16 @@ import React, { ReactElement } from 'react'
 
 import { RulerDrawerProps } from './RulerDrawer.types'
 import { polyline, textStyle } from './RulerDrawer.styles'
-import { useOverlayContext } from '../../contexts'
-import { getLineLength } from '../../utils/common/getLineLength'
 
-export function RulerDrawer({ rulerPoints, textPoint, setMeasurementEditMode }: RulerDrawerProps): ReactElement | null {
-  const { image, pixelToCanvas } = useOverlayContext()
-
-  const canvasPoints = rulerPoints.map(pixelToCanvas)
-  const [textPointX, textPointY] = pixelToCanvas(textPoint)
-
-  const linePoints = canvasPoints
-    .map(point => {
-      const [x, y] = point
-      return `${x},${y}`
-    })
-    .join(' ')
-  const lineLength = image ? `${getLineLength(rulerPoints[0], rulerPoints[1], image)?.toFixed(2)}mm` : null
+export function RulerDrawer({ measurement, setMeasurementEditMode }: RulerDrawerProps): ReactElement | null {
+  const { textPoint, linePoints, length } = measurement
 
   return (
     <>
-      {canvasPoints && canvasPoints.length > 0 && image ? (
-        <>
-          <polyline onMouseDown={() => setMeasurementEditMode('move')} style={polyline.default} points={linePoints} />
-          <text style={{ ...textStyle.default }} x={textPointX} y={textPointY}>
-            {lineLength}
-          </text>
-        </>
-      ) : null}
+      <polyline onMouseDown={() => setMeasurementEditMode('move')} style={polyline.default} points={linePoints} />
+      <text style={{ ...textStyle.default }} x={textPoint[0]} y={textPoint[1]}>
+        {`${length}mm`}
+      </text>
     </>
   )
 }
