7eaec7f07234ef9e4dbb01572adda93d2ae8ef36
Takhyun Kim <kimkih1218@lunit.io>
2022-06-17 15:01:33 +0900

    feat(viewer): apply DrawingMeasurement type to CircleDrawer component

--- packages/insight-viewer/src/Viewer/CircleDrawer/index.tsx
+++ packages/insight-viewer/src/Viewer/CircleDrawer/index.tsx
@@ -3,30 +3,20 @@ import React, { ReactElement } from 'react'
 
 import { circleStyle, textStyle } from './CircleDrawer.styles'
 import { CircleDrawerProps } from './CircleDrawer.types'
-import { getCircleRadius } from '../../utils/common/getCircleRadius'
-import { getLineLengthWithoutImage } from '../../utils/common/getLineLengthWithoutImage'
-import { useOverlayContext } from '../../contexts'
 
-export function CircleDrawer({ points, textPoint, setMeasurementEditMode }: CircleDrawerProps): ReactElement | null {
-  const { image, pixelToCanvas } = useOverlayContext()
-
-  const [startPoint, endPoint] = points.map(pixelToCanvas)
-  const [textPointX, textPointY] = pixelToCanvas(textPoint)
-  const radius = getCircleRadius(points, image)
-
-  const drawingRadius = getLineLengthWithoutImage(startPoint, endPoint)
-  const [cx, cy] = startPoint
+export function CircleDrawer({ measurement, setMeasurementEditMode }: CircleDrawerProps): ReactElement | null {
+  const { center, radius, textPoint, drawingRadius } = measurement
 
   return (
     <>
       <circle
         onMouseDown={() => setMeasurementEditMode('move')}
         style={circleStyle.default}
-        cx={cx}
-        cy={cy}
+        cx={center[0]}
+        cy={center[1]}
         r={drawingRadius}
       />
-      <text style={{ ...textStyle.default }} x={textPointX} y={textPointY}>
+      <text style={{ ...textStyle.default }} x={textPoint[0]} y={textPoint[1]}>
         {`radius: ${radius.toFixed(2)}mm`}
       </text>
     </>
