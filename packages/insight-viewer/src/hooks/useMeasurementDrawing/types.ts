a170766337fc5b9445c1c1bd7cbf7a76d489fe23
Takhyun Kim <kimkih1218@lunit.io>
2022-06-17 14:45:17 +0900

    feat(viewer): delete unused useAnnotation, MeasurementDrawing hook

--- packages/insight-viewer/src/hooks/useMeasurementDrawing/types.ts
+++
@@ -1,9 +0,0 @@
-import { MeasurementMode, Measurement } from '../../types'
-
-export interface UseMeasurementDrawingProps {
-  svgElement: React.RefObject<SVGSVGElement> | null
-  device?: 'all' | 'mouse-only' | 'touch-only' | 'stylus-only' | 'mouse-and-stylus'
-  measurements: Measurement[]
-  mode: MeasurementMode
-  onAdd: (annotation: Measurement) => void
-}
