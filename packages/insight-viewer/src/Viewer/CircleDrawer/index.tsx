4cb36b66d6fb228660f7ba1f849ad15e2e2938ae
Takhyun Kim <kimkih1218@lunit.io>
2022-06-28 15:17:04 +0900

    feat(viewer): modify CircleDrawer style and add outline

--- packages/insight-viewer/src/Viewer/CircleDrawer/index.tsx
+++ packages/insight-viewer/src/Viewer/CircleDrawer/index.tsx
@@ -4,19 +4,30 @@ import React, { ReactElement } from 'react'
 import { circleStyle, textStyle } from './CircleDrawer.styles'
 import { CircleDrawerProps } from './CircleDrawer.types'
 
-export function CircleDrawer({ measurement, setMeasurementEditMode }: CircleDrawerProps): ReactElement | null {
+export function CircleDrawer({
+  isSelectedMode,
+  measurement,
+  setMeasurementEditMode,
+}: CircleDrawerProps): ReactElement | null {
   const { center, radius, textPoint, drawingRadius } = measurement
 
   return (
     <>
       <circle
         onMouseDown={() => setMeasurementEditMode('move')}
-        style={circleStyle.default}
+        style={circleStyle.outline}
         cx={center[0]}
         cy={center[1]}
         r={drawingRadius}
       />
-      <text style={{ ...textStyle.default }} x={textPoint[0]} y={textPoint[1]}>
+      <circle
+        onMouseDown={() => setMeasurementEditMode('move')}
+        style={circleStyle[isSelectedMode ? 'select' : 'default']}
+        cx={center[0]}
+        cy={center[1]}
+        r={drawingRadius}
+      />
+      <text style={{ ...textStyle[isSelectedMode ? 'select' : 'default'] }} x={textPoint[0]} y={textPoint[1]}>
         {`radius: ${radius.toFixed(2)}mm`}
       </text>
     </>
