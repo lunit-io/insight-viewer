c4eeb69297b730c230407f6f4bc4f468dfaccf11
Takhyun Kim <kimkih1218@lunit.io>
2022-07-12 16:07:10 +0900

    feat(viewer-docs): delete isEditingTextPoint to measurement mock data

--- apps/insight-viewer-docs/mocks/ruler.ts
+++ apps/insight-viewer-docs/mocks/ruler.ts
@@ -4,7 +4,6 @@ export const RULER_MEASUREMENTS: Measurement[] = [
   {
     id: 1,
     type: 'ruler',
-    isEditingTextPoint: false,
     points: [
       [160.18285714285713, 201.73714285714283],
       [213.57714285714283, 205.39428571428567],
@@ -19,7 +18,6 @@ export const RULER_MEASUREMENTS: Measurement[] = [
   {
     id: 2,
     type: 'ruler',
-    isEditingTextPoint: false,
     points: [
       [179.19999999999996, 292.4342857142857],
       [225.27999999999997, 291.7028571428571],
@@ -34,7 +32,6 @@ export const RULER_MEASUREMENTS: Measurement[] = [
   {
     id: 3,
     type: 'ruler',
-    isEditingTextPoint: false,
     points: [
       [348.8914285714285, 221.48571428571424],
       [266.23999999999995, 239.77142857142854],
@@ -49,7 +46,6 @@ export const RULER_MEASUREMENTS: Measurement[] = [
   {
     id: 4,
     type: 'ruler',
-    isEditingTextPoint: false,
     points: [
       [300.6171428571428, 348.0228571428571],
       [224.5485714285714, 366.3085714285714],
