9a4a37635145f4f2e824ea286c72700201de3b06
Takhyun Kim <kimkih1218@lunit.io>
2022-08-01 18:10:25 +0900

    docs(viewer-docs): apply hasArrowHead field to lines mock data

--- apps/insight-viewer-docs/mocks/lines.ts
+++ apps/insight-viewer-docs/mocks/lines.ts
@@ -11,6 +11,7 @@ export const LINE_ANNOTATIONS: LineAnnotation[] = [
       [185.05142857142854, 183.05142857142854],
       [305.7371428571428, 188.17142857142855],
     ],
+    hasArrowHead: true,
   },
   {
     id: 2,
@@ -21,6 +22,7 @@ export const LINE_ANNOTATIONS: LineAnnotation[] = [
       [263.3142857142857, 248.87999999999997],
       [370.1028571428571, 197.67999999999998],
     ],
+    hasArrowHead: false,
   },
   {
     id: 3,
@@ -31,6 +33,7 @@ export const LINE_ANNOTATIONS: LineAnnotation[] = [
       [155.79428571428568, 267.1657142857143],
       [264.7771428571428, 297.88571428571424],
     ],
+    hasArrowHead: true,
   },
   {
     id: 4,
@@ -41,5 +44,6 @@ export const LINE_ANNOTATIONS: LineAnnotation[] = [
       [350.35428571428565, 256.92571428571426],
       [283.79428571428565, 352.7428571428571],
     ],
+    hasArrowHead: false,
   },
 ]
