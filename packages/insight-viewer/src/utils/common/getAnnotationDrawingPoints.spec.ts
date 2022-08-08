8df7b7a3246fa5fa6f0a535d38a1fcbf72e59fb8
seunghyunOh <sh5h@lunit.io>
2022-08-08 15:19:21 +0900

    refactor(viewer): remove duplicated expressions

--- packages/insight-viewer/src/utils/common/getAnnotationDrawingPoints.spec.ts
+++ packages/insight-viewer/src/utils/common/getAnnotationDrawingPoints.spec.ts
@@ -3,19 +3,19 @@ import { Point } from '../../types'
 import { MOCK_POLYGON_LIST } from '../../mocks/polygons'
 
 describe('getAnnotationDrawingPoints:', () => {
-  it('should return points in polygon mode', () => {
-    const MOCK_POINTS_1: Point[] = [[10, 10]]
-    const MOCK_POINTS_2: Point[] = [
-      [10, 10],
-      [20, 20],
-      [30, 30],
-      [40, 40],
-    ]
-    const MOCK_POLYGON = MOCK_POLYGON_LIST[0].points
-    const MOCK_POINT_1: Point = [20, 20]
-    const MOCK_POINT_2: Point = [30, 30]
-    const MOCK_POINT_3: Point = [100, 100]
+  const MOCK_POINTS_1: Point[] = [[10, 10]]
+  const MOCK_POINTS_2: Point[] = [
+    [10, 10],
+    [20, 20],
+    [30, 30],
+    [40, 40],
+  ]
+  const MOCK_POLYGON = MOCK_POLYGON_LIST[0].points
+  const MOCK_POINT_1: Point = [20, 20]
+  const MOCK_POINT_2: Point = [30, 30]
+  const MOCK_POINT_3: Point = [100, 100]
 
+  it('should return points in polygon mode', () => {
     expect(getAnnotationDrawingPoints(MOCK_POINTS_1, MOCK_POINT_1, 'polygon')).toEqual([
       [10, 10],
       [20, 20],
@@ -30,18 +30,6 @@ describe('getAnnotationDrawingPoints:', () => {
     expect(getAnnotationDrawingPoints(MOCK_POLYGON, MOCK_POINT_3, 'polygon')).toEqual([...MOCK_POLYGON, MOCK_POINT_3])
   })
   it('should return points in free line mode', () => {
-    const MOCK_POINTS_1: Point[] = [[10, 10]]
-    const MOCK_POINTS_2: Point[] = [
-      [10, 10],
-      [20, 20],
-      [30, 30],
-      [40, 40],
-    ]
-    const MOCK_POLYGON = MOCK_POLYGON_LIST[0].points
-    const MOCK_POINT_1: Point = [20, 20]
-    const MOCK_POINT_2: Point = [30, 30]
-    const MOCK_POINT_3: Point = [100, 100]
-
     expect(getAnnotationDrawingPoints(MOCK_POINTS_1, MOCK_POINT_1, 'freeLine')).toEqual([
       [10, 10],
       [20, 20],
@@ -56,17 +44,6 @@ describe('getAnnotationDrawingPoints:', () => {
     expect(getAnnotationDrawingPoints(MOCK_POLYGON, MOCK_POINT_3, 'freeLine')).toEqual([...MOCK_POLYGON, MOCK_POINT_3])
   })
   it('should return points in line mode', () => {
-    const MOCK_POINTS_1: Point[] = [[10, 10]]
-    const MOCK_POINTS_2: Point[] = [
-      [10, 10],
-      [20, 20],
-      [30, 30],
-      [40, 40],
-    ]
-
-    const MOCK_POINT_1: Point = [20, 20]
-    const MOCK_POINT_2: Point = [30, 30]
-
     expect(getAnnotationDrawingPoints(MOCK_POINTS_1, MOCK_POINT_1, 'line')).toEqual([
       [10, 10],
       [20, 20],
@@ -77,17 +54,6 @@ describe('getAnnotationDrawingPoints:', () => {
     ])
   })
   it('should return points in text mode', () => {
-    const MOCK_POINTS_1: Point[] = [[10, 10]]
-    const MOCK_POINTS_2: Point[] = [
-      [10, 10],
-      [20, 20],
-      [30, 30],
-      [40, 40],
-    ]
-
-    const MOCK_POINT_1: Point = [20, 20]
-    const MOCK_POINT_2: Point = [30, 30]
-
     expect(getAnnotationDrawingPoints(MOCK_POINTS_1, MOCK_POINT_1, 'text')).toEqual([
       [10, 10],
       [20, 20],
@@ -98,17 +64,6 @@ describe('getAnnotationDrawingPoints:', () => {
     ])
   })
   it('should return points in circle mode', () => {
-    const MOCK_POINTS_1: Point[] = [[10, 10]]
-    const MOCK_POINTS_2: Point[] = [
-      [10, 10],
-      [20, 20],
-      [30, 30],
-      [40, 40],
-    ]
-
-    const MOCK_POINT_1: Point = [20, 20]
-    const MOCK_POINT_2: Point = [30, 30]
-
     expect(getAnnotationDrawingPoints(MOCK_POINTS_1, MOCK_POINT_1, 'circle')).toEqual([
       [10, 10],
       [20, 20],
