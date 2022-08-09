153aa44fdac58ab265c5ab33fc93a825ee3f53b9
Takhyun Kim <kimkih1218@lunit.io>
2022-08-09 09:13:07 +0900

    test(viewer-docs): add count initial data, delete test of circle

--- apps/insight-viewer-docs/cypress/integration/measurement.viewer.spec.ts
+++ apps/insight-viewer-docs/cypress/integration/measurement.viewer.spec.ts
@@ -1,9 +1,10 @@
 import { setup } from '../support/utils'
 import { VIEWPORT_WIDTH, VIEWPORT_HEIGHT, $LOADED } from '../support/const'
 import { RULER_MEASUREMENTS } from '../../mocks/ruler'
+import { CIRCLE_MEASUREMENTS } from '../../mocks/circles'
 
 describe(
-  'Interaction',
+  'Measurement Viewer',
   {
     viewportWidth: VIEWPORT_WIDTH,
     viewportHeight: VIEWPORT_HEIGHT,
@@ -15,7 +16,7 @@ describe(
       cy.visit('/measurement')
     })
 
-    it('shows initial viewport', () => {
+    it('shows initial measurement viewer', () => {
       cy.get($LOADED).should('be.exist')
     })
 
@@ -55,6 +56,50 @@ describe(
           // Check mock measurement list and target Element
           cy.get(targetDataAttr).should('not.exist')
           cy.get('[data-cy-id]').should('have.length', mockRulerMeasurementLength - (i + 1))
+        })
+
+        cy.get('[data-cy-id]').should('have.length', 0)
+      })
+    })
+
+    describe('Circle Measurement', () => {
+      // Gets the number of circle mock data used by Measurement Viewer docs
+      const mockCircleMeasurementLength = CIRCLE_MEASUREMENTS.length
+
+      it('initial setting of circle measurement testing', () => {
+        cy.get('[data-cy-remove-mode="true"]').click()
+        cy.get('[value="circle"]').click({ force: true })
+      })
+
+      it('count circle measurement', () => {
+        cy.get('[data-cy-id]').should('have.length', mockCircleMeasurementLength)
+      })
+
+      it('Measurement cannot be deleted when remove mode is not activated', () => {
+        // Constant DOM Element and data-attr
+        const targetRulerMeasurement = CIRCLE_MEASUREMENTS[0]
+        const targetDataAttr = `[data-cy-id="${targetRulerMeasurement.id}"]`
+
+        // Click target DOM Element
+        cy.get(targetDataAttr).click({ force: true })
+
+        // Check mock measurement list and target Element
+        cy.get(targetDataAttr).should('exist')
+        cy.get('[data-cy-id]').should('have.length', mockCircleMeasurementLength)
+      })
+
+      it('delete circle measurement and count measurement', () => {
+        cy.get('[data-cy-remove-mode="false"]').click()
+
+        CIRCLE_MEASUREMENTS.forEach((measurement, i) => {
+          const targetDataAttr = `[data-cy-id="${measurement.id - 1}"]`
+
+          // Click target DOM Element
+          cy.get(targetDataAttr).click({ force: true })
+
+          // Check mock measurement list and target Element
+          cy.get(targetDataAttr).should('not.exist')
+          cy.get('[data-cy-id]').should('have.length', mockCircleMeasurementLength - (i + 1))
         })
 
         cy.get('[data-cy-id]').should('have.length', 0)
