4bdb5ca7035853b91c5698f6f9c5e903fea907ea
SangYeob Yu <syyu@lunit.io>
2022-09-07 09:10:31 +0900

    chore(root): require scope on commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [2, 'always', ['release', 'root', 'viewer', 'docs']],
+    'scope-empty': [2, 'never'],
   },
 }
