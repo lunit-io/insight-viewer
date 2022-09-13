3dd8ecbcbc6f0dac2389416f665b6e9aed85a62f
seunghyunOh <sh5h@lunit.io>
2022-09-13 12:14:15 +0900

    fix(viewer): text default position

--- libs/insight-viewer/src/utils/common/getRulerTextPosition.spec.ts
+++ libs/insight-viewer/src/utils/common/getRulerTextPosition.spec.ts
@@ -7,8 +7,8 @@ describe('getRulerTextPosition: ', () => {
     const MOCK_POINT_2: Point = [439, 112]
     const MOCK_POINT_3: Point = [62, 232]
 
-    expect(getRulerTextPosition(MOCK_POINT_1)).toEqual([39, 27])
-    expect(getRulerTextPosition(MOCK_POINT_2)).toEqual([449, 127])
-    expect(getRulerTextPosition(MOCK_POINT_3)).toEqual([72, 247])
+    expect(getRulerTextPosition(MOCK_POINT_1)).toEqual([39, 2])
+    expect(getRulerTextPosition(MOCK_POINT_2)).toEqual([449, 102])
+    expect(getRulerTextPosition(MOCK_POINT_3)).toEqual([72, 222])
   })
 })
--- libs/insight-viewer/src/utils/common/getRulerTextPosition.ts
+++ libs/insight-viewer/src/utils/common/getRulerTextPosition.ts
@@ -1,9 +1,11 @@
 import { Point } from '../../types'
 import { RULER_TEXT_POSITION_SPACING } from '../../const'
 
+const TOP_LOCATION_POSITION = 25
+
 export function getRulerTextPosition(point: Point): Point {
   const positionX = point[0] + RULER_TEXT_POSITION_SPACING.x
-  const positionY = point[1] + RULER_TEXT_POSITION_SPACING.y
+  const positionY = point[1] + RULER_TEXT_POSITION_SPACING.y - TOP_LOCATION_POSITION
 
   return [positionX, positionY]
 }
