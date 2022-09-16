2ec311cf6d4d9222fea8f78f25fc2fb8d14d6631
양혜진 <hjyang@lunit.io>
2022-09-16 17:11:24 +0900

    refactor(viewer): refine radius calculation logic

--- libs/insight-viewer/src/utils/common/getCircleRadius.ts
+++ libs/insight-viewer/src/utils/common/getCircleRadius.ts
@@ -3,11 +3,28 @@ import { IMAGER_PIXEL_SPACING } from './const'
 import type { Point } from '../../types'
 import type { Image } from '../../Viewer/types'
 
-function calculateRadius(startPoint: Point, endPoint: Point, col?: number, row?: number): number {
+/**
+ * The ImageSpacing uses cornerstone image's columnPixelSpacing, rowPixelSpacing
+ * If there is no image or cornerstone's pixel spacing does not exist,
+ * radius is calculated without spacing value
+ */
+
+interface ImageSpacing {
+  columnPixelSpacing: number
+  rowPixelSpacing: number
+}
+
+function calculateRadius(startPoint: Point, endPoint: Point, imageSpacing?: ImageSpacing): number {
   const [x1, y1] = startPoint
   const [x2, y2] = endPoint
 
-  const [distanceX, distanceY] = col && row ? [(x2 - x1) * col, (y2 - y1) * row] : [x2 - x1, y2 - y1]
+  let distanceX = x2 - x1
+  let distanceY = y2 - y1
+
+  if (imageSpacing) {
+    distanceX = (x2 - x1) * imageSpacing.columnPixelSpacing
+    distanceY = (y2 - y1) * imageSpacing.rowPixelSpacing
+  }
 
   const radius = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2))
 
@@ -33,26 +50,26 @@ export function getCircleRadiusByMeasuringUnit(
   currentImage: Image | null
 ): { radius: number; unit: 'px' | 'mm' } {
   if (!currentImage) {
-    return { radius: calculateRadius(startPoint, endPoint, 1, 1), unit: 'px' }
+    return { radius: calculateRadius(startPoint, endPoint), unit: 'px' }
   }
 
   if (currentImage.columnPixelSpacing && currentImage.rowPixelSpacing) {
     const { columnPixelSpacing, rowPixelSpacing } = currentImage
 
-    return { radius: calculateRadius(startPoint, endPoint, columnPixelSpacing, rowPixelSpacing), unit: 'mm' }
+    return { radius: calculateRadius(startPoint, endPoint, { columnPixelSpacing, rowPixelSpacing }), unit: 'mm' }
   }
 
   const imagerPixelSpacing = currentImage.data.string(IMAGER_PIXEL_SPACING) as string | undefined
 
   if (!imagerPixelSpacing) {
-    return { radius: calculateRadius(startPoint, endPoint, 1, 1), unit: 'px' }
+    return { radius: calculateRadius(startPoint, endPoint), unit: 'px' }
   }
 
   if (imagerPixelSpacing.length !== 0) {
     const [columnPixelSpacing, rowPixelSpacing] = imagerPixelSpacing.split('\\').map(Number)
 
-    return { radius: calculateRadius(startPoint, endPoint, columnPixelSpacing, rowPixelSpacing), unit: 'mm' }
+    return { radius: calculateRadius(startPoint, endPoint, { columnPixelSpacing, rowPixelSpacing }), unit: 'mm' }
   }
 
-  return { radius: calculateRadius(startPoint, endPoint, 1, 1), unit: 'px' }
+  return { radius: calculateRadius(startPoint, endPoint), unit: 'px' }
 }
