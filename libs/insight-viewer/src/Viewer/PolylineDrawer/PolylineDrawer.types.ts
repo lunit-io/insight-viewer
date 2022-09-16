751daa2b0ab96fe8a039e98cc83a241925047be3
Takhyun Kim <kimkih1218@lunit.io>
2022-09-16 15:25:36 +0900

    feat(viewer): add showAnnotationLabel prop to Annotation Drawer

--- libs/insight-viewer/src/Viewer/PolylineDrawer/PolylineDrawer.types.ts
+++ libs/insight-viewer/src/Viewer/PolylineDrawer/PolylineDrawer.types.ts
@@ -4,6 +4,7 @@ export interface PolylineDrawerProps {
   annotation: PolygonAnnotation | FreeLineAnnotation | LineAnnotation
   isSelectedMode: boolean
   lineHead: LineHeadMode
+  showAnnotationLabel: boolean
   selectedAnnotationLabel: string | number | null
   setAnnotationEditMode: (mode: EditMode) => void
   isPolygonSelected?: boolean
