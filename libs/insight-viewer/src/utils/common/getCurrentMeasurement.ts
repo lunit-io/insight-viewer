3657bacababa1a1537fe7a3a605532541caab924
양혜진 <hjyang@lunit.io>
2022-09-19 14:54:26 +0900

    refactor(viewer): refine duplicated getMeasurement logic

--- libs/insight-viewer/src/utils/common/getCurrentMeasurement.ts
+++ libs/insight-viewer/src/utils/common/getCurrentMeasurement.ts
@@ -11,37 +11,26 @@ export function getCurrentMeasurement(
   currentTextPosition: Point | null,
   mouseDownPoint: Point | null,
   mouseMovePoint: Point,
-  currentPoints: [centerPoint: Point, endPoint: Point],
+  prevPoints: [centerPoint: Point, endPoint: Point],
   measurements: Measurement[],
   image: Image | null
 ) {
   let currentMeasurement = null
-  if (drawingMode === 'circle') {
-    if (mouseDownPoint !== null) {
-      currentMeasurement = getMeasurement(
-        [mouseDownPoint, mouseMovePoint],
-        currentTextPosition,
-        drawingMode,
-        measurements,
-        image
-      )
-    }
-    if (isEditing && editMode) {
-      const [currentCenterPoint, currentEndPoint] = currentPoints
-      const radius = getCircleRadiusByCenter(currentCenterPoint, currentEndPoint)
-      const currentStartPoint: Point = [currentCenterPoint[0] - radius, currentCenterPoint[1]]
+  let currentPoints = prevPoints
 
-      currentMeasurement = getMeasurement(
-        [currentStartPoint, currentEndPoint],
-        currentTextPosition,
-        drawingMode,
-        measurements,
-        image
-      )
-    }
-  } else {
-    currentMeasurement = getMeasurement(currentPoints, currentTextPosition, drawingMode, measurements, image)
+  if (drawingMode === 'circle' && mouseDownPoint !== null) {
+    currentPoints = [mouseDownPoint, mouseMovePoint]
   }
+
+  if (drawingMode === 'circle' && isEditing && editMode) {
+    const [currentCenterPoint, currentEndPoint] = prevPoints
+    const radius = getCircleRadiusByCenter(currentCenterPoint, currentEndPoint)
+    const currentStartPoint: Point = [currentCenterPoint[0] - radius, currentCenterPoint[1]]
+
+    currentPoints = [currentStartPoint, currentEndPoint]
+  }
+
+  currentMeasurement = getMeasurement(currentPoints, currentTextPosition, drawingMode, measurements, image)
 
   return currentMeasurement
 }
