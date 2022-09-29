0f25edc55b3c3e1d0eb521bd7af107bc86a74136
seunghyunOh <sh5h@lunit.io>
2022-09-29 13:38:56 +0900

    chore(viewer): remove duplicated variable

--- libs/insight-viewer/src/utils/common/getRulerConnectingLine.ts
+++ libs/insight-viewer/src/utils/common/getRulerConnectingLine.ts
@@ -4,10 +4,9 @@ const getDistance = (pointA: Point, pointB: Point) => {
   return Math.pow(pointA[0] - pointB[0], 2) + Math.pow(pointA[1] - pointB[1], 2)
 }
 
-export function getRulerConnectingLine(points: [Point, Point], textPoint: Point): [Point, Point] {
-  const [startPoint, endPoint] = points
+export function getRulerConnectingLine([startPoint, endPoint]: [Point, Point], textPoint: Point): [Point, Point] {
   const distanceFromStartPoint = getDistance(startPoint, textPoint)
   const distanceFromEndPoint = getDistance(endPoint, textPoint)
 
-  return distanceFromStartPoint < distanceFromEndPoint ? [startPoint, textPoint] : [endPoint, textPoint]
+  return [distanceFromStartPoint < distanceFromEndPoint ? startPoint : endPoint, textPoint]
 }
