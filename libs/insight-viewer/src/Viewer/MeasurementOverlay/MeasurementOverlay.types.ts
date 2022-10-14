e7dda506ee19a79b8b9b9dcbf8c439e0430dbcc3
Takhyun Kim <kimkih1218@lunit.io>
2022-10-14 16:08:03 +0900

    feat(viewer): combine onSelect, onRemove prop in Viewer

--- libs/insight-viewer/src/Viewer/MeasurementOverlay/MeasurementOverlay.types.ts
+++ libs/insight-viewer/src/Viewer/MeasurementOverlay/MeasurementOverlay.types.ts
@@ -5,5 +5,7 @@ export interface MeasurementOverlayProps extends MeasurementViewerProps {
   mode?: MeasurementMode
   isDrawing?: boolean
   onAdd?: (measurement: Measurement) => void
+  onRemove?: (measurement: Measurement) => void
+  onSelect: (measurement: Measurement | null) => void
   selectedMeasurement: Measurement | null
 }
