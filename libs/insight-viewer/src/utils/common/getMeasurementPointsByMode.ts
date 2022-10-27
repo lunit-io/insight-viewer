f9e900e4225f6b5f96616a7b41234f2d17da242a
양혜진 <hjyang@lunit.io>
2022-10-27 12:22:19 +0900

    feat(viewer): WIP

--- libs/insight-viewer/src/utils/common/getMeasurementPointsByMode.ts
+++ libs/insight-viewer/src/utils/common/getMeasurementPointsByMode.ts
@@ -17,12 +17,7 @@ export function getMeasurementPointsByMode(
 ): [Point, Point] {
   let currentPoints = prevPoints // 이것 때문에 처음에 살짝 뜰 수 있으므로 주의
 
-  if (drawingMode === 'circle' && mouseDownPoint !== null) {
-    currentPoints = [mouseDownPoint, mouseMovePoint]
-  }
-
   if (drawingMode === 'circle' && isEditing && editMode && editTargetPoints) {
-
     const radius = getCircleRadiusByCenter(prevPoints[0], prevPoints[1])
     const startPoint = getCircleStartPoint(prevPoints[0], radius)
 
