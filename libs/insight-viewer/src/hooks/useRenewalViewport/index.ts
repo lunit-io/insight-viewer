d8bc37d192566aa6d304bc2f06a70e4f87522b4d
Takhyun Kim <kimkih1218@lunit.io>
2022-12-01 15:57:18 +0900

    feat(viewer): add useRenewalViewport hook

---
+++ libs/insight-viewer/src/hooks/useRenewalViewport/index.ts
@@ -0,0 +1,124 @@
+/**
+ * @fileoverview Handles the Viewer's viewport.
+ */
+import { useState, useEffect, useCallback } from 'react'
+
+import { BASE_VIEWPORT, DEFAULT_VIEWPORT_OPTIONS } from '../../const'
+import { formatViewerViewport } from '../../utils/common/formatViewport'
+import { getDefaultViewportForImage } from '../../utils/cornerstoneHelper'
+
+import type { Viewport } from '../../types'
+import type {
+  SetViewportAction,
+  UseRenewalViewportParams,
+  UseRenewalViewportReturnType,
+  InitialViewportSettingOption,
+} from './type'
+
+/**
+ * @param initialViewport The user-defined initial viewport.
+ * @returns {viewport} The viewport which is used as a Viewer's viewport prop.
+ * @returns {setViewport} The viewport setter which is used as a Viewer's onViewportChange prop.
+ * @returns {resetViewport} It resets a Viewer's viewport.
+ * @returns {initialized} Whether the viewport is initialized or not.
+ */
+export function useRenewalViewport(
+  { image, element, options = DEFAULT_VIEWPORT_OPTIONS, getInitialViewport }: UseRenewalViewportParams = {
+    options: DEFAULT_VIEWPORT_OPTIONS,
+  }
+): UseRenewalViewportReturnType {
+  const [viewport, setViewport] = useState<Viewport>({
+    ...BASE_VIEWPORT,
+    _viewportOptions: options,
+  })
+
+  const [InitialViewportSettingOption] = useState<InitialViewportSettingOption>({ getInitialViewport })
+
+  const getDefaultViewport = useCallback(() => {
+    if (image && element) {
+      const defaultViewport = getDefaultViewportForImage(element, image)
+
+      return formatViewerViewport(defaultViewport)
+    }
+
+    return null
+  }, [image, element])
+
+  function resetViewport() {
+    const defaultViewport = getDefaultViewport()
+
+    if (!defaultViewport) {
+      setViewport({
+        ...BASE_VIEWPORT,
+        _viewportOptions: options,
+      })
+
+      return
+    }
+
+    /**
+     * If the user has customized settings, reset using the custom settings
+     * otherwise set to defaultViewport
+     */
+    if (getInitialViewport) {
+      const initialViewport = getInitialViewport(defaultViewport)
+
+      setViewport((prevViewport) => ({ ...prevViewport, ...initialViewport }))
+    } else {
+      setViewport({ ...defaultViewport, _viewportOptions: options })
+    }
+  }
+
+  /**
+   * We assigned the function type and the value type
+   * for the immediate viewport assignment as union type
+   * to utilize the previous viewport.
+   */
+  const handleViewportChange = useCallback(
+    (setViewportAction: SetViewportAction) => {
+      const defaultViewport = getDefaultViewport()
+
+      setViewport((prevViewport) => {
+        if (!defaultViewport) return prevViewport
+
+        const newViewport =
+          typeof setViewportAction === 'function' ? setViewportAction(prevViewport) : setViewportAction
+
+        if (prevViewport._viewportOptions.fitScale && newViewport.scale < defaultViewport.scale) {
+          return { ...newViewport, scale: defaultViewport.scale }
+        }
+
+        return newViewport
+      })
+    },
+    [getDefaultViewport]
+  )
+
+  useEffect(() => {
+    setViewport((prevViewport) => ({ ...prevViewport, _viewportOptions: { fitScale: options.fitScale } }))
+  }, [options.fitScale])
+
+  /**
+   * The purpose of setting the initial Viewport value
+   * when the image is changed
+   */
+  useEffect(() => {
+    const defaultViewport = getDefaultViewport()
+
+    if (!defaultViewport) return
+
+    const getInitialViewport = InitialViewportSettingOption.getInitialViewport
+
+    const initialViewport = getInitialViewport ? getInitialViewport(defaultViewport) : defaultViewport
+
+    setViewport((prevViewport) => ({ ...prevViewport, ...initialViewport }))
+  }, [InitialViewportSettingOption.getInitialViewport, getDefaultViewport])
+
+  return {
+    viewport,
+    setViewport: handleViewportChange,
+    resetViewport,
+    getDefaultViewport,
+    initialized: viewport.scale !== BASE_VIEWPORT.scale, // BASE_VIEWPORT.scale is 0.
+  }
+}
---
+++ libs/insight-viewer/src/hooks/useRenewalViewport/type.ts
@@ -0,0 +1,23 @@
+import type { Image } from '../../Viewer/types'
+import type { Viewport, ViewportOptions } from '../../types'
+
+export interface UseRenewalViewportParams {
+  image?: Image
+  element?: HTMLDivElement
+  options?: ViewportOptions
+  getInitialViewport?: (defaultViewport: Viewport) => Viewport
+}
+
+export interface UseRenewalViewportReturnType {
+  viewport: Viewport
+  initialized: boolean
+  resetViewport: () => void
+  getDefaultViewport: () => void
+  setViewport: (setViewportAction: SetViewportAction) => void
+}
+
+export interface InitialViewportSettingOption {
+  getInitialViewport?: (defaultViewport: Viewport) => Viewport
+}
+
+export type SetViewportAction = Viewport | ((prevViewport: Viewport) => Viewport)
