47738b3b8891f41bfc1164f1ac7c6b805a464822
Takhyun Kim <kimkih1218@lunit.io>
2022-12-20 15:16:19 +0900

    refactor(viewer): change to declare outside useViewport getViewport

--- libs/insight-viewer/src/Viewport/useViewport/index.ts
+++ libs/insight-viewer/src/Viewport/useViewport/index.ts
@@ -19,6 +19,24 @@ const getDefaultViewport = (image: Image | undefined, element: HTMLDivElement | 
   return formatViewerViewport(defaultViewport)
 }
 
+const getViewportWithFitScaleOption = (
+  viewport: Viewport,
+  image: Image | undefined,
+  element: HTMLDivElement | undefined
+): Viewport => {
+  const defaultViewport = getDefaultViewport(image, element)
+
+  if (!defaultViewport) return viewport
+
+  const currentFitScaleOption = viewport._viewportOptions.fitScale
+
+  if (currentFitScaleOption && viewport.scale < defaultViewport.scale) {
+    return { ...viewport, scale: defaultViewport.scale }
+  }
+
+  return viewport
+}
+
 export function useViewport(
   { image, element, options = DEFAULT_VIEWPORT_OPTIONS, getInitialViewport }: UseViewportParams = {
     image: undefined,
@@ -35,35 +53,8 @@ export function useViewport(
   const elementRef = useRef(element)
   const getInitialViewportRef = useRef(getInitialViewport)
 
-  const getDefaultViewport = useCallback(() => {
-    if (imageRef.current && elementRef.current) {
-      const defaultViewport = getDefaultViewportForImage(elementRef.current, imageRef.current)
-
-      return formatViewerViewport(defaultViewport)
-    }
-
-    return null
-  }, [])
-
-  const getViewportWithFitScaleOption = useCallback(
-    (viewport: Viewport, fitScale: boolean): Viewport => {
-      const defaultViewport = getDefaultViewport()
-
-      const ViewportOfUpdatingFitScaleOption = { ...viewport, _viewportOptions: { fitScale } }
-
-      if (!defaultViewport) return ViewportOfUpdatingFitScaleOption
-
-      if (fitScale && ViewportOfUpdatingFitScaleOption.scale < defaultViewport.scale) {
-        return { ...ViewportOfUpdatingFitScaleOption, scale: defaultViewport.scale }
-      }
-
-      return ViewportOfUpdatingFitScaleOption
-    },
-    [getDefaultViewport]
-  )
-
   const resetViewport = useCallback(() => {
-    const defaultViewport = getDefaultViewport()
+    const defaultViewport = getDefaultViewport(imageRef.current, elementRef.current)
 
     if (!defaultViewport) {
       setViewport({
