16251d8c48e304ab02cea7587152edaf4eec6fd3
Takhyun Kim <kimkih1218@lunit.io>
2023-04-05 12:53:45 +0900

    feat(dev): delete unused combined-overlay page to dev docs

--- apps/insight-viewer-dev/pages/combined-overlay.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const DynamicComponentWithNoSSR = dynamic(() => import('../containers/CombinedOverlay/Tab'), {
-  ssr: false,
-})
-
-export default DynamicComponentWithNoSSR
