6d3884c245747cc69fdfcf40367617daa35649df
Takhyun Kim <kimkih1218@lunit.io>
2023-04-25 21:01:33 +0900

    refactor(viewer): delete unused useViewport hook

--- libs/insight-viewer/src/index.ts
+++ libs/insight-viewer/src/index.ts
@@ -1,10 +1,11 @@
 export { InsightViewer as default } from './Viewer'
 export { HeatmapViewer } from './Viewer/HeatmapViewer'
 export { useMultipleImages } from './hooks/useMultipleImages'
-export { useViewport } from './hooks/useViewport'
 export { useInteraction } from './hooks/useInteraction'
+export { useDicomFile } from './hooks/useDicomFile'
 export { useImage } from './hooks/useImage'
 export { useFrame } from './hooks/useFrame'
+export { useOverlayContext } from './contexts'
 export type { Interaction, SetInteraction } from './hooks/useInteraction/types'
 export type {
   Viewport,
@@ -30,6 +31,4 @@ export type {
   ArrowLineAnnotation,
 } from './types'
 export type { DragAction, DragEvent, Click, Drag, Wheel } from './hooks/useInteraction/types'
-export { useDicomFile } from './hooks/useDicomFile'
-export { useOverlayContext } from './contexts'
 export type { OverlayContext } from './contexts'
