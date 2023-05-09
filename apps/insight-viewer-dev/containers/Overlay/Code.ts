6cb16a4d1e196e3d6895387d8506c0a63d1a8464
Takhyun Kim <kimkih1218@lunit.io>
2023-05-09 16:46:14 +0900

    docs(dev): update Overlay example code

--- apps/insight-viewer-dev/containers/Overlay/Code.ts
+++ apps/insight-viewer-dev/containers/Overlay/Code.ts
@@ -1,21 +1,41 @@
 export const CODE = `\
-  import InsightViewer, { useImage, useViewport } from '@lunit/insight-viewer'
+  import { useRef } from 'react'
+  import InsightViewer, { useImage } from '@lunit/insight-viewer'
+  import { useViewport } from '@lunit/insight-viewer/viewport
+
+  import type { Viewport } from '@lunit/insight-viewer'
 
   const style = {
     width: '500px',
     height: '500px'
   }
 
+  function OverlayLayer({ scale, hflip, vflip, x, y, invert }: { viewport: Viewport }) {
+    <div style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', color: 'blue' }}>
+      scale: <span data-cy-scale>{scale.toFixed(2)}</span>
+      hflip/vflip: <span data-cy-hflip>{hflip}</span> / <span data-cy-vflip>{vflip}</span>
+      translation: <span data-cy-x>{x?.toFixed(2)}</span> / <span data-cy-y>{y?.toFixed(2)}</span>
+      invert: <span data-cy-invert>{invert}</span>
+    </div>
+  }
+
   export default function Viewer() {
+    const viewerRef = useRef<HTMLDivElement>(null)
+
     const { image } = useImage({
       wadouri: IMAGE_ID,
     })
-    const { viewport, setViewport } = useViewport()
+
+    const { viewport, setViewport } = useViewport({
+      image,
+      element: viewerRef.current,
+    })
 
     return (
       <div style={style}>
-        <InsightViewer 
-          image={image} 
+        <InsightViewer
+          viewerRef={viewerRef}
+          image={image}
           viewport={viewport}
           onViewportChange={setViewport}
         >
