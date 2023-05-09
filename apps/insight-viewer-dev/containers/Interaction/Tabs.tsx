bd35146bfb7d91d20eec6f3c8800631d82f8ea45
Takhyun Kim <kimkih1218@lunit.io>
2023-05-09 16:58:56 +0900

    docs(dev): delete unused default interaction docs

--- apps/insight-viewer-dev/containers/Interaction/Tabs.tsx
+++ apps/insight-viewer-dev/containers/Interaction/Tabs.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Base from './Base'
 import Custom from './Custom'
-import DefaultInteraction from './DefaultInteraction'
 
 export default function App(): JSX.Element {
   const [active, setActive] = useState(0)
@@ -16,7 +15,6 @@ export default function App(): JSX.Element {
       <TabList>
         <Tab>Base Interaction</Tab>
         <Tab className="custom-tab">Custom Interaction</Tab>
-        <Tab className="default-tab">Default Interaction</Tab>
       </TabList>
 
       <TabPanels>
@@ -25,9 +23,6 @@ export default function App(): JSX.Element {
         </TabPanel>
         <TabPanel p={0} pt={6}>
           {active === 1 && <Custom />}
-        </TabPanel>
-        <TabPanel p={0} pt={6}>
-          {active === 2 && <DefaultInteraction />}
         </TabPanel>
       </TabPanels>
     </Tabs>
