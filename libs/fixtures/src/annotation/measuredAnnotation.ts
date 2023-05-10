06ff9bd4aa405bb153726cda9758b788949c6537
Takhyun Kim <kimkih1218@lunit.io>
2023-05-10 11:33:45 +0900

    chore(dev): modify area, ruler mock data and add measuredAnnotation

---
+++ libs/fixtures/src/annotation/measuredAnnotation.ts
@@ -0,0 +1,25 @@
+import type { Annotation } from '@lunit/insight-viewer/annotation'
+
+export const MEASURED_ANNOTATIONS: Annotation[] = [
+  {
+    id: 4,
+    type: 'ruler',
+    startAndEndPoint: [
+      [300.6171428571428, 348.0228571428571],
+      [224.5485714285714, 366.3085714285714],
+    ],
+    measuredValue: 38.2,
+    textPoint: [234.5485714285714, 386.3085714285714],
+    unit: 'mm',
+  },
+  {
+    id: 5,
+    lineWidth: 1.5,
+    type: 'area',
+    centerPoint: [141.16571428571427, 218.89142857142855],
+    measuredValue: 16.9670963417275,
+    radius: 16.9670963417275,
+    unit: 'mm',
+    textPoint: null,
+  },
+]
