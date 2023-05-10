b98b35c98ec69570075fce3e9d84857fafb26968
Takhyun Kim <kimkih1218@lunit.io>
2023-05-10 11:44:51 +0900

    test(dev): apply initial measurement value to drawer e2e test

--- apps/insight-viewer-dev-e2e/src/e2e/measurement.drawer.cy.ts
+++ apps/insight-viewer-dev-e2e/src/e2e/measurement.drawer.cy.ts
@@ -1,6 +1,6 @@
 import { setup, drawMeasurements, deleteAndCheckAnnotationOrMeasurement } from '../support/utils'
 import { VIEWPORT_WIDTH, VIEWPORT_HEIGHT, $LOADED } from '../support/const'
-import { RULER_MEASUREMENTS } from '@insight-viewer-library/fixtures'
+import { RULER_MEASUREMENTS, MEASURED_ANNOTATIONS } from '@insight-viewer-library/fixtures'
 
 describe(
   'Measurement Drawer',
@@ -16,7 +16,6 @@ describe(
     })
 
     it('shows initial measurement drawer', () => {
-      cy.get('[data-cy-tab="drawer"]').click()
       cy.get($LOADED).should('be.exist')
     })
 
@@ -25,21 +24,21 @@ describe(
       const mockRulerMeasurementLength = RULER_MEASUREMENTS.length
 
       it('count polygon annotation before drawing', () => {
-        cy.get('[data-cy-id]').should('have.length', 0)
+        cy.get('[data-cy-id]').should('have.length', MEASURED_ANNOTATIONS.length)
       })
 
       it('drawing ruler measurement', () => {
         drawMeasurements(RULER_MEASUREMENTS)
 
-        cy.get('[data-cy-id]').should('have.length', mockRulerMeasurementLength)
+        cy.get('[data-cy-id]').should('have.length', mockRulerMeasurementLength + MEASURED_ANNOTATIONS.length)
       })
 
       it('delete ruler measurement and count measurement', () => {
-        const targetMeasurement = RULER_MEASUREMENTS[1]
+        const targetMeasurement = MEASURED_ANNOTATIONS[0]
 
         deleteAndCheckAnnotationOrMeasurement(targetMeasurement, 'not.exist')
 
-        cy.get('[data-cy-id]').should('have.length', mockRulerMeasurementLength - 1)
+        cy.get('[data-cy-id]').should('have.length', mockRulerMeasurementLength + MEASURED_ANNOTATIONS.length - 1)
       })
     })
   }
