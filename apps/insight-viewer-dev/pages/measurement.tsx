59cb3ac655bb4940aa562507b109b7d74fa30809
Takhyun Kim <kimkih1218@lunit.io>
2023-05-11 13:47:02 +0900

    fix(dev): delete unused measurement dev docs page

--- apps/insight-viewer-dev/pages/measurement.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const DynamicComponentWithNoSSR = dynamic(() => import('../containers/Measurement'), {
-  ssr: false,
-})
-
-export default DynamicComponentWithNoSSR
