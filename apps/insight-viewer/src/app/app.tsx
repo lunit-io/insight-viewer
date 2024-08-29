7517470b61fa8a7481711183e6977b0c9850d97f
LTakhyunKim <kimkih1218@lunit.io>
2024-08-29 23:08:42 +0900

    refactor: separate MultiViewer example code

--- apps/insight-viewer/src/app/app.tsx
+++ apps/insight-viewer/src/app/app.tsx
@@ -1,72 +1,7 @@
-import { useState } from 'react';
-import { DicomViewer } from '@lunit-insight-viewer/react';
-
-import type { MappingToolWithKey } from '@lunit-insight-viewer/core';
-
-const imageIds = [
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000000.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000001.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000003.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000004.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000005.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000006.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000007.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000008.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000009.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000010.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm',
-  'wadouri:https://static.lunit.io/insight/samples/cxr/Nodule.dcm',
-  'wadouri:https://static.lunit.io/insight/samples/mmg/case01_LCC.dcm',
-];
-const imageIds2 = [
-  'wadouri:https://static.lunit.io/insight/samples/cxr/Nodule.dcm',
-  'wadouri:https://static.lunit.io/insight/samples/mmg/case01_LCC.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000000.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000001.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000003.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000004.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000005.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000006.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000007.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000008.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000009.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000010.dcm',
-  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm',
-  'wadouri:https://static.lunit.io/insight/samples/cxr/Nodule.dcm',
-  'wadouri:https://static.lunit.io/insight/samples/mmg/case01_LCC.dcm',
-];
-
-const tools: MappingToolWithKey[] = [
-  { tool: 'frame' },
-  { tool: 'pan' },
-  { tool: 'windowing' },
-  { tool: 'zoom' },
-];
+import { MultiViewer } from './MultiViewer';
 
 function App() {
-  const [value, setValue] = useState<boolean>(false);
-
-  return (
-    <div style={{ display: 'flex', gap: 10 }}>
-      <div style={{ display: 'flex', gap: 2, width: '500px', height: '500px' }}>
-        <DicomViewer imageIds={imageIds} tools={tools} />
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          width: '500px',
-          height: '500px',
-        }}
-      >
-        <DicomViewer imageIds={value ? imageIds : imageIds2} tools={tools} />
-        <button onClick={() => setValue((prev) => !prev)}>스위치</button>
-      </div>
-    </div>
-  );
+  return <MultiViewer />;
 }
 
 export default App;
---
+++ apps/insight-viewer/src/app/image.ts
@@ -0,0 +1,43 @@
+import type { MappingToolWithKey } from '@lunit-insight-viewer/core';
+
+export const tools: MappingToolWithKey[] = [
+  { tool: 'frame' },
+  { tool: 'pan' },
+  { tool: 'windowing' },
+  { tool: 'zoom' },
+];
+
+export const imageIds = [
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000000.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000001.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000003.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000004.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000005.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000006.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000007.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000008.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000009.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000010.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm',
+  'wadouri:https://static.lunit.io/insight/samples/cxr/Nodule.dcm',
+  'wadouri:https://static.lunit.io/insight/samples/mmg/case01_LCC.dcm',
+];
+export const imageIds2 = [
+  'wadouri:https://static.lunit.io/insight/samples/cxr/Nodule.dcm',
+  'wadouri:https://static.lunit.io/insight/samples/mmg/case01_LCC.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000000.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000001.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000003.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000004.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000005.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000006.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000007.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000008.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000009.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000010.dcm',
+  'wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000011.dcm',
+  'wadouri:https://static.lunit.io/insight/samples/cxr/Nodule.dcm',
+  'wadouri:https://static.lunit.io/insight/samples/mmg/case01_LCC.dcm',
+];
