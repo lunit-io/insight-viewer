83f115d812fcdb5e8a672e04ef2ed638933c94d2
LTakhyunKim <kimkih1218@lunit.io>
2024-09-25 16:04:22 +0900

    docs: update useEffect using comment

--- libs/react-dicom-viewer/src/useStackViewport.ts
+++ libs/react-dicom-viewer/src/useStackViewport.ts
@@ -38,6 +38,7 @@ export const useStackViewport = ({
     viewerFactoryRef!.current.getSnapshot
   );
 
+  /* initialize viewer */
   useEffect(() => {
     if (viewerInfo) return;
     if (!viewerFactoryRef.current || !element) return;
@@ -45,6 +46,7 @@ export const useStackViewport = ({
     viewerFactoryRef.current.init(element, imageIds, tools, onChange);
   }, [element, imageIds, tools, viewerInfo, onChange]);
 
+  /* sync viewer with viewerInfo */
   useEffect(() => {
     if (!viewerInfo || !viewerFactoryRef.current || !element) return;
     if (snapshot === viewerInfo) return;
@@ -52,6 +54,7 @@ export const useStackViewport = ({
     viewerFactoryRef.current.updateSnapshot(viewerInfo, element);
   }, [viewerInfo, snapshot, element]);
 
+  /* update viewer configuration */
   useEffect(() => {
     if (!viewerFactoryRef.current || !element) return;
 
