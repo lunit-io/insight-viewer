dd8e07b845597ab70267e2f3f1dc7b2eaf512094
LTakhyunKim <kimkih1218@lunit.io>
2024-12-02 15:25:07 +0900

    docs: delete unused example code to app

--- apps/insight-viewer/src/app/app.tsx
+++ apps/insight-viewer/src/app/app.tsx
@@ -1,14 +1,6 @@
 import { ControlledViewer } from './ControlledViewer';
-import { MultiViewer } from './MultiViewer';
-import {
-  HighLevelDicomViewerWithHook,
-  LowLevelDicomViewerWithHook,
-} from './DicomViewerWithHook';
 
 function App() {
-  // return <HighLevelDicomViewerWithHook />;
-  // return <LowLevelDicomViewerWithHook />;
-  // return <MultiViewer />;
   return <ControlledViewer />;
 }
 
