74cb2e87e69f5da269d431c34caa8bdae322dc66
Takhyun Kim <kimkih1218@lunit.io>
2024-12-06 14:49:13 +0900

    docs: apply docusaurus comment to app

--- apps/insight-viewer/src/app/app.tsx
+++ apps/insight-viewer/src/app/app.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 import { ControlledViewer } from './api-docs/ControlledDIcomViewer/docs';
 
+// TODO: 향후 docusaurus 로 대체
 function App() {
-  const [docs, setDocs] = useState<string>('');
+  const [docs, setDocs] = useState<string>('ControlledViewer');
   return (
     <div>
       <button onClick={() => setDocs('ControlledViewer')}>
