998977f8d842143ab8f4fd7f05790eac1e1962a3
Takhyun Kim <kimkih1218@lunit.io>
2024-12-10 17:37:32 +0900

    feat: update viewport tool action handling logic to docs

--- apps/insight-viewer/src/app/api-docs/ViewportControlViewer/Example.tsx
+++ apps/insight-viewer/src/app/api-docs/ViewportControlViewer/Example.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
-import { DicomViewer } from '@lunit-insight-viewer/react';
+import { useDicomViewer } from '@lunit-insight-viewer/react';
 
+import type { Tool } from '@lunit-insight-viewer/core';
 import { imageIds, tool as defaultTool } from '../../image';
 
 export function ViewportControlViewer() {
-  const [tool, setTool] = useState(defaultTool);
+  const [tool, setTool] = useState<Tool>({
+    pan: 'wheelDrag',
+  });
+  const { viewerElementRef } = useDicomViewer({
+    imageIds,
+    tool,
+  });
 
   return (
     <div
@@ -15,19 +22,23 @@ export function ViewportControlViewer() {
         marginTop: '10px',
       }}
     >
-      <div style={{ display: 'flex', gap: '10px', width: '50%' }}>
+      <div style={{ display: 'flex', gap: '10px', width: '80%' }}>
         <button onClick={() => setTool(defaultTool)}>기본 툴</button>
         <button onClick={() => setTool(null)}>비어있는 툴</button>
-        <button onClick={() => setTool({ frame: 'wheel' })}>프레임 툴</button>
-        <button onClick={() => setTool({ pan: 'wheelDrag' })}>팬 툴</button>
+        <button onClick={() => setTool({ frame: 'wheel' })}>
+          프레임 툴 with wheel
+        </button>
+        <button onClick={() => setTool({ pan: 'wheelDrag' })}>
+          팬 툴 with wheelDrag
+        </button>
         <button onClick={() => setTool({ windowing: 'primaryDrag' })}>
-          윈도우링 툴
+          윈도우링 툴 with primaryDrag
         </button>
         <button onClick={() => setTool({ zoom: 'secondaryDrag' })}>
-          줌 툴
+          줌 툴 with secondaryDrag
         </button>
       </div>
-      <DicomViewer imageIds={imageIds} tool={tool} />
+      <div style={{ width: '500px', height: '500px' }} ref={viewerElementRef} />
     </div>
   );
 }
