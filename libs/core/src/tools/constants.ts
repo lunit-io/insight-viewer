ba9e8f314e9c770c342dcceac0b6042dda427a08
Takhyun Kim <kimkih1218@lunit.io>
2024-12-10 16:41:52 +0900

    refactor: rename support key naming

--- libs/core/src/tools/constants.ts
+++ libs/core/src/tools/constants.ts
@@ -6,16 +6,10 @@ import {
   Enums as csToolsEnums,
 } from '@cornerstonejs/tools';
 
-/**
- * left: mouse left click
- * right: mouse right click
- * middle: mouse wheel click
- * wheel: mouse wheel
- */
 export const SUPPORT_KEYS = {
-  left: csToolsEnums.MouseBindings.Primary,
-  right: csToolsEnums.MouseBindings.Secondary,
-  middle: csToolsEnums.MouseBindings.Auxiliary,
+  primaryDrag: csToolsEnums.MouseBindings.Primary,
+  secondaryDrag: csToolsEnums.MouseBindings.Secondary,
+  wheelDrag: csToolsEnums.MouseBindings.Auxiliary,
   // TODO: Cornerstone 3D 에서 Wheel 커스텀 방식 체크, 아래는 임시 타입
   wheel: csToolsEnums.MouseBindings.Fourth_Button,
 };
@@ -31,8 +25,8 @@ export const DEFAULT_MAPPED_TOOL_WITH_KEY: Record<
   keyof typeof MAPPED_SUPPORT_TOOL,
   keyof typeof SUPPORT_KEYS
 > = {
-  pan: 'middle',
-  windowing: 'left',
+  pan: 'wheelDrag',
+  windowing: 'primaryDrag',
   frame: 'wheel',
-  zoom: 'right',
+  zoom: 'secondaryDrag',
 };
--- libs/core/src/tools/types.ts
+++ libs/core/src/tools/types.ts
@@ -5,18 +5,6 @@ import type { Types } from '@cornerstonejs/tools';
 export type ToolGroup = Types.IToolGroup;
 export type SupportedTool = keyof typeof MAPPED_SUPPORT_TOOL;
 export type SupportedKey = keyof typeof SUPPORT_KEYS;
-
-/**
- * ===== Default tool key mapping info =====
- *
- * windowing: mouse left button
- *
- * zoom: mouse right button
- *
- * pan: mouse middle button
- *
- * frame: mouse scroll button
- */
 export type Tool = {
   tool: SupportedTool;
   key?: SupportedKey;
